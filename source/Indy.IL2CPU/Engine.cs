6e702262eece47db24b5c58d9257402e7273174c
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-08-29 16:08:44 +0000

    More structure

--- source/Indy.IL2CPU/Engine.cs
+++ source/Indy.IL2CPU/Engine.cs
@@ -10,12 +10,15 @@ using Mono.Cecil.Cil;
 
 namespace Indy.IL2CPU {
 	public class Engine {
-		private OpCodeMap mMap = new OpCodeMap();
+
+		protected OpCodeMap mMap = new OpCodeMap();
+
 		public void Execute(string aAssembly, string opAssembly) {
 			mMap.LoadOpMapFromAssembly(opAssembly);
             AssemblyDefinition xAD = AssemblyFactory.GetAssembly(aAssembly);
-			if (xAD.EntryPoint == null)
-				throw new NotSupportedException("Libraries are not yet supported!");
+            if (xAD.EntryPoint == null) {
+                throw new NotSupportedException("Libraries are not yet supported!");
+            }
 			foreach (Instruction xInstruction in xAD.EntryPoint.Body.Instructions) {
                 mMap.GetOpForOpCode(xInstruction.OpCode.Code).Assemble(xInstruction);
 			}
