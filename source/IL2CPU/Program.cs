5a894231e9c9bfab7fe1379b730b730329ffcbf7
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-08-29 10:18:05 +0000

    Initial checkin

---
+++ source/IL2CPU/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IL2CPU {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
