654bcf7e56f585a6bab3e267aa41e6d5047ecaee
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2007-08-29 13:33:49 +0000

    missing file

---
+++ source/Indy.IL2CPU.Core/Engine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Indy.IL2CPU.IL;
+
+namespace Indy.IL2CPU {
+	public class Engine {
+		private OpCodeMap mMap = new OpCodeMap();
+		public void Execute(string assembly) {
+			Assembly a = Assembly.ReflectionOnlyLoadFrom(assembly);
+			if (a.EntryPoint == null)
+				throw new NotSupportedException("Libraries are not yet supported!");
+			ILReader reader = new ILReader(a.EntryPoint.GetMethodBody().GetILAsByteArray());
+			byte curByte;
+			while(reader.TryReadByte(out curByte))
+			{
+				mMap.GetOpForOpCode(curByte).Process(reader);
+			}
+			Console.WriteLine("Done");
+		}
+	}
+}
