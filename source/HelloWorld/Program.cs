ebf8e7edc313b829b5e72bbbf3cd552961161080
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-08-31 14:18:31 +0000

    more tests

--- source/HelloWorld/Program.cs
+++ source/HelloWorld/Program.cs
@@ -2,8 +2,20 @@
 
 namespace HelloWorld {
 	public class Program {
-		public static void Main(string[] args) {
+        
+        public static void Jump() {
+            Integer();
+        }
+
+        public static void Integer() {
+            int i = 22;
+        }
+
+        public static void NewObject() {
             object x = new string('t', 1);
+        }
+
+        public static void Main(string[] args) {
             string s = "Test";
 			Console.WriteLine("Hello world!");
 			Console.ReadLine();
