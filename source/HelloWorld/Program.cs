9a82bb1beb33de9501631935b130694817837c15
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-08-31 13:31:29 +0000

    Simple steps to test first.

--- source/HelloWorld/Program.cs
+++ source/HelloWorld/Program.cs
@@ -3,6 +3,8 @@
 namespace HelloWorld {
 	public class Program {
 		public static void Main(string[] args) {
+            object x = new string('t', 1);
+            string s = "Test";
 			Console.WriteLine("Hello world!");
 			Console.ReadLine();
 		}
