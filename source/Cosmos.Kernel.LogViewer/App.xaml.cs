90313bc1e6710f6bd64aab2fcdc62cd3fc965b32
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2007-11-13 16:33:25 +0000

    Initial Try to make a log viewer

---
+++ source/Cosmos.Kernel.LogViewer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Xml;
+
+namespace Cosmos.Kernel.LogViewer {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App: Application {
+		private void Application_Startup(object sender, StartupEventArgs e) {
+			System.Diagnostics.PresentationTraceSources.DataBindingSource.Switch.Level = System.Diagnostics.SourceLevels.All;
+			System.Diagnostics.PresentationTraceSources.DataBindingSource.Listeners.Add(new ConsoleTraceListener());
+		}
+	}
+}
