de524fb6fc060c4d8c62bca78b14af0252928c70
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2007-11-17 13:12:52 +0000

    Added correct QEmu version

---
+++ Tools/qemu/qemu-win.bat
@@ -0,0 +1,20 @@
+REM Start qemu on windows.
+@ECHO OFF
+
+REM SDL_VIDEODRIVER=directx is faster than windib. But keyboard cannot work well.
+SET SDL_VIDEODRIVER=windib
+
+REM SDL_AUDIODRIVER=waveout or dsound can be used. Only if QEMU_AUDIO_DRV=sdl.
+SET SDL_AUDIODRIVER=dsound
+
+REM QEMU_AUDIO_DRV=dsound or fmod or sdl or none can be used. See qemu -audio-help.
+SET QEMU_AUDIO_DRV=dsound
+
+REM QEMU_AUDIO_LOG_TO_MONITOR=1 displays log messages in QEMU monitor.
+SET QEMU_AUDIO_LOG_TO_MONITOR=0
+
+REM PCI-based PC(default): -M pc 
+REM ISA-based PC         : -M isapc
+REM -M isapc is added for NE2000 ISA card.
+
+qemu.exe -L . -m 128 -hda linux.img -soundhw all -localtime -M isapc
