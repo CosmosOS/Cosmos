a489efab6ed30abd315e3e800afb985da284a136
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-11-18 12:53:30 +0000

    worked around bug in ISO tool

--- source/ISO/Build.bat
+++ source/ISO/Build.bat
@@ -1,4 +1,5 @@
 del cosmos.iso
+attrib files\boot\grub\stage2_eltorito -r
 ..\..\Tools\mkisofs\mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o Cosmos.iso files
 pause
 ..\..\tools\qemu\qemu.exe -L ..\..\tools\qemu -cdrom Cosmos.iso -boot d
