58e089a2fc232f4d2e6c47e4fd2fb72162844ac7
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-11-18 12:34:57 +0000

    ISO auto boot

---
+++ source/ISO/Build.bat
@@ -0,0 +1,5 @@
+del cosmos.iso
+..\..\Tools\mkisofs\mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o Cosmos.iso files
+pause
+..\..\tools\qemu\qemu.exe -L ..\..\tools\qemu -cdrom Cosmos.iso -boot d
+pause
