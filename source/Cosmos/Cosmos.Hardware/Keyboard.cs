2a74da3814772f9b8817389728588489bb4fbffc
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-11-20 15:46:22 +0000

    New Cosmos Shell

---
+++ source/Cosmos/Cosmos.Hardware/Keyboard.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Hardware
+{
+    class Keyboard
+    {
+    }
+}
---
+++ source/Cosmos/Cosmos.Shell.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Shell.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
