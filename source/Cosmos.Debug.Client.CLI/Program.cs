edc32cc8b8658805af5f6806d1b233c9bbb3a56f
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2007-11-21 15:35:57 +0000

    QEmu launcher added

---
+++ source/Cosmos.Debug.Client.CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.Debug.Client.CLI {
+	class Program {
+		static void Main(string[] args) {
+			string xBasePath = Path.Combine(Path.GetDirectoryName(typeof (Program).Assembly.Location), @"..\..\..\..\Build\Cosmos\ISO\");
+			ProcessStartInfo xProcessInfo = new ProcessStartInfo();
+			xProcessInfo.FileName = Path.Combine(xBasePath, "Build.bat");
+			xProcessInfo.CreateNoWindow = true;
+			xProcessInfo.WorkingDirectory = xBasePath;
+			Process.Start(xProcessInfo).WaitForExit();
+		}
+	}
+}
