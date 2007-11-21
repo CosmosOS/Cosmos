930ab5724a76d6acf324b094d97340a44a9439eb
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2007-11-21 16:59:30 +0000

    Bios Int sample

---
+++ source/Cosmos/Cosmos.Kernel/Interrupts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Kernel {
+    public class Interrupts {
+        // Interrupt 15, AH=0
+        [X86.Int(0x15, 0x00)]
+        public void ReadChar() {
+        }
+    }
+}
