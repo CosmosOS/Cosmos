a1cad2adfa67de165626f712d1570f7fd9db7919
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-01 15:31:45 +0000

    not building due to array issue

--- source/Cosmos/Cosmos.Kernel/Staging/StageQueue.cs
+++ source/Cosmos/Cosmos.Kernel/Staging/StageQueue.cs
@@ -3,65 +3,65 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Cosmos.Kernel.Staging {
-        /// <summary>
-        /// Represents a stage queue.
-        /// </summary>
-        public class StageQueue {
-                /// <summary>
-                /// The list of initialize stages.
-                /// </summary>
-                private Queue<IStage> _initialize = new Queue<IStage> ();
+	/// <summary>
+	/// Represents a stage queue.
+	/// </summary>
+	public class StageQueue {
+		/// <summary>
+		/// The list of initialize stages.
+		/// </summary>
+		private Queue<IStage> _initialize = new Queue<IStage> ();
 
-                /// <summary>
-                /// The list of teardown stages.
-                /// </summary>
-                private Queue<IStage> _teardown = new Queue<IStage> ();
+		/// <summary>
+		/// The list of teardown stages.
+		/// </summary>
+		private Stack<IStage> _teardown = new Stack<IStage> ();
 
-                private IStage _current;
-                /// <summary>
-                /// Gets the current kernel stage.
-                /// </summary>
-                public IStage Current {
-                        get {
-                                return _current;
-                        }
-                }
+		private IStage _current;
+		/// <summary>
+		/// Gets the current kernel stage.
+		/// </summary>
+		public IStage Current {
+			get {
+				return _current;
+			}
+		}
 
-                /// <summary>
-                /// Enqueues a stage.
-                /// </summary>
-                /// <param name="stage"></param>
-                public void Enqueue(IStage stage) {
-                        _initialize.Enqueue (stage);
-                }
+		/// <summary>
+		/// Enqueues a stage.
+		/// </summary>
+		/// <param name="stage"></param>
+		public void Enqueue(IStage stage) {
+			_initialize.Enqueue (stage);
+		}
 
-                /// <summary>
-                /// Runs the tasks.
-                /// </summary>
-                public void Run() {
-                        while (_initialize.Count != 0) {
-                                _current = _initialize.Dequeue ();
-                                _current.Initialize ();
-                                
-                                Console.Write ("Entered stage ");
-                                Console.Write (Current.Name);
-                                Console.WriteLine (".");
+		/// <summary>
+		/// Runs the tasks.
+		/// </summary>
+		public void Run() {
+			while (_initialize.Count != 0) {
+				_current = _initialize.Dequeue ();
+				_current.Initialize ();
 
-                                _teardown.Enqueue (_current);
-                        }
-                }
+				Console.Write ("Entered stage ");
+				Console.Write (Current.Name);
+				Console.WriteLine (".");
 
-                /// <summary>
-                /// Runs the teardown.
-                /// </summary>
-                public void Teardown() {
-                        while (_teardown.Count != 0) {
-                                _current = _teardown.Dequeue ();
-                                _current.Teardown ();
-                                Console.Write ("Left stage ");
-                                Console.Write (Current.Name);
-                                Console.WriteLine (".");
-                        }
-                }
-        }
+				_teardown.Push (Current);
+			}
+		}
+
+		/// <summary>
+		/// Runs the teardown.
+		/// </summary>
+		public void Teardown() {
+			while (_teardown.Count != 0) {
+				_current = _teardown.Pop ();
+				_current.Teardown ();
+				Console.Write ("Left stage ");
+				Console.Write (Current.Name);
+				Console.WriteLine (".");
+			}
+		}
+	}
 }
