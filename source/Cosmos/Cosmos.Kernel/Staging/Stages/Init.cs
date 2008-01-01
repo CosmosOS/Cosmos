73cd6daa8aaeafcd4fbfa708cff9bdade747aee4
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-01 12:51:31 +0000

    Made the Init stage.

---
+++ source/Cosmos/Cosmos.Kernel/Staging/Stages/Init.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Kernel.Staging.Stages {
+        public class Init : IStage {
+                #region IStage Members
+
+                public string Name {
+                        get {
+                                return "Init";
+                        }
+                }
+
+                public void Initialize() {
+                        CPU.Init ();
+                }
+
+                public void Teardown() {
+
+                }
+
+                #endregion
+        }
+}
