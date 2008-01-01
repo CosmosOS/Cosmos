06d24622703abac7f71807195c807547552dc772
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-01 13:34:47 +0000

    init broken

--- source/Cosmos/Cosmos.Shell.Console/Program.cs
+++ source/Cosmos/Cosmos.Shell.Console/Program.cs
@@ -5,9 +5,10 @@ using System.Text;
 namespace Cosmos.Shell.Console {
 	class Program {
 		static void Main() {
-			Kernel.CPU.Init();
+                        Kernel.Stages.Initialize ();
 			System.Console.WriteLine("Cosmos creation complete");
 			Kernel.Interrupts.DoTest();
+                        Kernel.Stages.Teardown ();
 		}
 	}
 }
