e277632e8da38726fba06388c287dfc000f8b994
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-01 17:27:23 +0000

    Moved Matthijs stuff into his own kernel stage.

--- source/Cosmos/Cosmos.Kernel/Staging/Stages/MatthijsStage.cs
+++ source/Cosmos/Cosmos.Kernel/Staging/Stages/MatthijsStage.cs
@@ -11,7 +11,59 @@ namespace Cosmos.Kernel.Staging.Stages {
 		}
 
 		public override void Initialize() {
-			CPU.TestATA ();
+			TestATA ();
+		}
+
+		public static unsafe void TestATA() {
+			Hardware.Storage.ATA.Initialize (CPU.Sleep);
+			Hardware.Storage.ATA xDrive = new Cosmos.Hardware.Storage.ATA (0, 0);
+			byte* xBuffer = (byte*) Heap.MemAlloc (512);
+			if (xDrive.ReadBlock (1, xBuffer)) {
+				Console.WriteLine ("Reading went fine");
+			} else {
+				Console.WriteLine ("Error reading");
+			}
+			FileSystem.Ext2 xExt2 = new Cosmos.Kernel.FileSystem.Ext2 (xDrive);
+			if (xExt2.Initialize ()) {
+				Console.WriteLine ("Ext2 Initialized");
+			} else {
+				Console.WriteLine ("Ext2 Initialization failed!");
+			}
+			byte[] xItem = xExt2.ReadFile (new string[] { "readme.txt" });
+			if (xItem == null) {
+				Console.WriteLine ("Couldn't read file!");
+				return;
+			}
+			Console.Write ("File length = ");
+			Hardware.Storage.ATAOld.WriteNumber ((uint) xItem.Length, 32);
+			Console.WriteLine (" bytes");
+			DebugUtil.SendByteStream ("CPU", "Readme.txt contents", xItem);
+			char[] xChars = new char[xItem.Length - 1];
+			for (int i = 0; i < xChars.Length; i++) {
+				xChars[i] = (char) xItem[i];
+			}
+			String s = new String (xChars);
+			Console.Write ("Contents: '");
+			Console.Write (s);
+			Console.WriteLine ("'");
+			Console.Write ("Contents of root (");
+			string[] xItems = xExt2.GetDirectoryEntries (new string[0]);
+			Hardware.Storage.ATAOld.WriteNumber ((uint) xItems.Length, 8);
+			Console.WriteLine (" items):");
+			DebugUtil.SendNumber ("CPU", "Dir items count", (uint) xItems.Length, 32);
+			if (xItems == null) {
+				Console.WriteLine ("    Result array is null!");
+			}
+			for (int i = 0; i < xItems.Length; i++) {
+				Console.Write ("   - ");
+				Console.Write (xItems[i]);
+				Console.Write (" [");
+				Hardware.Storage.ATAOld.WriteNumber ((uint) xItems[i][0], 16);
+				Console.Write ("] (Length = ");
+				System.Diagnostics.Debugger.Break ();
+				Hardware.Storage.ATAOld.WriteNumber ((uint) xItems[i].Length, 8);
+				Console.WriteLine (")");
+			}
 		}
 
 		public override void Teardown() {
