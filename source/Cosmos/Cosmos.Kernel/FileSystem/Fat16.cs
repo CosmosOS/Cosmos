722fc094fa9503668428bb0f3a7146ab14282ed1
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-02 16:15:23 +0000

    Added a well-defined filesystem interface. Started with FAT16.

---
+++ source/Cosmos/Cosmos.Kernel/FileSystem/Fat16.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Kernel.FileSystem
+{
+    public class Fat16 : FileSystem2
+    {
+        private struct Header
+        {
+            // Both FAT 16 and FAT 32
+            public ushort _jumpBoot; // 3
+            public byte[] _oemName; // 8
+            public ushort _bytesPerSector; // 2
+            public byte _sectorPerCluster; // 1
+            public ushort _reservedSectorCount; // 2
+            public byte _allocationTables; // 1
+            public ushort _rootEntryCount; // 2
+            public ushort _totalSectors; // 2
+            public byte _media; // 1
+            public ushort _fatSize; // 2
+            public ushort _sectorsPerTrack; // 2
+            public ushort _numberOfHeads; // 2
+            public ushort _hiddenSectors; // 4
+            public ushort _totalSectors32; // 4
+
+            #region FAT 16
+            public byte _driveNumber; // 1
+            public byte _reserved; // 1
+            public byte _bootSig; // 1
+            public ushort _volumeId; // 4
+            public byte[] _volumeLabel; // 11
+            public byte[] _fileSystemType; // 8
+            #endregion
+
+            // TODO: FAT32
+
+            /// <summary>
+            /// Reads the header from a byte source.
+            /// </summary>
+            /// <param name="source">The source data.</param>
+            public void Read(byte[] source)
+            {
+                int pos = 0;
+
+                _jumpBoot = ReadShort(source, pos, 0xFFF);
+                _oemName = ReadByte(source, pos, 8);
+            }
+
+            private ushort ReadShort(byte[] source, int pos, ushort mask)
+            {
+                ushort result = (ushort)(source[pos] >> 8);
+                result = (ushort)((result + source[pos + 1]) >> 8);
+                result = (ushort)((result + source[pos + 2]));
+                return (ushort)(result & mask);
+            }
+
+            private byte[] ReadByte(byte[] source, int pos, int count)
+            {
+                byte[] result = new byte[count];
+                for (int i = 0; i < count; i++)
+                    result[i] = source[i + pos];
+                return result;
+            }
+
+        }
+
+        public override File[] GetFiles(Path path)
+        {
+            return new File[0];
+        }
+
+        public override int ReadData(Path path, byte[] data, int start, int count)
+        {
+            return 0;
+        }
+
+        private string _label;
+        public override string Label
+        {
+            get
+            {
+                return _label;
+            }
+            set
+            {
+                _label = value;
+                WriteLabel();
+            }
+        }
+
+        private void WriteLabel()
+        {
+        }
+
+        public override void Open()
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
---
+++ source/Cosmos/Cosmos.Kernel/FileSystem/Path.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Kernel.FileSystem
+{
+    /// <summary>
+    /// Represents a path in a filesystem agnostic fashion.
+    /// </summary>
+    public class Path
+    {
+        private string[] _parts;
+        /// <summary>
+        /// Gets the steps.
+        /// </summary>
+        public string[] Parts
+        {
+            get
+            {
+                return _parts;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new path.
+        /// </summary>
+        /// <param name="args">Each step in getting to the file.</param>
+        public Path(params string[] parts)
+        {
+            _parts = parts;
+        }
+    }
+}
