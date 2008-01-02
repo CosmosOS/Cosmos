5b7bf857f209e72f8c169b8768cbeedc5947fd39
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-02 10:59:07 +0000

    Preliminary work on the log viewer.

---
+++ source/Cosmos.Kernel.LogTail/Handlers/MessageLogHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cosmos.Kernel.LogTail.Handlers
+{
+    public partial class MessageLogHandler : LogHandler
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Messages";
+            }
+        }
+
+        public MessageLogHandler()
+        {
+            InitializeComponent();
+        }
+
+        public override void HandleMessage(LogMessage message)
+        {
+            // We only handle these.
+            if (message.Name != "Warning" && message.Name != "Error")
+                return;
+
+            // Create the item.
+            ListViewItem item = new ListViewItem(new string[] { message["Module"], message["String"] });
+            item.ImageKey = message.Name.ToLowerInvariant();
+
+            // Add it.
+            listView.Items.Add(item);
+        }
+    }
+}
---
+++ source/Cosmos.Kernel.LogTail/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cosmos.Kernel.LogTail
+{
+    public partial class MainForm : Form
+    {
+        private Handlers.LogHandler[] _handlers;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            BuildTabs();
+        }
+
+        private void BuildTabs()
+        {
+            _handlers = Handlers.LogHandler.GetHandlers();
+            foreach (Handlers.LogHandler handler in _handlers)
+            {
+                TabPage page = new TabPage();
+                page.Text = handler.Title;
+                page.Controls.Add(handler);
+                handler.Dock = DockStyle.Fill;
+                tabControl.TabPages.Add(page);
+            }
+        }
+    }
+}
