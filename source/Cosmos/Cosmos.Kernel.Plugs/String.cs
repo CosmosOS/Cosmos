8e5e6a027a8c8d0a2020a68eeda7bb31b227fc6a
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-02 07:55:42 +0000

    Made the string plug, but it doesn't work. Got the prompter class seperating out spaces.

---
+++ source/Cosmos/Cosmos.Kernel.Plugs/String.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Indy.IL2CPU.Plugs;
+
+namespace Cosmos.Kernel.Plugs
+{
+    [Plug(Target=typeof(System.String))]
+    public class String
+    {
+        /*public int IndexOf(char c)
+        {
+            // TODO: We can't get 'this'
+            //string me = ToString();
+            //for (int i = 0; i < me.Length; i++)
+            //{
+            //    if (me[i] == c)
+            //        return i;
+            //}
+            return -1;
+        }*/
+    }
+}
--- source/Cosmos/Cosmos.Shell.Console/Prompter.cs
+++ source/Cosmos/Cosmos.Shell.Console/Prompter.cs
@@ -16,15 +16,47 @@ namespace Cosmos.Shell.Console {
 
 		private List<Commands.CommandBase> _commands;
 
-		public override void Initialize() {
+        private bool running = true;
 
-			while (true) {
-				string a = System.Console.ReadLine ();
-				System.Console.WriteLine (a);
-			}
+        public void Stop()
+        {
+            running = false;
+        }
+
+		public override void Initialize() {
+            while (running)
+            {
+                string line = System.Console.ReadLine();
+                int index = IndexOf(line, ' ') + 1;
+                string command = Substring(line, index);
+                System.Console.WriteLine(command);
+            }
 		}
 
-		public override void Teardown() {
+        private int IndexOf(string source, char look)
+        {
+            for (int i = 0; i < source.Length; i++)
+                if (source[i] == look)
+                    return i;
+            return -1;
+        }
+
+        private string Substring(string source, int index)
+        {
+            List<Char> target = new List<char>();
+            for (int i = index; i < source.Length; i++)
+                target.Add(source[i]);
+
+            // HACK: Should use .ToArray here.
+            char[] final = new char[target.Count];
+            for (int i = 0; i < final.Length; i++)
+                final[i] = target[i];
+
+            return new string(final);
+        }
+
+        public override void Teardown()
+        {
 			
 		}
 	}
