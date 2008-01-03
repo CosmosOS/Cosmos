db9619497ac9f2ed86039f3793e666af48395750
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-03 06:08:31 +0000

    Undid breaking changes.

--- source/Cosmos/Cosmos.Kernel/CPU.cs
+++ source/Cosmos/Cosmos.Kernel/CPU.cs
@@ -22,8 +22,8 @@ namespace Cosmos.Kernel {
 			Hardware.PIC.Init();
 			Console.WriteLine("Done");
 			Console.Write("Initializing Serial 0...");
-			//Hardware.Serial.InitSerial(0);
-			Console.WriteLine("Serial Bypassed.");
+			Hardware.Serial.InitSerial(0);
+			Console.WriteLine("Done");
 			Console.Write("Initializing Debug Utility...");
 			Hardware.DebugUtil.Initialize();
 			Hardware.DebugUtil.SendMessage("Logging", "Initialized!");
@@ -34,7 +34,7 @@ namespace Cosmos.Kernel {
 			Console.Write("Creating IDT...");
 			Hardware.CPU.CreateIDT();
 			Console.WriteLine("Done");
-			//Keyboard.Initialize ();
+			Keyboard.Initialize ();
 			//TestATA();
 		}
 
