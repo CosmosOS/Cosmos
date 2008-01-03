fb65032168f9dd523e67a647ea6e50667168e1c7
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2008-01-03 14:16:33 +0000

    Works again

--- Build/Cosmos/PS1.bat
+++ Build/Cosmos/PS1.bat
@@ -3,4 +3,5 @@ echo You must first run Set-ExecutionPolicy remotesigned
 echo You may also have to unblock this file using file, properties in explorer
 echo -
 
-powershell -command %1
+powershell -command "%1"
+pause
