f2de6510ec9be403baf1bcc7a25d5f8811140471
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2008-01-09 17:03:06 +0000

    Strong names

--- source/Cosmos/Cosmos.Shell.Console/Program.cs
+++ source/Cosmos/Cosmos.Shell.Console/Program.cs
@@ -5,12 +5,18 @@ using Cosmos.Build.Windows;
 
 namespace Cosmos.Shell.Console {
 	public class Program {
+
+        #region Build Console
+        // This contains code to launch the build console. Most users should not chagne this.
         [STAThread]
         public static void Main() {
 			var xBuilder = new Builder();
 			xBuilder.Build();
         }
-        
+        #endregion
+
+        // Here is where your Cosmos code goes. This is the code that will be executed during Cosmos boot.
+        // Write your code, and run. Cosmos build console will appear, select your target, and thats it!
         public static void Init() {
 			Kernel.CPU.Init();
 
