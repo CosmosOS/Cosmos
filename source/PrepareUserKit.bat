57334fa0aa6c7ac96b553eaf14f9fabd1ee02a01
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2008-01-10 14:34:51 +0000

    PrepareUserKit.bat improved

--- source/PrepareUserKit.bat
+++ source/PrepareUserKit.bat
@@ -1,7 +1,13 @@
 @echo off
 mkdir ..\Build\GAC
 copy ".\Cosmos\Cosmos.Shell.Console\bin\Debug\*.dll" "..\Build\GAC\" /Y
-del "..\Build\Config\*.pdb"
-del "..\Build\Config\*.vshost.exe"
-"..\Tools\7zip\7z.exe" a -t7z -sfx Test.exe "..\Build\*.*" -r
+del /Q "..\Build\Config\*.pdb"
+del /Q "..\Build\Config\*.vshost.exe"
+del /Q "..\Build\Cosmos.iso"
+del /Q "..\Build\output.bin"
+del /Q "..\Build\setup.pdb"
+del /Q "..\Build\ISO\output.bin"
+del /Q "..\Build\Tools\asm\*.*"
+del /Q "..\Build\Tools\Cosmos.Kernel.Plugs\*.pdb"
+"..\Tools\7zip\7z.exe" a -t7z -mx=9 -sfx Test.exe "..\Build\*.*" -r
 pause
