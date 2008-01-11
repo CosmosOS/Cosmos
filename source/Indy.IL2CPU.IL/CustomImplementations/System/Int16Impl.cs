85a9ac346f764a85219764d60d2d850202cca490
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-11 10:42:26 +0000

    Int16 Parsing

--- source/Indy.IL2CPU.IL/CustomImplementations/System/Int16Impl.cs
+++ source/Indy.IL2CPU.IL/CustomImplementations/System/Int16Impl.cs
@@ -18,5 +18,19 @@ namespace Indy.IL2CPU.IL.CustomImplementations.System {
 			xResult[5] = xDigits[aThis & 0xF];
 			return new String(xResult);
 		}
-	}
+
+        public static Int16 Parse(string s)
+        {
+            const string digits = "0123456789";
+            Int16 result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Int16 ind = (Int16)digits.IndexOf(digits[i]);
+                result = (result * 10) + ind;
+            }
+
+            return result;
+        }
+    }
 }
