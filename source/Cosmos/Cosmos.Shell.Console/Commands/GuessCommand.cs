664cb9452dd557c7205b3b38c0dacec5833a1baf
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-13 15:02:33 +0000

    Made a Guess game, which doesn't work. Int16.Parse seems to be broken (due to an IndexOf problem I think).

---
+++ source/Cosmos/Cosmos.Shell.Console/Commands/GuessCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.Shell.Console.Commands
+{
+    public class GuessCommand : CommandBase
+    {
+        public override string Name
+        {
+            get { return "guess"; }
+        }
+
+        public override string Summary
+        {
+            get { return "Plays a game of guess."; }
+        }
+
+        public override void Execute(string param)
+        {
+            short num = 5; // TODO: Random Number Generator
+            short guess = short.Parse(System.Console.ReadLine());
+
+            while (guess != num)
+            {
+                if (guess > num)
+                    System.Console.WriteLine("Too High.");
+                else
+                    System.Console.WriteLine("Too Low.");
+                guess = short.Parse(System.Console.ReadLine());
+            }
+
+            System.Console.WriteLine("You Got It!!!!");
+        }
+
+        public override void Help()
+        {
+            System.Console.WriteLine(Name);
+            System.Console.Write("  "); System.Console.WriteLine(Summary);
+        }
+    }
+}
