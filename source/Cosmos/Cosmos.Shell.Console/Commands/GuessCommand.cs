d0cfd9e4ef64d34af264f1aee0bb0a7731676c0c
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-13 18:26:34 +0000

    Made a MT RNG (well, copied code). Doesn't work. Needs to be moved into the System.Random class, but I'm not sure how.

--- source/Cosmos/Cosmos.Shell.Console/Commands/GuessCommand.cs
+++ source/Cosmos/Cosmos.Shell.Console/Commands/GuessCommand.cs
@@ -19,15 +19,32 @@ namespace Cosmos.Shell.Console.Commands
 
         public override void Execute(string param)
         {
-            short num = 5;
+            MT mt = new MT();
+            int n = mt.Next();
+            if (n <= 0)
+                n = -n;
+
+            n /= (Int32.MaxValue / Int16.MaxValue);
+            n /= (Int16.MaxValue / 20);
+
+            System.Console.WriteLine("I am thinking of a number between 0 and 20. What is it?");
+            short num = (short)n;
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.Write("Take a guess: ");
+            System.Console.ForegroundColor = ConsoleColor.White;
             short guess = short.Parse(System.Console.ReadLine());
 
             while (guess != num)
             {
+                System.Console.ForegroundColor = ConsoleColor.Red;
                 if (guess > num)
                     System.Console.WriteLine("Too high.");
                 else
                     System.Console.WriteLine("Too low.");
+
+                System.Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.Write("Take another guess: ");
+                System.Console.ForegroundColor = ConsoleColor.White;
                 guess = short.Parse(System.Console.ReadLine());
             }
 
