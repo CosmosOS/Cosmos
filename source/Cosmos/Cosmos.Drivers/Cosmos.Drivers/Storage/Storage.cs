4fa997e2b325793caa2cacd88165f161e4e9435e
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-17 08:49:27 +0000

    Started preliminary work on the object framework.

---
+++ source/Cosmos/Cosmos.Drivers/Cosmos.Drivers/Storage/Storage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.Drivers.Storage
+{
+    /// <summary>
+    /// Represents a file system.
+    /// </summary>
+    public abstract class Storage : Driver
+    {
+    }
+}
