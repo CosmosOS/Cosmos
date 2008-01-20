bf8b2a2225bd613f33ac97413ae6c70c0026afa3
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-20 19:54:31 +0000

    Started guess game.

---
+++ source/Cosmos.Shell.Guess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Shell.Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
