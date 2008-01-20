686478b2bf78449e75aa74ab9022a4dfe679fcd7
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-20 20:14:20 +0000

    Guess shell complete. Just don't use the RNG for RSA or anything :).

--- source/Cosmos.Shell.Guess/Program.cs
+++ source/Cosmos.Shell.Guess/Program.cs
@@ -1,13 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Cosmos.Build.Windows;
 
 namespace Cosmos.Shell.Guess
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        #region Build Console
+        // This contains code to launch the build console. Most users should not chagne this.
+        [STAThread]
+        public static void Main()
         {
+            var xBuilder = new Builder();
+            xBuilder.Build();
+        }
+        #endregion
+
+        // Here is where your Cosmos code goes. This is the code that will be executed during Cosmos boot.
+        // Write your code, and run. Cosmos build console will appear, select your target, and thats it!
+        public static void Init()
+        {
+            Kernel.CPU.Init();
+
+            Kernel.Staging.DefaultStageQueue stages = new Cosmos.Kernel.Staging.DefaultStageQueue();
+            
+            System.Console.Clear();
+            System.Console.BackgroundColor = ConsoleColor.Black;
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine("Cosmos Kernel. Copyright 2007-2008 The Cosmos Project.");
+            System.Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Now Booting...");
+
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("Success.");
+            Kernel.CPU.PrintTime();
+
+
+            stages.Run();
+
+            Random mt = new Random();
+            short num = mt.Next();
+
+            System.Console.WriteLine("I am thinking of a number between 0 and 255. What is it?");
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.Write("Take a guess: ");
+            System.Console.ForegroundColor = ConsoleColor.White;
+            short guess = short.Parse(System.Console.ReadLine());
+
+            while (guess != num)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                if (guess > num)
+                    System.Console.WriteLine("Too high.");
+                else
+                    System.Console.WriteLine("Too low.");
+
+                System.Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.Write("Take another guess: ");
+                System.Console.ForegroundColor = ConsoleColor.White;
+                guess = short.Parse(System.Console.ReadLine());
+            }
+
+            System.Console.WriteLine("You got it!!!!");
+
+
+            System.Console.WriteLine("Done");
+
+            stages.Teardown();
         }
     }
 }
---
+++ source/Cosmos.Shell.Guess/Random.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.Shell.Guess
+{
+    /// <summary>
+    /// Uses nasty security holed pointer logic to get the next
+    /// value.
+    /// </summary>
+    public unsafe class Random
+    {
+        private byte* val;
+
+        public Random()
+        {
+            int max = Hardware.RTC.GetHours();
+            max *= (int)Hardware.RTC.GetMinutes();
+            max *= (int)Hardware.RTC.GetSeconds();
+            for (int i = 0; i < max; i++)
+                Next();
+            val += (byte)max;
+        }
+
+        public byte Next()
+        {
+            byte nxt = *val;
+            val = (byte*)nxt;
+            return nxt;
+        }
+    }
+}
