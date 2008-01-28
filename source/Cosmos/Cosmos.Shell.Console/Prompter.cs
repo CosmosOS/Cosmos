111a599db117e54abacdd7a8a3a29646eaf4dd2b
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-28 13:57:03 +0000

    Removed Guess, use the Guess kernel instead.

--- source/Cosmos/Cosmos.Shell.Console/Prompter.cs
+++ source/Cosmos/Cosmos.Shell.Console/Prompter.cs
@@ -29,7 +29,6 @@ namespace Cosmos.Shell.Console {
 			_commands.Add(new Commands.EchoCommand());
 			_commands.Add(new Commands.ExitCommand(Stop)); // TODO: Fix this.
             _commands.Add(new Commands.FailCommand());
-			_commands.Add(new Commands.GuessCommand());
 			_commands.Add(new Commands.HelpCommand(_commands));
 			_commands.Add(new Commands.TestsCommand());
 			_commands.Add(new Commands.TypeCommand());
