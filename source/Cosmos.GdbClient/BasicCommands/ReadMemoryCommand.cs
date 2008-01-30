48ebc0bf7b2a1e6ebf15efaa84e7701360b6da59
moitoius_cp <7bd20ad30720b36bd251fb928c419f8754d57bfcCkJyHhZD>
2008-01-30 16:52:30 +0000

    Added the readmemory command.

---
+++ source/Cosmos.GdbClient/BasicCommands/ReadMemoryCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.GdbClient.BasicCommands
+{
+    public class ReadMemoryCommand : CommandBase<byte[]>
+    {
+        public ReadMemoryCommand() : base(GdbController.Instance) { }
+
+        protected override void Execute()
+        {
+        }
+    }
+}
