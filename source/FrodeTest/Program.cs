16c5fef50e15be9f039552c85c79d09437899442
Scalpel_cp <165da7fc5536ee16440a98f161bfa866a8b94595G55xazV5>
2008-03-08 21:33:10 +0000

    Testproject created for Frode "Scalpel" Lillerud

---
+++ source/FrodeTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cosmos.Build.Windows;
+
+namespace FrodeTest
+{
+    class Program
+    {
+        #region Cosmos Builder logic
+        // Most users wont touch this. This will call the Cosmos Build tool
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var xBuilder = new Builder();
+            xBuilder.Build();
+        }
+        #endregion
+
+        // Main entry point of the kernel
+        public static void Init()
+        {
+            Cosmos.Kernel.CPU.Init();
+            Console.WriteLine("Done booting");
+            while (true)
+                ;
+        }
+    }
+}
