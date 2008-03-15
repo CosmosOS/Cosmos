e6e66bed3a8d211f4fb159ef72b8e81a24384dd2
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2008-03-15 13:21:16 +0000

    Base harness for filesystems

---
+++ source/Cosmos.Kernel.FileSystems/BaseFileSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cosmos.Kernel.FileSystems {
+	public abstract class BaseFileSystem {
+		protected readonly Hardware.BlockDevice mBackend;
+		protected BaseFileSystem(Hardware.BlockDevice aBackend) {
+			if (aBackend == null) {
+				throw new ArgumentNullException("aBackend");
+			}
+			mBackend = aBackend;
+		}
+
+		public abstract ulong Read(string[] aFile, ulong aStart, ulong aCount, byte[] aBuffer);
+		public abstract ulong Write(string[] aFile, ulong aStart, ulong aCount, byte[] aBuffer);
+		public abstract void DeleteFile(string[] aFile);
+		public abstract void CreateFile(string[] aFile);
+		public abstract void MoveFile(string[] aSource, string[] aDest);
+	}
+}
