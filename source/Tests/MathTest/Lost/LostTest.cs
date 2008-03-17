daa82ebf980d1057439499d4822ee4dd93759424
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-03-17 11:03:01 +0000

    I've added my own test project.

---
+++ source/Tests/MathTest/Lost/LostTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using Cosmos.Build.Windows;
+
+namespace Lost
+{
+    class LostTest
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
