2dd48582180e4206c3c72b4077e7d289913572da
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-09 11:15:41 +0000

    [+] Added je instruction.

---
+++ source/Tests/MathTest/Lost/JIT/AMD64/JumpIfEqual.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lost.JIT.AMD64
+{
+	[Serializable]
+	public sealed class JumpIfEqual: ConditionalJumpInstruction
+	{
+		public JumpIfEqual(int targetOffset) : base(targetOffset) { }
+
+		protected override int OpcodeBase
+		{
+			get { return 0x74; }
+		}
+	}
+}
