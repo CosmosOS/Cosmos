d08636d0bd1ddcfb731af541ec7f1c9d6d94ca8f
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-09 11:17:47 +0000

    [+] Added break-like instrcution. (int 3)

---
+++ source/Tests/MathTest/Lost/JIT/AMD64/BreakPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lost.JIT.AMD64
+{
+	[Serializable]
+	public sealed class BreakPoint: ProcessorInstruction
+	{
+		public override int? Size
+		{
+			get { return 1; }
+		}
+
+		public override void Compile(Stream destStream)
+		{
+			destStream.WriteByte(0xCC);
+		}
+	}
+}
