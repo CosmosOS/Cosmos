ab9b804e9e86c2a724544f6846cc3b3a563fc2c0
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-09 11:17:21 +0000

    [+] Added add instruction.

---
+++ source/Tests/MathTest/Lost/JIT/AMD64/And.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lost.JIT.AMD64
+{
+	[Serializable]
+	public sealed class And : DestSourceInstruction
+	{
+		public And(InstructionOperand dest, InstructionOperand source)
+			: base(dest, source)
+		{
+		}
+		public And(InstructionOperand dest, byte source)
+			: base(dest, source)
+		{
+		}
+		public And(InstructionOperand dest, short source)
+			: base(dest, source)
+		{
+		}
+		public And(InstructionOperand dest, int source)
+			: base(dest, source)
+		{
+		}
+		public And(InstructionOperand dest, long source)
+			: base(dest, source)
+		{
+		}
+
+		public override byte AccumulatorOpCode
+		{
+			get
+			{
+				return 0x24;
+			}
+		}
+		public override byte ImmediateExt
+		{
+			get { return 0x04; }
+		}
+		public override byte ImmediateOpCode
+		{
+			get { return 0x80; }
+		}
+		public override byte RegisterOpCode
+		{
+			get { return 0x20; }
+		}
+	}
+}
