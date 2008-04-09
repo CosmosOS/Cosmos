d7a08b3fadde6ccee50ead7edbe4729c5c6c11bf
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-09 11:18:44 +0000

    [+] Conditional Jump instruction base handler.

---
+++ source/Tests/MathTest/Lost/JIT/AMD64/ConditionalJumpInstruction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lost.JIT.AMD64
+{
+	[Serializable]
+	public abstract class ConditionalJumpInstruction: ProcessorInstruction
+	{
+		public ConditionalJumpInstruction(int targetOffset)
+		{
+			TargetOffset = targetOffset;
+		}
+
+		public int TargetOffset { get; set; }
+
+		public override int? Size
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override void Compile(Stream destStream)
+		{
+			if (TargetOffset.FitsInByte())
+			{
+				destStream.WriteByte(OpcodeBase);
+				destStream.WriteSByte(TargetOffset);
+				return;
+			} else
+			{
+				destStream.WriteByte(0x0F);
+				destStream.WriteByte(OpcodeBase + 0x10);
+				destStream.WriteInt(TargetOffset);
+			}
+		}
+
+		protected abstract int OpcodeBase { get; }
+	}
+}
