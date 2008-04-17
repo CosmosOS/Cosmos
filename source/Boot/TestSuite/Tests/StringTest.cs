2210f66a8c63c82e7f5c814eea7028d239ab7421
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-17 07:43:57 +0000

    [+] LastIndexOf checking in TestSuite.

--- source/Boot/TestSuite/Tests/StringTest.cs
+++ source/Boot/TestSuite/Tests/StringTest.cs
@@ -24,6 +24,7 @@ namespace TestSuite.Tests
         public override void Test()
         {
             Assert("ABCDEFG".IndexOf('E') == 4, "\"ABCDEFG\".IndexOf('E') == 4");
+			Assert("ABCDEFG".LastIndexOf('E') == 4, "\"ABCDEFG\".IndexOf('E') == 4");
             Assert("0123456789".IndexOf('5') == 5, "\"0123456789\".IndexOf('5') == 5");
             Assert("0123456789"[5] == '5', "\"0123456789\"[5] == '5'");
             Assert("a" != "b", "\"a\" != \"b\"");
