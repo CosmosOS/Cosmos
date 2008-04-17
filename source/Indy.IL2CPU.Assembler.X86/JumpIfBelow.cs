16604e73877caff7104eafdd380e2a6141862286
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-17 23:20:12 +0000

    [+] Blt_Un now works with 64bit operands. [+] JumpIfBelow to support this.

---
+++ source/Indy.IL2CPU.Assembler.X86/JumpIfBelow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indy.IL2CPU.Assembler.X86
+{
+	/// <summary>
+	/// CMP DEST, SOURCE
+	/// if (DEST &lt; SOURCE) jump (unsigned)
+	/// </summary>
+	[OpCode(0xFFFFFFFF, "jb")]
+	public class JumpIfBelow : JumpBase
+	{
+		public JumpIfBelow(string aAddress)
+			: base(aAddress)
+		{
+		}
+		public override string ToString()
+		{
+			return "jb " + Address;
+		}
+	}
+}
