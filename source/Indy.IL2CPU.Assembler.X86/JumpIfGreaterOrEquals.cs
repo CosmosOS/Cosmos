8991489489e74423f634df543c2dba47d307d7af
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-17 22:26:16 +0000

    [!] JumpIfGreaterOrEqual produces the same opcode as JumpIfGreaterOrEqual, but it's opcode in attribute is jbe.

--- source/Indy.IL2CPU.Assembler.X86/JumpIfGreaterOrEquals.cs
+++ source/Indy.IL2CPU.Assembler.X86/JumpIfGreaterOrEquals.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 
 namespace Indy.IL2CPU.Assembler.X86 {
+	[Obsolete("This instruction is a copy of JumpIfGreaterOrEqual, and has wrong opcode in attribute")]
 	[OpCode(0xFFFFFFFF, "jbe")]
 	public class JumpIfGreaterOrEquals: JumpBase {
 		public JumpIfGreaterOrEquals(string aAddress)
