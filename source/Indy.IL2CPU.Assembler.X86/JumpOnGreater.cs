45377fd9e6ed4226d75453fc7c66b215534f475a
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-17 22:27:12 +0000

    [+] JumpOnGreater class created to replace strange class JumpIfGreater.

---
+++ source/Indy.IL2CPU.Assembler.X86/JumpOnGreater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indy.IL2CPU.Assembler.X86
+{
+	/// <summary>
+	/// CMP DEST, SOURCE
+	/// if (DEST > SOURCE) jump (signed)
+	/// </summary>
+	[OpCode(0xFFFFFFFF, "jg")]
+	[Obsolete("This instruction's generated asm code differs from it's name. Use JumpOnGreater instead.")]
+	public class JumpOnGreater : JumpBase
+	{
+		public JumpOnGreater(string aAddress)
+			: base(aAddress)
+		{
+		}
+		public override string ToString()
+		{
+			return "jg " + Address;
+		}
+	}
+}
