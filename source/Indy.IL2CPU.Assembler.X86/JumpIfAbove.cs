436762e62f13cabeebe953e45ffab8ed0bddacb2
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-17 22:40:06 +0000

    [+] JumpIfAbove instruction.

---
+++ source/Indy.IL2CPU.Assembler.X86/JumpIfAbove.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indy.IL2CPU.Assembler.X86
+{
+	/// <summary>
+	/// CMP DEST, SOURCE
+	/// if (DEST > SOURCE) jump (unsigned)
+	/// </summary>
+	[OpCode(0xFFFFFFFF, "ja")]
+	public class JumpIfAbove : JumpBase
+	{
+		public JumpIfAbove(string aAddress)
+			: base(aAddress)
+		{
+		}
+		public override string ToString()
+		{
+			return "ja " + Address;
+		}
+	}
+}
