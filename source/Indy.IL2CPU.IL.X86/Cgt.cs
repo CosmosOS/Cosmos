175ad54c3f6a5e9f334b74652a28a31137f74565
LostTheBlack_cp <ab7a9e09ed51487c5dcf3d095965e66df40e33b6DwtEdTHV>
2008-04-18 12:09:13 +0000

    [+] Cgt & Cgt_Un both should work with 64bit operands now.

--- source/Indy.IL2CPU.IL.X86/Cgt.cs
+++ source/Indy.IL2CPU.IL.X86/Cgt.cs
@@ -5,6 +5,7 @@ using System.IO;
 using CPUx86 = Indy.IL2CPU.Assembler.X86;
 using CPU = Indy.IL2CPU.Assembler;
 using Indy.IL2CPU.Assembler;
+using Indy.IL2CPU.Assembler.X86;
 
 namespace Indy.IL2CPU.IL.X86 {
 	[OpCode(OpCodeEnum.Cgt)]
@@ -28,27 +29,45 @@ namespace Indy.IL2CPU.IL.X86 {
 			string BaseLabel = CurInstructionLabel + "__";
 			string LabelTrue = BaseLabel + "True";
 			string LabelFalse = BaseLabel + "False";
-			new CPUx86.Pop(CPUx86.Registers.EAX);
-			if (xStackItem.Size > 4) {
-				new CPUx86.Add("esp", "4");
+			if (xStackItem.Size > 4)
+			{
+				new CPUx86.Xor("esi", "esi");
+				new CPUx86.Add("esi", "1");
+				new CPUx86.Xor("edi", "edi");
+				//esi = 1
+				new CPUx86.Pop(CPUx86.Registers.EAX);
+				new CPUx86.Pop(CPUx86.Registers.EDX);
+				//value2: EDX:EAX
+				new CPUx86.Pop("ebx");
+				new CPUx86.Pop("ecx");
+				//value1: ECX:EBX
+				new CPUx86.Sub("ebx", "eax");
+				new CPUx86.SubWithCarry("ecx", "edx");
+				//result = value1 - value2
+				new CPUx86.ConditionalMove(Condition.Greater, "edi", "esi");
+				new CPUx86.Push("edi");
+
+				//new CPUx86.JumpOnGreater(LabelTrue);
+				//new CPUx86.Push("00h");
+				//new CPUx86.JumpAlways(NextInstructionLabel);
+				//new CPU.Label(LabelTrue);
+				//new CPUx86.Push("01h");
+
+			} else
+			{
+				new CPUx86.Pop(CPUx86.Registers.EAX);
+				new CPUx86.Compare(CPUx86.Registers.EAX, CPUx86.Registers.AtESP);
+				new CPUx86.JumpIfGreater(LabelTrue);
+				new CPUx86.JumpAlways(LabelFalse);
+				new CPU.Label(LabelTrue);
+				new CPUx86.Add(CPUx86.Registers.ESP, "4");
+				new CPUx86.Push("01h");
+				new CPUx86.JumpAlways(NextInstructionLabel);
+				new CPU.Label(LabelFalse);
+				new CPUx86.Add(CPUx86.Registers.ESP, "4");
+				new CPUx86.Push("00h");
+				new CPUx86.JumpAlways(NextInstructionLabel);
 			}
-			new CPUx86.Compare(CPUx86.Registers.EAX, CPUx86.Registers.AtESP);
-			new CPUx86.JumpIfGreater(LabelTrue);
-			new CPUx86.JumpAlways(LabelFalse);
-			new CPU.Label(LabelTrue);
-			new CPUx86.Add(CPUx86.Registers.ESP, "4");
-			if (xStackItem.Size > 4) {
-				new CPUx86.Add("esp", "4");
-			}
-			new CPUx86.Push("01h");
-			new CPUx86.JumpAlways(NextInstructionLabel);
-			new CPU.Label(LabelFalse);
-			new CPUx86.Add(CPUx86.Registers.ESP, "4");
-			if (xStackItem.Size > 4) {
-				new CPUx86.Add("esp", "4");
-			}
-			new CPUx86.Push("00h");
-			new CPUx86.JumpAlways(NextInstructionLabel);
 		}
 	}
 }
