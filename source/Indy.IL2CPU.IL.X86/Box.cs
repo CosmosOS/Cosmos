2373fb1a719649ae5ca03e53d9f8c1eeb24e3553
smremde_cp <2296c648a83df9531cb573d8dfdead9aa751ab92VfNzPxkR>
2008-06-11 17:05:03 +0000

    fixed boxing bug

--- source/Indy.IL2CPU.IL.X86/Box.cs
+++ source/Indy.IL2CPU.IL.X86/Box.cs
@@ -35,16 +35,21 @@ namespace Indy.IL2CPU.IL.X86 {
 			if (mTheSize % 4 != 0) {
 				xSize += 4 - (mTheSize % 4);
 			}
-			new CPUx86.Pushd("0x" + (ObjectImpl.FieldDataOffset + xSize).ToString("X").ToUpper());
-			new CPUx86.Call(CPU.Label.GenerateLabelName(RuntimeEngineRefs.Heap_AllocNewObjectRef));
-			new CPUx86.Move("dword", CPUx86.Registers.AtESP, "0x" + mTypeId.ToString("X"));
-			new CPUx86.Move("dword", "[esp + 4]", "0x" + InstanceTypeEnum.BoxedValueType.ToString("X"));
-			for (int i = 0; i < (xSize / 4); i++) {
-				new CPUx86.Pop(CPUx86.Registers.EDX);
-                new CPUx86.Move("dword", "[esp + 0x" + (ObjectImpl.FieldDataOffset + (i * 4)).ToString("X") + "]", "edx");
-			}
-			Assembler.StackContents.Pop();
-			Assembler.StackContents.Push(new StackContent(4, false, false, false));
+            new CPUx86.Pushd("0x" + (ObjectImpl.FieldDataOffset + xSize).ToString("X").ToUpper());
+            new CPUx86.Call(CPU.Label.GenerateLabelName(RuntimeEngineRefs.Heap_AllocNewObjectRef));
+            new CPUx86.Pop("eax");
+            new CPUx86.Move("eax", "[esp]");
+            new CPUx86.Move("dword", "[eax]", "0x" + mTypeId.ToString("X"));
+            new CPUx86.Move("dword", "[eax + 4]", "0x" + InstanceTypeEnum.BoxedValueType.ToString("X"));
+            new CPU.Comment("xSize is " + xSize);
+            for (int i = 0; i < (xSize / 4); i++)
+            {
+                new CPUx86.Pop(CPUx86.Registers.EDX);
+                new CPUx86.Move("dword", "[eax + 0x" + (ObjectImpl.FieldDataOffset + (i * 4)).ToString("X") + "]", "edx");
+            }
+            new CPUx86.Push("eax");
+            Assembler.StackContents.Pop();
+            Assembler.StackContents.Push(new StackContent(4, false, false, false));
 		} 
 	}
 }
