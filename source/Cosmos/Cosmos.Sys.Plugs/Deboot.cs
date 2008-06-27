2fa6ba535f7a376d399a97b859df41ed66857189
ralfkronemeyer_cp <cc1a8f1172ffa4f8c67285c38f60ca9d52c2d535En6tEjS2>
2008-06-27 13:04:46 +0000

    Added Cosmos.Sys.Plugs with empty Deboot Plug

---
+++ source/Cosmos/Cosmos.Sys.Plugs/Deboot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Indy.IL2CPU.Plugs;
+
+namespace Cosmos.Sys.Plugs
+{
+    [Plug(Target = typeof(Sys.Deboot))]
+    public class Deboot
+    {
+    }
+}
