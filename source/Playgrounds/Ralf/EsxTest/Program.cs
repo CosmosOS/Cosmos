288fd27f61acfb096c2966884e205c7166294a50
ralfkronemeyer_cp <cc1a8f1172ffa4f8c67285c38f60ca9d52c2d535En6tEjS2>
2008-06-27 07:32:38 +0000

    Added Playgrounds\Ralf\EsxTest

---
+++ source/Playgrounds/Ralf/EsxTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Cosmos.Build.Windows;
+
+namespace EsxTest
+{
+    class Program
+    {
+        #region Cosmos Builder logic
+        // Most users wont touch this. This will call the Cosmos Build tool
+        [STAThread]
+        static void Main(string[] args)
+        {
+            BuildUI.Run();
+        }
+
+        #endregion
+
+        // Main entry point of the kernel
+        public static void Init()
+        {
+            Cosmos.Sys.Boot.Default();
+            Console.WriteLine("Sleep ESX!");
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
