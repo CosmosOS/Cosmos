46fe3738abec23a702f939af03c9291df75c30a7
cipher_cp <821fea98d6bc9380c58b8952f0c812fba5990545qNDTEvJk>
2008-06-28 15:36:07 +0000

    Test project for me and reboot plug

---
+++ source/CipherTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cosmos.Build.Windows;
+
+namespace CipherTest
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// This is here to run it on Windows and see results when necessary
+			// Then can be run on Cosmos to see if values are the same
+			//RTL8139.CreateTestFrame();
+			BuildUI.Run();
+		}
+
+		public static void Init()
+		{
+			Cosmos.Sys.Boot.Default();
+			Console.WriteLine("Boot complete");
+			if (Console.ReadLine() == "r")
+			{
+				Console.WriteLine("Rebooting...");
+				Cosmos.Sys.Deboot.Reboot();
+			}
+			else
+				Cosmos.Sys.Deboot.Halt();
+		}
+	}
+}
--- source/Cosmos/Cosmos.Sys.Plugs/Assemblers/Reboot.cs
+++ source/Cosmos/Cosmos.Sys.Plugs/Assemblers/Reboot.cs
@@ -2,6 +2,7 @@
 using Assembler=Indy.IL2CPU.Assembler.Assembler;
 using CPUAll = Indy.IL2CPU.Assembler;
 using CPUx86 = Indy.IL2CPU.Assembler.X86;
+using Indy.IL2CPU.Assembler.X86;
 
 namespace Cosmos.Sys.Plugs.Assemblers
 {
@@ -9,8 +10,22 @@ namespace Cosmos.Sys.Plugs.Assemblers
     {
         public override void Assemble(Assembler aAssembler)
         {
-            //TODO: Implement asm reboot, For now we just call Halt
-            new CPUAll.Label(".loop");
+        	new CPUx86.Cli();
+        	new CPUx86.Move(Registers.DX, 0x60);
+    /* Clear all keyboard buffers (output and command buffers) */
+        	new CPUAll.Label(".clearBuffer");
+			new CPUx86.InByte(Registers.AL, "064h");
+        	new CPUx86.Move(Registers.AH, Registers.AL);
+        	new CPUx86.Test(Registers.AH,1);
+        	new CPUx86.JumpIfZero(".skipClearIO");
+			new CPUx86.InByte(Registers.AL,Registers.DX);
+        	new CPUAll.Label(".skipClearIO");
+        	new CPUx86.Test(Registers.AH,2);
+        	new JumpIfNotZero(".clearBuffer");
+        	new Move(Registers.DX,0x64);
+        	new Move(Registers.AL, 0xfe);
+        	new Out(Registers.DX, Registers.AL);
+            new CPUAll.Label(".loop");//failed... halt
             new CPUx86.Hlt();
             new CPUx86.Jump(".loop");
         }
