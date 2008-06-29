f1004561529cbbd2d064fd18d1ed41c07e8520b1
cipher_cp <821fea98d6bc9380c58b8952f0c812fba5990545qNDTEvJk>
2008-06-29 15:32:46 +0000

    delegates with single args working, delegate test for me

--- source/CipherTest/Program.cs
+++ source/CipherTest/Program.cs
@@ -17,10 +17,34 @@ namespace CipherTest
 			BuildUI.Run();
 		}
 
+		public static void Times4(int i)
+		{
+			Console.WriteLine("i=" + i.ToString());
+			int x = i * 4;
+			Console.WriteLine(x);
+			//return x;
+		}
+
+		public static void Times2(int i,int t)
+		{
+			Console.WriteLine("i=" + i.ToString() + ";t=" + t.ToString());
+			int x = i * 2;
+			Console.WriteLine(x);
+		}
+
+		public delegate void Test(int i);
+
 		public static void Init()
 		{
 			Cosmos.Sys.Boot.Default();
 			Console.WriteLine("Boot complete");
+			Console.WriteLine("Press a key to test delegates!");
+			Test t = Times4;
+			//int x=
+			t(4);
+			//t += Times2;
+			//t(4,3);
+			//Console.WriteLine(x);
 			if (Console.ReadLine() == "r")
 			{
 				Console.WriteLine("Rebooting...");
@@ -28,6 +52,14 @@ namespace CipherTest
 			}
 			else
 				Cosmos.Sys.Deboot.Halt();
+		}
+		public static int return2(int i)
+		{
+			return 2;
+		}
+		public static int return3(int i)
+		{
+			return 3;
 		}
 	}
 }
--- source/Indy.IL2CPU.IL.X86/CustomImplementations/System/Assemblers/MulticastDelegate_Invoke.cs
+++ source/Indy.IL2CPU.IL.X86/CustomImplementations/System/Assemblers/MulticastDelegate_Invoke.cs
@@ -5,89 +5,97 @@ using System.Text;
 using Indy.IL2CPU.Plugs;
 using CPUx86 = Indy.IL2CPU.Assembler.X86;
 using CPU = Indy.IL2CPU.Assembler;
+using Indy.IL2CPU.Assembler.X86;
 
-namespace Indy.IL2CPU.IL.X86.CustomImplementations.System.Assemblers {
-    public class MulticastDelegate_Invoke : AssemblerMethod, INeedsMethodInfo {
-        /// <summary>
-        /// <para>This method implements Multicast Invoke. This means that it should call all delegates
-        /// in the current multicast delegate. </para>
-        /// <para>The argument size is available in the <code>$$ArgSize$$</code> field. This value is already rounded to 4byte boundaries</para>
-        /// </summary>
-        /// <param name="aAssembler"></param>
-        public override void Assemble(Assembler.Assembler aAssembler) {
-            if (MethodInfo == null) {
-                throw new Exception("This AssemblerMethod needs MethodInfo!");
-            }
-            /*
-             * EAX contains the GetInvocationList() array at the index at which it was last used
-             * EDX contains the index at which the EAX is
-             * EBX contains the number of items in the array
-             * ECX contains the argument size
-             */
-            new CPU.Label("____DEBUG_FOR_MULTICAST___");
-            new CPUx86.Move("eax", "[" + MethodInfo.Arguments[0].VirtualAddresses[0] + "]");
-            var xGetInvocationListMethod = typeof(MulticastDelegate).GetMethod("GetInvocationList");
-            new CPUx86.Push("eax");
-            new CPUx86.Call(CPU.Label.GenerateLabelName(xGetInvocationListMethod));
-            new CPUx86.Pop("eax");
-            new CPUx86.Add("eax", "8");
-            new CPUx86.Move("ebx", "[eax]");
-            new CPUx86.Add("eax", "8");
-            new CPUx86.Move("edi", "0");
-            new CPUx86.Move("ecx", "[" + MethodInfo.Arguments[0].VirtualAddresses[0] + "]");
-            new CPUx86.Move("ecx", "[ecx + " + (MethodInfo.Arguments[0].TypeInfo.Fields["$$ArgSize$$"].Offset + 12) + "]");
-            
-            new CPU.Label(".BEGIN_OF_LOOP");
-            new CPUx86.Compare("edx", "ebx");
-            new CPUx86.JumpIfEqual(".END_OF_INVOKE_");
-            new CPUx86.Pushad();
-            new CPUx86.Move("edi", "[eax]");
-            new CPUx86.Move("edi", "[edi + " + (MethodInfo.Arguments[0].TypeInfo.Fields["System.Object System.Delegate._target"].Offset+12) + "]");
-            new CPUx86.Compare("edi", "0");
-            new CPUx86.JumpIfZero(".NO_THIS");
-            new CPUx86.Push("edi");
+namespace Indy.IL2CPU.IL.X86.CustomImplementations.System.Assemblers
+{
+	public class MulticastDelegate_Invoke : AssemblerMethod, INeedsMethodInfo
+	{
+		/// <summary>
+		/// <para>This method implements Multicast Invoke. This means that it should call all delegates
+		/// in the current multicast delegate. </para>
+		/// <para>The argument size is available in the <code>$$ArgSize$$</code> field. This value is already rounded to 4byte boundaries</para>
+		/// </summary>
+		/// <param name="aAssembler"></param>
+		public override void Assemble(Assembler.Assembler aAssembler)
+		{
+			if (MethodInfo == null)
+			{
+				throw new Exception("This AssemblerMethod needs MethodInfo!");
+			}
+			/*
+			 * EAX contains the GetInvocationList() array at the index at which it was last used
+			 * EDX contains the index at which the EAX is
+			 * EBX contains the number of items in the array
+			 * ECX contains the argument size
+			 */
+			new CPU.Label("____DEBUG_FOR_MULTICAST___");
+			new CPUx86.Cli();//DEBUG ONLY
+#warning reenable interupts when issue is fixed!!!
+			new CPUx86.Move("eax", "[" + MethodInfo.Arguments[0].VirtualAddresses[0] + "]");
+			var xGetInvocationListMethod = typeof(MulticastDelegate).GetMethod("GetInvocationList");
+			new CPUx86.Push("eax");//addrof this
+			new CPUx86.Call(CPU.Label.GenerateLabelName(xGetInvocationListMethod));
+			new CPUx86.Pop("eax");//list
+			new CPUx86.Add("eax", "8");//addrof list.count??
+			new CPUx86.Move("ebx", "[eax]");//list.count
+			new CPUx86.Add("eax", "8");//why? -- start of list i think?
+			new CPUx86.Move("edi", "0");
+			new CPUx86.Move("ecx", "[" + MethodInfo.Arguments[0].VirtualAddresses[0] + "]");//addrof the delegate
+			new CPUx86.Move("ecx", "[ecx + " + (MethodInfo.Arguments[0].TypeInfo.Fields["$$ArgSize$$"].Offset + 12) + "]");//the size of the arguments to the method? + 12??? -- 12 is the size of the current call stack.. i think
+			new CPUx86.Xor("edx", "edx");//make sure edx is 0
+			new CPU.Label(".BEGIN_OF_LOOP");
+			new CPUx86.Compare("edx", "ebx");//are we at the end of this list
+			new CPUx86.JumpIfEqual(".END_OF_INVOKE_");//then we better stop
+			new CPUx86.Pushad();
+			new CPUx86.Move("edi", "[" + MethodInfo.Arguments[0].VirtualAddresses[0] + "]");
+			new CPUx86.Move("edi", "[edi + " + (MethodInfo.Arguments[0].TypeInfo.Fields["System.Object System.Delegate._target"].Offset + 12) + "]");
+			new CPUx86.Compare("edi", "0");
+			new CPUx86.JumpIfZero(".NO_THIS");
+			new CPUx86.Push("edi");
 
-            new CPU.Label(".NO_THIS");
-            new CPUx86.Sub("esp", "ecx");
-            new CPUx86.Move("esi",
-                            "esp");
-            new CPUx86.Compare("edi", "0");
-            new CPUx86.JumpIfZero(".NO_THIS2");
-            new CPUx86.Add("esi", "4");
+			new CPU.Label(".NO_THIS");
+			new CPUx86.Move("esi", "esp");
+			new CPUx86.Sub("esp", "ecx");
+			new CPUx86.Compare("edi", "0");//_target is null
+			new CPUx86.JumpIfZero(".NO_THIS2");
+			new CPUx86.Add("esi", "4");
 
-            new CPU.Label(".NO_THIS2");
-            new CPUx86.Move("edi", "[eax]");
-            new CPUx86.Move("edi", "[edi + " + (MethodInfo.Arguments[0].TypeInfo.Fields["System.IntPtr System.Delegate._methodPtr"].Offset + 12) + "]");
-            new CPUx86.Push("edi");
-            new CPUx86.Move("edi", "esp");
-            new CPUx86.Add("edi", "4");
-            // substract twice
-            new CPUx86.Add("esi",
-                           "ecx");
-            new CPUx86.Add("esi",
-                           "ecx");
-            new CPUx86.Add("esi",
-                           "44"); // 32 for the Pushad, 16 for the method header
-            new CPUx86.RepeatMovsb();
-            new CPUx86.Pop("edi");
-            new CPUx86.Call("edi");
-            new CPUx86.Popad();
-            new CPUx86.Add("edx",
-                           "1");
-            new CPUx86.Add("eax", "4");
-            new CPUx86.Jump(".BEGIN_OF_LOOP");
+			new CPU.Label(".NO_THIS2");
+			new CPUx86.Move("edi", "[eax]");
+			new CPUx86.Move("edi", "[edi + " + (MethodInfo.Arguments[0].TypeInfo.Fields["System.IntPtr System.Delegate._methodPtr"].Offset + 12) + "]");//
+			new CPUx86.Push("edi");
+			new CPUx86.Move("edi", "esp");
+			new CPUx86.Add("edi", "4");
+			//we allocated the argsize on the stack once, and it we need to get above the original args
+			new CPUx86.Add("esi","ecx");
+			new CPUx86.Add("esi","ecx");
+			new CPUx86.Add("esi", "44"); // 32 for the Pushad + 12 for the call stack
+			new CPUx86.RepeatMovsb();
+			new CPUx86.Pop("edi");
+			new CPUx86.Call("edi");
+			if (MethodInfo.ReturnSize > 0)
+			{
+				//we return a var
+			}
+			new CPUx86.Popad();
+			new CPUx86.Inc("edx");
+			new CPUx86.Add("eax", "4");
+			new CPUx86.Jump(".BEGIN_OF_LOOP");
 
-            new CPU.Label(".END_OF_INVOKE_");
-            //MethodInfo.Arguments[0].
-//            new CPUx86.Move("ebx", "[eax + " + (MethodInfo.Arguments[0].TypeInfo.Fields["$$ArgSize$$"].Offset + 12) + "]");
+			new CPU.Label(".END_OF_INVOKE_");
+			new CPUx86.Sti();
+#warning remove this ^ sti call when issue is fixed!!!
+			//MethodInfo.Arguments[0].
+			//            new CPUx86.Move("ebx", "[eax + " + (MethodInfo.Arguments[0].TypeInfo.Fields["$$ArgSize$$"].Offset + 12) + "]");
 
-            //new CPUx86.Move("eax", CPUx86.Registers.
-        }
+			//new CPUx86.Move("eax", CPUx86.Registers.
+		}
 
-        public MethodInformation MethodInfo
-        {
-            get;
-            set;
-        }
-    }
+		public MethodInformation MethodInfo
+		{
+			get;
+			set;
+		}
+	}
 }
