6a90e8aee13052f1e616d222d47b9856d883a9b7
smremde_cp <2296c648a83df9531cb573d8dfdead9aa751ab92VfNzPxkR>
2008-08-08 14:23:30 +0000

    added addss and movss sse instructions

---
+++ source/Indy.IL2CPU.Assembler.X86/SSE/MovSS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Indy.IL2CPU.Assembler.X86.SSE {
+	[OpCode(0xFFFFFFFF, "movss")]
+    public class MovSS : Instruction
+    {
+		private string mDestination;
+		private string mSource;
+		public MovSS(string aDestination, string aSource) {
+			mDestination = aDestination;
+			mSource = aSource;
+		}
+		
+		public override string ToString() {
+			return "movss " + mDestination + ", " + mSource;
+		}
+	}
+}
