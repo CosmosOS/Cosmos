f18f26536241d6cb3a7252a322840f4262fe274e
Dokugogagoji_cp <b6e2aec4c75f6c7ed24ad4643d9c3f2418b49072x4GURU9x>
2008-12-22 18:30:08 +0000

    quick fix for movs

--- source/Indy.IL2CPU/Assembler/x86/Movs.cs
+++ source/Indy.IL2CPU/Assembler/x86/Movs.cs
@@ -5,15 +5,37 @@ using System.Text;
 
 namespace Indy.IL2CPU.Assembler.X86 {
     [OpCode("movs")]
-    public class Movs: Instruction, IInstructionWithSize, IInstructionWithPrefix {
-        public InstructionPrefixes Prefixes {
+    public class Movs : InstructionWithSize, IInstructionWithPrefix
+    {
+        public InstructionPrefixes Prefixes
+        {
             get;
             set;
         }
 
-        public byte Size {
+        public byte Size
+        {
             get;
             set;
+        }
+
+        public override string ToString()
+        {
+            var xPref = "";
+            if ((Prefixes & InstructionPrefixes.Repeat) != 0)
+            {
+                xPref = "rep ";
+            }
+            switch (Size)
+            {
+                case 32:
+                    return xPref + "movsd";
+                case 16:
+                    return xPref + "movsw";
+                case 8:
+                    return xPref + "movsb";
+                default: throw new Exception("Size not supported!");
+            }
         }
     }
 }
