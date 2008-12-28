3035070bd6a530f394b0c4ed7643f6a3029364ea
Scalpel_cp <165da7fc5536ee16440a98f161bfa866a8b94595G55xazV5>
2008-12-28 12:17:26 +0000

    Fixed paths in .zip file

--- Setup/CreateVisualStudioTemplate.bat
+++ Setup/CreateVisualStudioTemplate.bat
@@ -1,4 +1,6 @@
 del Cosmos.vsi
-..\Build\tools\7zip\7za.exe -r a CosmosBoot.zip ProjectTemplate
+cd ProjectTemplate
+..\..\Build\tools\7zip\7za.exe -r a ..\CosmosBoot.zip *.*
+cd ..
 ..\Build\tools\7zip\7za.exe a -tzip Cosmos.vsi Cosmos.vscontent CosmosBoot.zip
 del CosmosBoot.zip
