92c24ec40566e9a378e682c5967374dc462791a6
feal87_cp <74e14436fe02db0c34c91c96612cc24198649e26vgD9mkgp>
2009-02-12 16:45:57 +0000

    Little fix on the previous commit.

--- source/BaseTest/Program.cs
+++ source/BaseTest/Program.cs
@@ -19,8 +19,66 @@ namespace BaseTest
 
         public static void Eratosthene()
         {
-            Console.WriteLine("The sieve of Eratosthene (A little benchmark on the memoryaccess of Cosmos)");
-            Console.Write("Executing 4096 byte array test : ");
+            Console.WriteLine("Eratosthene check (A little benchmark on the memoryaccess/cpu of Cosmos)");
+            Console.Write("Executing 1024000 element (int) array test : ");
+
+            Int32 BeforeSeconds = RTC.GetSeconds();
+            Int32 BeforeMinutes = RTC.GetMinutes();
+            Int32 BeforeHours = RTC.GetHours();
+
+            Boolean[] arr = new Boolean[1024000];
+            for (int i = 2; (i * i) <= arr.Length; i++)
+            {
+                for (int j = (i * i); j <= arr.Length; j = j + i)
+                {
+                    arr[j] = true;
+                }
+            }
+
+            Int32 AfterSeconds = RTC.GetSeconds();
+            Int32 AfterMinutes = RTC.GetMinutes();
+            Int32 AfterHours = RTC.GetHours();
+
+            Int32 DiffsHours = (AfterHours - BeforeHours);
+            Int32 DiffsMinutes = (AfterMinutes - BeforeMinutes);
+            Int32 DiffsSeconds = (AfterSeconds - BeforeSeconds);
+
+            Int32 FinalHours = DiffsHours;
+            Int32 FinalMinutes = DiffsMinutes;
+            Int32 FinalSeconds = DiffsSeconds;
+
+            Int32 Completed = 0;
+            while (Completed < 3)
+            {
+                Completed = 0;
+                if (DiffsHours < 0)
+                    FinalHours = 24 - DiffsHours;
+                else
+                    Completed++;
+                if (DiffsMinutes < 0)
+                {
+                    FinalMinutes = 60 - DiffsMinutes;
+                    FinalHours--;
+                }
+                else
+                    Completed++;
+                if (DiffsSeconds < 0)
+                {
+                    FinalSeconds = 60 - DiffsSeconds;
+                    FinalMinutes--;
+                }
+                else
+                    Completed++;
+            }
+            Console.WriteLine(FinalHours + " Hours" +
+                              FinalMinutes + " Minutes" +
+                              FinalSeconds + " Seconds");
+        }
+
+        public static void PrimeNumbers()
+        {
+            Console.WriteLine("Traditional prime number check (A little benchmark on the memoryaccess/cpu of Cosmos)");
+            Console.Write("Executing 4096 element array test : ");
 
             Int32 BeforeSeconds = RTC.GetSeconds();
             Int32 BeforeMinutes = RTC.GetMinutes();
@@ -85,6 +143,7 @@ namespace BaseTest
 
             Console.WriteLine("Welcome! This will be my playground to test out the various features i'll implement. :)");
 
+            PrimeNumbers();
             Eratosthene();
 
             Deboot.ShutDown();
