6ca8120fbd97636ea5e4d895acc44db5344ce353
Dokugogagoji_cp <b6e2aec4c75f6c7ed24ad4643d9c3f2418b49072x4GURU9x>
2009-02-14 08:59:14 +0000

    Deleted APM Code as we doesn't want BIOS Call

--- source/Cosmos/Cosmos.Sys.Plugs/Assemblers/Shutdown.cs
+++ source/Cosmos/Cosmos.Sys.Plugs/Assemblers/Shutdown.cs
@@ -8,29 +8,12 @@ namespace Cosmos.Sys.Plugs.Assemblers{
     {
         public override void Assemble(Assembler aAssembler)
         {
-            //ACPI Way...ONLY QEMu
+            //ACPI Way...ONLY QEMu And Boschs
             new CPUx86.Move { DestinationReg = CPUx86.Registers.DX, SourceValue = 0xB004 };
             new CPUx86.Move { DestinationReg = CPUx86.Registers.AX, SourceValue= 0x0 | 0x2000};
             new CPUx86.Out { DestinationReg = CPUx86.Registers.AX, Size = (byte)CPUx86.Instruction.InstructionSize.Word };
             //TODO: ACPI Way...see http://forum.osdev.org/viewtopic.php?t=16990
-            //APM Way...not for QEmu
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.AX, SourceValue = 0x5304 };
-            new CPUx86.Xor { DestinationReg = CPUx86.Registers.BX, SourceReg = CPUx86.Registers.BX };
-            new CPUx86.Interrupt { DestinationValue = 0x15 };
-            /*new CPUx86.Move { DestinationReg = CPUx86.Registers.AX, SourceValue = 0x5300 };
-            new CPUx86.Xor { DestinationReg = CPUx86.Registers.BX, SourceReg = CPUx86.Registers.BX};
-            new CPUx86.Interrupt { DestinationValue = 0x15 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.AX, SourceValue = 0x5301 };
-            new CPUx86.Xor { DestinationReg = CPUx86.Registers.BX, SourceReg = CPUx86.Registers.BX };
-            new CPUx86.Interrupt { DestinationValue = 0x15 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.AX, SourceValue = 0x5308 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.BX, SourceValue = 1 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.CX, SourceReg = CPUx86.Registers.BX };
-            new CPUx86.Interrupt { DestinationValue = 0x15 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.AX, SourceValue = 0x5307 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.BX, SourceValue = 1 };
-            new CPUx86.Move { DestinationReg = CPUx86.Registers.CX, SourceValue = 3 };
-            new CPUx86.Interrupt { DestinationValue = 0x15 };*/
+            
         }
     }
 }
