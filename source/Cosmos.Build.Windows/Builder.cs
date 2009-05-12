d8e8f8c3ca90b05a50d7a61902c7ea96f383abca
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2009-05-12 16:59:41 +0000

    Modified comment to be clear that 2009 is server 2008, not vs 2008.

--- source/Cosmos.Build.Windows/Builder.cs
+++ source/Cosmos.Build.Windows/Builder.cs
@@ -451,7 +451,7 @@ namespace Cosmos.Compiler.Builder {
             RemoveFile(aDrive + @":\syslinux.cfg");
             File.Copy(xPath + @"syslinux.cfg", aDrive + @":\syslinux.cfg");
             // Set MBR
-            //TODO: Hangs on 2008 - maybe needs admin permissions? Or maybe its not compat?
+            //TODO: Hangs on Windows Server 2008 - maybe needs admin permissions? Or maybe its not compat?
             // Runs from command line ok in 2008.....
             Global.Call(ToolsPath + "syslinux.exe", "-fma " + aDrive + ":", ToolsPath, true, true);
         }
