47b556bddd71dc593de840ac77413dbb9f596e47
jdguzman_cp <1af410b6d6b8ed62e871cfe75df683c658511bcf7yA7jPGF>
2009-06-11 01:53:07 +0000

    Added a project playground to test ext2 fs.

---
+++ source/Ext2.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Cosmos.Compiler.Builder;
+using Cosmos.Debug;
+
+namespace Ext2.Test
+{
+    class Program
+    {
+        #region Cosmos Builder logic
+        // Most users wont touch this. This will call the Cosmos Build tool
+        [STAThread]
+        static void Main ( string [] args )
+        {
+            BuildUI.Run ();
+        }
+        #endregion
+
+        // Main entry point of the kernel
+        public static void Init ()
+        {
+            var xboot = new Cosmos.Sys.Boot ( ) ;
+            xboot.Execute (  );
+
+            Console.WriteLine ( "Welcome! You just booted C# code. Please edit Program.cs to fit your needs" );
+            while ( true )
+                ;
+        }
+    }
+}
