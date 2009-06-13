b6ccbb5f34691c6d91fdc35009f41e13b7496be0
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2009-06-13 09:10:54 +0000

    Scrolling fixes to BuildUI

--- source/Boot/Playgrounds/Kudzu/KudzuTest/Debugger.cs
+++ source/Boot/Playgrounds/Kudzu/KudzuTest/Debugger.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Dbg = Cosmos.Debug;
+//using Dbg = Cosmos.Debug;
 
 namespace Cosmos.Playground.Kudzu {
-    public class Debugger {
-        public static void Main() {
-            Random xRandom = new Random((int)(Cosmos.Hardware.Global.TickCount
-                + Cosmos.Hardware.RTC.GetSeconds()));
-            // Divide by 100, get remainder
-            Dbg.Debugger.Break();
-            int xMagicNo = xRandom.Next() % 100;
-            Dbg.Debugger.Send("The magic number is " + xMagicNo);
-            Console.WriteLine("Hello world");
-            Dbg.Debugger.TraceOn();
-            int xDummy = 4;
-            Dbg.Debugger.TraceOff();
-            Console.ReadLine();
-        }
+  public class Debugger {
+    public static void Main() {
+      Random xRandom = new Random((int)(Cosmos.Hardware.Global.TickCount
+          + Cosmos.Hardware.RTC.GetSeconds()));
+      // Divide by 100, get remainder
+      //Dbg.Debugger.Break();
+      int xMagicNo = xRandom.Next() % 100;
+      //Dbg.Debugger.Send("The magic number is " + xMagicNo);
+      Console.WriteLine("Hello world");
+      //Dbg.Debugger.TraceOn();
+      int xDummy = 4;
+      //Dbg.Debugger.TraceOff();
+      Console.ReadLine();
     }
+  }
 }
--- source/Boot/Playgrounds/Kudzu/KudzuTest/Program.cs
+++ source/Boot/Playgrounds/Kudzu/KudzuTest/Program.cs
@@ -6,26 +6,26 @@ namespace Cosmos.Playground.Kudzu {
 	class Program {
 		[STAThread]
 		static void Main(string[] args) {
-            // This is here to run it on Windows and see results when necessary
-            // Then can be run on Cosmos to see if values are the same
-            //RTL8139.CreateTestFrame();
-            BuildUI.Run();
-        }
+      // This is here to run it on Windows and see results when necessary
+      // Then can be run on Cosmos to see if values are the same
+      //RTL8139.CreateTestFrame();
+      BuildUI.Run();
+    }
 
 		public static void Init() {
-            var xBoot = new Cosmos.Sys.Boot();
-            xBoot.Execute();            
-            Console.WriteLine("Boot complete");
+      var xBoot = new Cosmos.Sys.Boot();
+      xBoot.Execute();            
+      Console.WriteLine("Boot complete");
 
-            //PCITest.Test();
-            //Tests.DoAll();
-            RTL8139.Test();
-            Debugger.Main();
+      //PCITest.Test();
+      //Tests.DoAll();
+      //RTL8139.Test();
+      Debugger.Main();
 
-            Console.WriteLine("Done - Waiting");
-            Console.ReadLine();
-            //TODO: Make this automatically called after Init if no other shut downs are called
-            Cosmos.Sys.Deboot.ShutDown();
+      Console.WriteLine("Done - Waiting");
+      Console.ReadLine();
+      //TODO: Make this automatically called after Init if no other shut downs are called
+      Cosmos.Sys.Deboot.ShutDown();
 		}
 
         public static void DoLoop() {
