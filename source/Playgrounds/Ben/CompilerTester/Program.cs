5ded525f33d7ad8440999fbfab1cad40e5e7cb52
bklooste_cp <d493321b6d8ade6f3deed23e5331f2b3f7b089d1krzYEtdJ>
2009-06-13 15:22:12 +0000

    fix

--- source/Playgrounds/Ben/CompilerTester/Program.cs
+++ source/Playgrounds/Ben/CompilerTester/Program.cs
@@ -23,58 +23,58 @@ namespace CompilerTester
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var xBasePath = BuildOptions.Load().BuildPath;
-                var xCompileHelper = new CompilerHelper(); //Todo push ICompilerOptions into CompilerHelper
+            //try
+            //{
+            //    var xBasePath = BuildOptions.Load().BuildPath;
+            //    var xCompileHelper = new CompilerHelper(); //Todo push ICompilerOptions into CompilerHelper
 
-                xCompileHelper.GetCacheStateFile += new Func<Assembly, string>(delegate(Assembly aAssembly)
-                {
-                    return Path.Combine(xBasePath, aAssembly.GetName().Name + ".cachestate");
-                });
-                xCompileHelper.GetChecksumFile += new Func<Assembly, string>(delegate(Assembly aAssembly)
-                {
-                    return Path.Combine(xBasePath, aAssembly.GetName().Name + ".checksum");
-                });
-                xCompileHelper.GetOpCodeMap += new Func<Indy.IL2CPU.IL.OpCodeMap>(delegate { return new X86OpCodeMap(); });
-                xCompileHelper.SkipList.Add(typeof(Builder).Assembly);
-                xCompileHelper.GetAssembler += new Func<Assembly, bool, Indy.IL2CPU.Assembler.Assembler>(
-                    delegate(Assembly aAssembly, bool aIsMain)
-                    {
-                        return new Assembler();
-                    });
-                xCompileHelper.SaveAssembler += new Action<Assembly, Indy.IL2CPU.Assembler.Assembler>(delegate(Assembly aAssembly, Indy.IL2CPU.Assembler.Assembler aAssembler)
-                {
-                    using (var xOut = new StreamWriter(Path.Combine(xBasePath, aAssembly.GetName().Name + ".out"), false))
-                    {
-                        aAssembler.FlushText(xOut);
-                    }
-                });
-                xCompileHelper.DebugLog += delegate(LogSeverityEnum aSeverity, string aMessage) { Console.WriteLine("{0}: {1}", aSeverity, aMessage); };
+            //    xCompileHelper.GetCacheStateFile += new Func<Assembly, string>(delegate(Assembly aAssembly)
+            //    {
+            //        return Path.Combine(xBasePath, aAssembly.GetName().Name + ".cachestate");
+            //    });
+            //    xCompileHelper.GetChecksumFile += new Func<Assembly, string>(delegate(Assembly aAssembly)
+            //    {
+            //        return Path.Combine(xBasePath, aAssembly.GetName().Name + ".checksum");
+            //    });
+            //    xCompileHelper.GetOpCodeMap += new Func<Indy.IL2CPU.IL.OpCodeMap>(delegate { return new X86OpCodeMap(); });
+            //    xCompileHelper.SkipList.Add(typeof(Builder).Assembly);
+            //    xCompileHelper.GetAssembler += new Func<Assembly, bool, Indy.IL2CPU.Assembler.Assembler>(
+            //        delegate(Assembly aAssembly, bool aIsMain)
+            //        {
+            //            return new Assembler();
+            //        });
+            //    xCompileHelper.SaveAssembler += new Action<Assembly, Indy.IL2CPU.Assembler.Assembler>(delegate(Assembly aAssembly, Indy.IL2CPU.Assembler.Assembler aAssembler)
+            //    {
+            //        using (var xOut = new StreamWriter(Path.Combine(xBasePath, aAssembly.GetName().Name + ".out"), false))
+            //        {
+            //            aAssembler.FlushText(xOut);
+            //        }
+            //    });
+            //    xCompileHelper.DebugLog += delegate(LogSeverityEnum aSeverity, string aMessage) { Console.WriteLine("{0}: {1}", aSeverity, aMessage); };
 
 
-                string PlugPath = Path.Combine(Path.Combine(xBasePath, "Tools"), "Plugs"); 
-                xCompileHelper.Plugs.Add(Path.Combine(PlugPath, "Cosmos.Kernel.Plugs.dll"));
-                xCompileHelper.Plugs.Add(Path.Combine(PlugPath, "Cosmos.Hardware.Plugs.dll"));
-                xCompileHelper.Plugs.Add(Path.Combine(PlugPath, "Cosmos.Sys.Plugs.dll"));
-                xCompileHelper.CompileExe(typeof(MatthijsTest.Program).Assembly);
-            }
-            catch (Exception E)
-            {
-                Console.WriteLine("Error: " + E.ToString());
-                if (E.Message == "Temporary abort")
-                {
-                    Terminate = true;
-                }
-            }
-            finally
-            {
-                Console.WriteLine("Done.");
-                if (!Terminate)
-                {
-                    Console.ReadLine();
-                }
-            }
+            //    string PlugPath = Path.Combine(Path.Combine(xBasePath, "Tools"), "Plugs"); 
+            //    xCompileHelper.Plugs.Add(Path.Combine(PlugPath, "Cosmos.Kernel.Plugs.dll"));
+            //    xCompileHelper.Plugs.Add(Path.Combine(PlugPath, "Cosmos.Hardware.Plugs.dll"));
+            //    xCompileHelper.Plugs.Add(Path.Combine(PlugPath, "Cosmos.Sys.Plugs.dll"));
+            //    xCompileHelper.CompileExe(typeof(MatthijsTest.Program).Assembly);
+            //}
+            //catch (Exception E)
+            //{
+            //    Console.WriteLine("Error: " + E.ToString());
+            //    if (E.Message == "Temporary abort")
+            //    {
+            //        Terminate = true;
+            //    }
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Done.");
+            //    if (!Terminate)
+            //    {
+            //        Console.ReadLine();
+            //    }
+            //}
         }
         private static bool Terminate = false;
     }
