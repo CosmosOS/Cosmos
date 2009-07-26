1eca9f2a6d44e74edf7c5e64d8f8d9f7c4ec1ae5
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2009-07-26 15:11:07 +0000

    we now emit something

--- source2/Users/Kudzu/HelloWorld/Program.cs
+++ source2/Users/Kudzu/HelloWorld/Program.cs
@@ -4,6 +4,7 @@ using Cosmos.IL2CPU;
 using Cosmos.IL2CPU.X86;
 using Indy.IL2CPU.Assembler.X86;
 using S = Cosmos.Hardware.TextScreen;
+using System.IO;
 
 namespace HelloWorld {
 	class Program {
@@ -24,6 +25,12 @@ namespace HelloWorld {
       using (var xOldAsmblr = new CosmosAssembler(0)) {
         //InitializePlugs(aPlugs);
         xScanner.Execute(xEntryPoint);
+				var xPath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+				xPath = Path.Combine(xPath, "Output.asm");
+				using (var xOut = File.CreateText(xPath))
+				{
+					xOldAsmblr.FlushText(xOut);
+				}
       }
     }
 		#endregion
