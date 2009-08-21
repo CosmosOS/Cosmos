c0e0fdd25c20e64bd360de2dd5e5a5d2a8c144b6
sschocke_cp <7f003704bc94ef3acf0afcda2bf5f827e6e21455CS9agd82>
2009-08-21 16:09:41 +0000

    Created Sub Classes in SSchockeTest for Network testing and Float testing

---
+++ source/SSchockeTest/FloatTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.Playground.SSchocke
+{
+    public static class FloatTest
+    {
+        static float testFloat;
+
+        public static void Test()
+        {
+            testFloat = 10.5f;
+        }
+    }
+}
