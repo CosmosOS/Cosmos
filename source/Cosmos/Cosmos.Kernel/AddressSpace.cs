0c627d085e2e82024e82dddc50900fb151126d04
gero_cp <aedf1912d64e75efb3bb49b5159a7da9457c739bgpZb7EVV>
2009-08-23 12:58:41 +0000

    - 64 Bit read/write

--- source/Cosmos/Cosmos.Kernel/AddressSpace.cs
+++ source/Cosmos/Cosmos.Kernel/AddressSpace.cs
@@ -35,6 +35,13 @@ namespace Cosmos.Kernel {
         public abstract UInt32 Read32(UInt32 offset);
 
         /// <summary>
+        /// Reads 64 bits from a given offset if it within the valid range
+        /// </summary>
+        /// <param name="offset">The offset to read from</param>
+        /// <returns>32 bits of data read</returns>
+        public abstract UInt64 Read64( UInt32 offset );
+
+        /// <summary>
         /// Reads 8 bits from a given offset. The offset is not checked. Use with caution
         /// </summary>
         /// <param name="offset">The offset to read from</param>
@@ -56,6 +63,13 @@ namespace Cosmos.Kernel {
         public abstract UInt32 Read32Unchecked(UInt32 offset);
 
         /// <summary>
+        /// Reads 64 bits from a given offset. The offset is not checked. Use with caution
+        /// </summary>
+        /// <param name="offset">The offset to read from</param>
+        /// <returns>64 bits of data read</returns>
+        public abstract UInt64 Read64Unchecked( UInt64 offset );
+
+        /// <summary>
         /// Writes 8 bits from a given offset if it is within the valid range.
         /// </summary>
         /// <param name="offset">The offset to write to</param>
@@ -75,7 +89,14 @@ namespace Cosmos.Kernel {
         /// <param name="offset">The offset to write to</param>
         /// <param name="value">The data to write</param>
         public abstract void Write32(UInt32 offset, UInt32 value);
-        
+
+        /// <summary>
+        /// Writes 64 bits from a given offset if it is within the valid range.
+        /// </summary>
+        /// <param name="offset">The offset to write to</param>
+        /// <param name="value">The data to write</param>
+        public abstract void Write64( UInt32 offset, UInt64 value );
+
         /// <summary>
         /// Writes 8 bits from a given offset. The offset is not checked. Use with caution.
         /// </summary>
@@ -95,7 +116,14 @@ namespace Cosmos.Kernel {
         /// </summary>
         /// <param name="offset">The offset to write to</param>
         /// <param name="value">The data to write</param>
-        public abstract void Write32Unchecked(UInt32 offset, UInt32 value);        
+        public abstract void Write32Unchecked(UInt32 offset, UInt32 value);
+
+        /// <summary>
+        /// Writes 64 bits from a given offset. The offset is not checked. Use with caution.
+        /// </summary>
+        /// <param name="offset">The offset to write to</param>
+        /// <param name="value">The data to write</param>
+        public abstract void Write64Unchecked( UInt32 offset, UInt64 value ); 
     }
 
     public unsafe class MemoryAddressSpace : AddressSpace
@@ -122,6 +150,13 @@ namespace Cosmos.Kernel {
             return *(UInt32*)(this.Offset + offset);
         }
 
+        public override UInt64 Read64( UInt32 offset )
+        {
+            if( offset < 0 || offset > Size )
+                throw new ArgumentOutOfRangeException( "offset" );
+            return *( UInt64* )( this.Offset + offset );
+        }
+
         public override byte Read8Unchecked(UInt32 offset)
         {
             return *(byte*)(this.Offset + offset);
@@ -134,7 +169,12 @@ namespace Cosmos.Kernel {
         {
             return *(UInt32*)(this.Offset + offset);
         }
-       
+
+        public override UInt64 Read32Unchecked( UInt64 offset )
+        {
+            return *( UInt64* )( this.Offset + offset );
+        }
+
         public override void Write8(UInt32 offset, byte value)
         {
             if (offset < 0 || offset > Size)
@@ -153,7 +193,12 @@ namespace Cosmos.Kernel {
                 throw new ArgumentOutOfRangeException("offset"); 
             (*(UInt32*)(this.Offset + offset)) = value;
         }
-                
+        public override void Write64( UInt32 offset, UInt64 value )
+        {
+            if( offset < 0 || offset > Size )
+                throw new ArgumentOutOfRangeException( "offset" );
+            ( *( UInt64* )( this.Offset + offset ) ) = value;
+        }        
         public override void Write8Unchecked(UInt32 offset, byte value)
         {
             (*(byte*)(this.Offset + offset)) = value;
@@ -165,6 +210,10 @@ namespace Cosmos.Kernel {
         public override void Write32Unchecked(UInt32 offset, UInt32 value)
         {
             (*(UInt32*)(this.Offset + offset)) = value;
+        }
+        public override void Write64Unchecked( UInt32 offset, UInt64 value )
+        {
+            ( *( UInt64* )( this.Offset + offset ) ) = value;
         }
     }
 
@@ -195,6 +244,10 @@ namespace Cosmos.Kernel {
                 throw new ArgumentOutOfRangeException("offset");
             return Kernel.CPUBus.Read32((UInt16)(this.Offset + offset));
         }
+        public override UInt64 Read64( UInt32 offset )
+        {
+            throw new NotImplementedException(); 
+        }
 
         public override byte Read8Unchecked(UInt32 offset)
         {
@@ -207,6 +260,10 @@ namespace Cosmos.Kernel {
         public override UInt32 Read32Unchecked(UInt32 offset)
         {
             return Kernel.CPUBus.Read32((UInt16)(this.Offset + offset));
+        }
+        public override UInt64 Read64Unchecked( UInt32 offset )
+        {
+            throw new NotImplementedException(); 
         }
 
         public override void Write8(UInt32 offset, byte value)
@@ -227,7 +284,10 @@ namespace Cosmos.Kernel {
                 throw new ArgumentOutOfRangeException("offset");
             Kernel.CPUBus.Write32((UInt16)(this.Offset + offset), value);
         }
-
+        public override void Write64( UInt32 offset, UInt64 value )
+        {
+            throw new NotImplementedException(); 
+        }
         public override void Write8Unchecked(UInt32 offset, byte value)
         {
             Kernel.CPUBus.Write8((UInt16)(this.Offset + offset), value);
@@ -239,6 +299,10 @@ namespace Cosmos.Kernel {
         public override void Write32Unchecked(UInt32 offset, UInt32 value)
         {
             Kernel.CPUBus.Write32((UInt16)(this.Offset + offset), value);
+        }
+        public override void Write64Unchecked( UInt32 offset, UInt64 value )
+        {
+            throw new NotImplementedException();
         }
     }
 }
