246d4eebe139ec50e61fcd2dd104da68c5b45549
gero_cp <aedf1912d64e75efb3bb49b5159a7da9457c739bgpZb7EVV>
2009-08-25 09:55:21 +0000

    - Ported Leave ILOp

--- source2/IL2PCU/Cosmos.IL2CPU.X86/IL/Leave.cs
+++ source2/IL2PCU/Cosmos.IL2CPU.X86/IL/Leave.cs
@@ -1,37 +1,39 @@
 using System;
-
+using CPUx86 = Indy.IL2CPU.Assembler.X86;
 namespace Cosmos.IL2CPU.X86.IL
 {
-	[Cosmos.IL2CPU.OpCode(ILOpCode.Code.Leave)]
-	public class Leave: ILOp
-	{
-		public Leave(Cosmos.IL2CPU.Assembler aAsmblr):base(aAsmblr)
-		{
-		}
+    [Cosmos.IL2CPU.OpCode( ILOpCode.Code.Leave )]
+    public class Leave : ILOp
+    {
+        public Leave( Cosmos.IL2CPU.Assembler aAsmblr )
+            : base( aAsmblr )
+        {
+        }
 
-    public override void Execute(MethodInfo aMethod, ILOpCode aOpCode) {
-      //TODO: Implement this Op
+        public override void Execute( MethodInfo aMethod, ILOpCode aOpCode )
+        {
+            new CPUx86.Jump { DestinationLabel = AssemblerNasm.TmpBranchLabel( aMethod, aOpCode ) };
+        }
+
+
+        // using System;
+        // using System.IO;
+        // 
+        // 
+        // using CPU = Indy.IL2CPU.Assembler.X86;
+        // 
+        // namespace Indy.IL2CPU.IL.X86 {
+        // 	[OpCode(OpCodeEnum.Leave)]
+        // 	public class Leave: Op {public readonly string TargetLabel;
+        // 	public Leave(ILReader aReader, MethodInformation aMethodInfo)
+        // 			: base(aReader, aMethodInfo) {
+        // 			TargetLabel = GetInstructionLabel(aReader.OperandValueBranchPosition);
+        // 		}
+        // 		public override void DoAssemble() {
+        //         new CPU.Jump { DestinationLabel = TargetLabel };
+        // 		}
+        // 	}
+        // }
+
     }
-
-    
-		// using System;
-		// using System.IO;
-		// 
-		// 
-		// using CPU = Indy.IL2CPU.Assembler.X86;
-		// 
-		// namespace Indy.IL2CPU.IL.X86 {
-		// 	[OpCode(OpCodeEnum.Leave)]
-		// 	public class Leave: Op {public readonly string TargetLabel;
-		// 	public Leave(ILReader aReader, MethodInformation aMethodInfo)
-		// 			: base(aReader, aMethodInfo) {
-		// 			TargetLabel = GetInstructionLabel(aReader.OperandValueBranchPosition);
-		// 		}
-		// 		public override void DoAssemble() {
-		//         new CPU.Jump { DestinationLabel = TargetLabel };
-		// 		}
-		// 	}
-		// }
-		
-	}
 }
