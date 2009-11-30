abfd6d7aef996b20bc008fa71865769feef20b51
Dokugogagoji_cp <b6e2aec4c75f6c7ed24ad4643d9c3f2418b49072x4GURU9x>
2009-11-30 08:27:50 +0000

    fixing build goofed...

---
+++ source2/IL2PCU/Cosmos.IL2CPU.X86/X86/x87/FXStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.IL2CPU.X86.x87 {
+    [OpCode("fxrstor")]
+    public class FXStore : InstructionWithDestination
+    {
+        public static void InitializeEncodingData(Instruction.InstructionData aData) {
+            aData.EncodingOptions.Add(new InstructionData.InstructionEncodingOption {
+                OpCode = new byte[] { 0x0F, 0xAE },
+                NeedsModRMByte=true,
+                InitialModRMByteValue=0x08,
+                DestinationMemory = true,
+                ReverseRegisters=true
+            });
+        }
+	}
+}
---
+++ source2/IL2PCU/Cosmos.IL2CPU/CustomImplementation/System/DoubleImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cosmos.IL2CPU.Plugs;
+
+namespace Cosmos.IL2CPU.IL.CustomImplementations.System
+{
+    [Plug(Target = typeof(Double))]
+    public static class DoubleImpl
+    {
+        public static string ToString(ref double aThis)
+        {
+            return DoubleImpl2.GetNumberString(aThis);
+        }
+    }
+    public static class DoubleImpl2
+    {
+        public static string GetNumberString(double aValue)
+        {
+            const string xDigits = "0123456789";
+            const char divisor = '.';
+            char[] xResultChars= new char[70];
+            //BitConverter.(aValue,0);
+            return null;
+        }
+    }
+}
