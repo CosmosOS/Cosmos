eb1dcc4a951f17f12cb49847f256563144877fdf
ch2845_cp <36e18756a6d6ac748effa27c3a26f3a3509382f22C2EG5ms>
2009-12-17 22:16:35 +0000

    it aint workin but i'm tired of it so i'm done for now. on this test project. if someone else would like to make it work PLEASE Feel free to do so. i am also fixing a mistake i made in the last commit.

---
+++ source/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cosmos.Compiler.Builder;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        #region Cosmos Builder logic
+        // Most users wont touch this. This will call the Cosmos Build tool
+        [STAThread]
+        static void Main(string[] args)
+        {
+            BuildUI.Run();
+        }
+        #endregion
+
+        // Main entry point of the kernel
+        public static void Init()
+        {
+            var xBoot = new Cosmos.Sys.Boot();
+            xBoot.Execute();
+
+            float x;
+            x = 10.5F;
+            x = x / 2;
+
+            Cosmos.Sys.Deboot.ShutDown();
+        }
+    }
+}
