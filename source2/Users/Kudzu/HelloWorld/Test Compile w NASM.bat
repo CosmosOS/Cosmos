05eab2687e482b13899c68b7fecf54903f04cda7
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2010-04-06 16:24:10 +0000

    Further cleanup

--- source2/Users/Kudzu/HelloWorld/Test Compile w NASM.bat
+++
@@ -1,8 +0,0 @@
-REM Tools\nasm\nasm -g -f elf -F stabs -o output.obj Tools\asm\main.asm
-
-del nasm.log
-..\..\..\..\build\Tools\nasm\nasm.exe -g -f bin -o output.obj output.asm -E nasm.log
-  
-pause
-
-
