3bc272d9a7749e5ea747d4dc38805e514224d83b
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2010-07-10 22:57:15 +0000

    Initial checkin of Cosmos.Debug.GDB

---
+++ source2/Debug/Cosmos.Debug.GDB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Cosmos.Debug.GDB {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
