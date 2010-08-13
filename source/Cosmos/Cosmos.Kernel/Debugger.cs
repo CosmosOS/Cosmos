450ce435c1453a7780e4ec3fd7db11609e9f6040
mscoolnerd_cp <6146a91edb6be14edb68c183855b55452c6c4e1bA922exVz>
2010-08-13 03:24:24 +0000

    Added SendNumber, WriteNumber, and WriteBinary to Cosmos.Debug.Debugger

--- source/Cosmos/Cosmos.Kernel/Debugger.cs
+++ source/Cosmos/Cosmos.Kernel/Debugger.cs
@@ -18,16 +18,148 @@ namespace Cosmos.Debug {
 
         // TODO: Kudzu repacement methods for Cosmos.Hardware.DebugUtil
         public static unsafe void SendMessage(string aModule, string aData) {
-            string xSingleString = "Message Module: " + aModule + " Data: " + aData;
+            string xSingleString;
+            xSingleString = "Message Module: \"" + aModule + "\"";
+            xSingleString += " Data: \"" + aData + "\"";
             Send(xSingleString);
         }
 
         public static unsafe void SendError(string aModule, string aData)
         {
-            string xSingleString = "Error Module: " + aModule + " Data: " + aData;
+            string xSingleString;
+            xSingleString = "Error Module: \"" + aModule + "\"";
+            xSingleString += " Data: \"" + aData + "\"";
             Send(xSingleString);
         }
 
+        public static unsafe void SendNumber(string aModule, string aDescription, uint aNumber, byte aBits)
+        {
+            string xSingleString;
+            xSingleString = "Number Module: \"" + aModule + "\"";
+            xSingleString += " Description: \"" + aDescription + "\"";
+            xSingleString += " Number: \"" + CreateNumber(aNumber, aBits) + "\"";
+        }
+
+        public static unsafe void WriteNumber(uint aNumber, byte aBits)
+        {
+            WriteNumber(aNumber, aBits, true);
+        }
+
+        public static unsafe void WriteNumber(uint aNumber, byte aBits, bool aWritePrefix)
+        {
+            Send(CreateNumber(aNumber, aBits, aWritePrefix));
+        }
+
+        public static unsafe string CreateNumber(uint aNumber, byte aBits)
+        {
+            return CreateNumber(aNumber, aBits, true);
+        }
+
+        public static unsafe string CreateNumber(uint aNumber, byte aBits, bool aWritePrefix)
+        {
+            string xNumberString = null;
+            uint xValue = aNumber;
+            byte xCurrentBits = aBits;
+            if (aWritePrefix)
+            {
+                xNumberString += "0x";
+            }
+            while (xCurrentBits >= 4)
+            {
+                xCurrentBits -= 4;
+                byte xCurrentDigit = (byte)((xValue >> xCurrentBits) & 0xF);
+                string xDigitString = null;
+                switch (xCurrentDigit)
+                {
+                    case 0:
+                        xDigitString = "0";
+                        goto default;
+                    case 1:
+                        xDigitString = "1";
+                        goto default;
+                    case 2:
+                        xDigitString = "2";
+                        goto default;
+                    case 3:
+                        xDigitString = "3";
+                        goto default;
+                    case 4:
+                        xDigitString = "4";
+                        goto default;
+                    case 5:
+                        xDigitString = "5";
+                        goto default;
+                    case 6:
+                        xDigitString = "6";
+                        goto default;
+                    case 7:
+                        xDigitString = "7";
+                        goto default;
+                    case 8:
+                        xDigitString = "8";
+                        goto default;
+                    case 9:
+                        xDigitString = "9";
+                        goto default;
+                    case 10:
+                        xDigitString = "A";
+                        goto default;
+                    case 11:
+                        xDigitString = "B";
+                        goto default;
+                    case 12:
+                        xDigitString = "C";
+                        goto default;
+                    case 13:
+                        xDigitString = "D";
+                        goto default;
+                    case 14:
+                        xDigitString = "E";
+                        goto default;
+                    case 15:
+                        xDigitString = "F";
+                        goto default;
+                    default:
+                        xNumberString += xDigitString;
+                        break;
+                }
+            }
+            return xNumberString;
+        }
+
+        public static unsafe void WriteBinary(string aModule, string aMessage, byte[] aValue)
+        {
+            WriteBinary(aModule, aMessage, aValue, 0, aValue.Length);
+        }
+
+        public static unsafe void WriteBinary(string aModule, string aMessage, byte[] aValue, int aIndex, int aLength)
+        {
+            string xSingleString;
+            xSingleString = "Binary Module = \"" + aModule + "\"";
+            xSingleString += " Message = " + aMessage + "\"";
+            xSingleString += " Value = \"";
+            for (int i = 0; i < aLength; i++)
+            {
+                xSingleString += CreateNumber(aValue[aIndex + i], 8, false);
+            }
+            xSingleString += "\"";
+            Send(xSingleString);
+        }
+
+        public static unsafe void WriteBinary(string aModule, string aMessage, byte* aValue, int aIndex, int aLength)
+        {
+            string xSingleString;
+            xSingleString = "Binary Module = \"" + aModule + "\"";
+            xSingleString += " Message = " + aMessage + "\"";
+            xSingleString += " Value = \"";
+            for (int i = 0; i < aLength; i++)
+            {
+                xSingleString += CreateNumber(aValue[aIndex + i], 8, false);
+            }
+            xSingleString += "\"";
+            Send(xSingleString);
+        }
+        
         public unsafe static void ViewMemory()
         {
             ViewMemory(0);
