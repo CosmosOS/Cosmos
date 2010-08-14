46e035e12e0a1e2185e238fa449fff501b210100
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2010-08-14 01:43:06 +0000

    Updated VMX to include USB and more hardware.

--- source2/Users/Kudzu/Breakpoints/Program.cs
+++ source2/Users/Kudzu/Breakpoints/Program.cs
@@ -14,7 +14,7 @@ namespace BreakpointsKernel
             Console.WriteLine("Test");
             Debugger.Send("Hello from Cosmos!");
             Console.WriteLine("3 Cosmos booted successfully. Type a line of text to get it echoed back.");
-            Debugger.Break();
+            //Debugger.Break();
             Console.WriteLine("Test");
 
             while (true)
