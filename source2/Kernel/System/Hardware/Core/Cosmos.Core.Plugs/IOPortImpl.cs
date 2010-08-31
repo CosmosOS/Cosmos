ac62705dc7c4fce68a2906b0955a99a0d0a9f30a
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2010-08-31 00:15:27 +0000

    Bloody x08 fix!!!!!!!

--- source2/Kernel/System/Hardware/Core/Cosmos.Core.Plugs/IOPortImpl.cs
+++ source2/Kernel/System/Hardware/Core/Cosmos.Core.Plugs/IOPortImpl.cs
@@ -13,8 +13,8 @@ namespace Cosmos.Core.Plugs {
         public sealed class IOWrite8Asm : AssemblerMethod {
             public override void AssembleNew(object aAssembler, object aMethodInfo) {
                 //TODO: This is a lot of work to write to a single port. We need to have some kind of inline ASM option that can emit a single out instruction
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceDisplacement = 0x08, SourceIsIndirect = true };
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.EBP, SourceDisplacement = 0x04, SourceIsIndirect = true };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceDisplacement = 0x0C, SourceIsIndirect = true };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.EBP, SourceDisplacement = 0x08, SourceIsIndirect = true };
                 new CPUx86.Out { DestinationReg = CPUx86.Registers.AL };
             }
         }
@@ -23,8 +23,8 @@ namespace Cosmos.Core.Plugs {
 
         public sealed class IOWrite16Asm : AssemblerMethod {
             public override void AssembleNew(object aAssembler, object aMethodInfo) {
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x04 };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x0C };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
                 new CPUx86.Out { DestinationReg = CPUx86.Registers.AX };
             }
         }
@@ -33,8 +33,8 @@ namespace Cosmos.Core.Plugs {
 
         public sealed class IOWrite32Asm : AssemblerMethod {
             public override void AssembleNew(object aAssembler, object aMethodInfo) {
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x04 };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x0C };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
                 new CPUx86.Out { DestinationReg = CPUx86.Registers.EAX };
             }
         }
@@ -44,7 +44,7 @@ namespace Cosmos.Core.Plugs {
         public sealed class IORead8Asm : AssemblerMethod {
             public override void AssembleNew(object aAssembler, object aMethodInfo) {
                 //TODO: Also make an attribute that forces normal inlining fo a method
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x04 };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
                 //TODO: Do we need to clear rest of EAX first?
                 //    MTW: technically not, as in other places, it _should_ be working with AL too..
                 new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceValue = 0 };
@@ -59,7 +59,7 @@ namespace Cosmos.Core.Plugs {
 
         public sealed class IORead16Asm : AssemblerMethod {
             public override void AssembleNew(object aAssembler, object aMethodInfo) {
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x04 };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
                 new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceValue = 0 };
                 new CPUx86.In { DestinationReg = CPUx86.Registers.AX };
                 new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
@@ -72,7 +72,7 @@ namespace Cosmos.Core.Plugs {
 
         public sealed class IORead32Asm : AssemblerMethod {
             public override void AssembleNew(object aAssembler, object aMethodInfo) {
-                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x04 };
+                new CPUx86.Move { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.EBP, SourceIsIndirect = true, SourceDisplacement = 0x08 };
                 new CPUx86.In { DestinationReg = CPUx86.Registers.EAX };
                 new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
             }
