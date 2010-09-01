b4ee5c4680f4825899332191feaff5672abf2a92
mscoolnerd_cp <6146a91edb6be14edb68c183855b55452c6c4e1bA922exVz>
2010-09-01 05:23:12 +0000

    Added System.Math Plug

---
+++ source2/Kernel/System/Cosmos.System.Plugs.System/MathImpl.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.IL2CPU.Plugs;
+using Cosmos.System;
+
+namespace Cosmos.System.Plugs.System
+{
+    [Plug(Target = typeof(global::System.Math))]
+    public class MathImpl
+    {
+        public const double PI = 3.14159265358979;
+        public const double E = 2.71828182845905;
+
+        public static double Abs(double value)
+        {
+            double xResult;
+            if (value < 0)
+            {
+                xResult = value - (2 * value);
+            }
+            else
+            {
+                xResult = value;
+            }
+            return xResult;
+        }
+        
+        //public static float Abs(float value)
+        //{
+        //  float xResult;
+        //  if (value < 0)
+        //  {
+        //    xResult = value - (2 * value);
+        //  }
+        //  else
+        //  {
+        //    xResult = value;
+        //  }
+        //  return xResult;
+        //}
+
+        //public static long Abs(long value)
+        //{
+        //  long xResult;
+        //  if (value < 0)
+        //  {
+        //    xResult = value - (2 * value);
+        //  }
+        //  else
+        //  {
+        //    xResult = value;
+        //  }
+        //  return xResult;
+        //}
+
+        public static int Abs(int value)
+        {
+            int xResult;
+            if (value < 0)
+            {
+                xResult = value - (2 * value);
+            }
+            else
+            {
+                xResult = value;
+            }
+            return xResult;
+        }
+
+        public static double Pow(double x, double y)
+        {
+            double xResult = x;
+            if (y == 0)
+            {
+                xResult = 1;
+            }
+            else if (y == 1)
+            {
+                xResult = x;
+            }
+            else
+            {
+                for (int i = 2; i <= y; i++)
+                {
+                    xResult = xResult * x;
+                }
+            }
+            return xResult;
+        }
+    }
+}
