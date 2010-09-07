ef921ebfe6cdada189d5b52150edfa0bd069c4ad
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2010-09-07 00:36:19 +0000

    Cosmos.Build.StandAlone

---
+++ source2/Build/Cosmos.Build.StandAlone/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cosmos.Build.StandAlone {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void butnBuild_Click(object sender, EventArgs e) {
+            var xIL2CPU = new Cosmos.Build.MSBuild.IL2CPU();
+
+        }
+    }
+}
