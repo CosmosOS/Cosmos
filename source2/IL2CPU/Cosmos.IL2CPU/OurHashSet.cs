6f245d66c62920e00ec0ae1245726897a2ddc53f
Trivalik_cp <42497cfff885d3ca0e6fda54fb6262dd42101bd5sx56jUzf>
2010-09-07 14:29:37 +0000

    try to solve nams issue, on my machine internal problem, but could internal related

--- source2/IL2CPU/Cosmos.IL2CPU/OurHashSet.cs
+++ source2/IL2CPU/Cosmos.IL2CPU/OurHashSet.cs
@@ -4,56 +4,100 @@ using System.Linq;
 using System.Text;
 using System.Collections;
 
-namespace Cosmos.IL2CPU {
-    // Contains known types and methods, both scanned and unscanned
-    // We need both a HashSet and a List. HashSet for speed of checking
-    // to see if we already have it. And mItems contains an indexed list
-    // so we can scan it as it changes. Foreach can work on HashSet,
-    // but if foreach is used while its changed, a collection changed
-    // exception will occur and copy on demand for each loop has too
-    // much overhead.
-    // we use a custom comparer, because the default one does some intelligent magic, which breaks lookups. is probably related
-    // to comparing different types
-    // Its possible .NET 4.0 may have a better replacement, but be careful about the object compare issue (see note about custom
-    // comparer in ILScanner)
+namespace Cosmos.IL2CPU
+{
+	// Contains known types and methods, both scanned and unscanned
+	// We need both a HashSet and a List. HashSet for speed of checking
+	// to see if we already have it. And mItems contains an indexed list
+	// so we can scan it as it changes. Foreach can work on HashSet,
+	// but if foreach is used while its changed, a collection changed
+	// exception will occur and copy on demand for each loop has too
+	// much overhead.
+	// we use a custom comparer, because the default Hashcode does not work.
+	// In .NET 4.0 has the DeclaringType often changed to System.Object,
+	// didn't sure if hashcode changed. The situation now in .NET 4.0
+	// is that the Contains method in OurHashSet checked only the
+	// default Hashcode. With adding DeclaringType in the Hashcode it runs.
 
-    public class OurHashSet<T> : IEnumerable<T> {
-    private Dictionary<int, T> mItems = new Dictionary<int, T>();
+	public class OurHashSet<T> : IEnumerable<T>
+	{
+		private Dictionary<int, T> mItems = new Dictionary<int, T>();
 
-    public bool Contains(T aItem) {
-      if(aItem==null){
-        throw new ArgumentNullException("aItem");
-      }
-      return mItems.ContainsKey(aItem.GetHashCode());
-    }
+		public bool Contains(T aItem)
+		{
+			if (aItem == null)
+				throw new ArgumentNullException("aItem");
+			return mItems.ContainsKey(GetHash(aItem));
 
-    public void Add(T aItem) {
-      if (aItem == null) {
-        throw new ArgumentNullException("aItem");
-      }
-      mItems.Add(aItem.GetHashCode(), aItem);
-    }
+			var methodDeclaringType = aItem.GetType().GetMethod("get_DeclaringType");
+			var aItemDeclaringType = methodDeclaringType.Invoke(aItem, null);
 
-    public IEnumerator<T> GetEnumerator() {
-      return (from item in mItems
-              select item.Value).GetEnumerator();
-    }
+			bool ret2 = false;
+			foreach (var item in mItems)
+			{
+				var method = item.Value.GetType().GetMethod("get_DeclaringType");
+				var itemDeclaringType = method.Invoke(item.Value, null);
 
-    public T GetItemInList(T aItem) {
-      if (aItem == null) {
-        throw new ArgumentNullException("aItem");
-      }
-      T xResult;
-      if (mItems.TryGetValue(aItem.GetHashCode(), out xResult)) {
-        return xResult;
-      } else {
-        return aItem;
-      }
-    }
+				if (item.Value.ToString() == aItem.ToString()
+					&& itemDeclaringType == aItemDeclaringType)
+				{
+					if (ret2)
+						throw new Exception(" item mulitply in list!");
+					ret2 = true;
+				}
 
-    IEnumerator IEnumerable.GetEnumerator() {
-      return (from item in mItems
-              select item.Value).GetEnumerator();
-    }
-  }
+			}
+
+			return ret2;
+		}
+
+		public void Add(T aItem)
+		{
+			if (aItem == null)
+				throw new ArgumentNullException("aItem");
+			mItems.Add(GetHash(aItem), aItem);
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return (from item in mItems
+					select item.Value).GetEnumerator();
+		}
+
+		public T GetItemInList(T aItem)
+		{
+			if (aItem == null)
+				throw new ArgumentNullException("aItem");
+			T xResult;
+			if (mItems.TryGetValue(GetHash(aItem), out xResult))
+				return xResult;
+			else
+				return aItem;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return (from item in mItems
+					select item.Value).GetEnumerator();
+		}
+
+		public static Type GetDeclareType(T item)
+		{
+			var xMethodDeclaringType = item.GetType().GetMethod("get_DeclaringType");
+			var xDeclaringType = xMethodDeclaringType.Invoke(item, null) as Type;
+			return xDeclaringType;
+		}
+
+		public static string GetDeclareTypeString(T item)
+		{
+			var xName = GetDeclareType(item);
+			return xName == null ? "null" : xName.ToString();
+		}
+
+		public static int GetHash(T item)
+		{
+			//return item.GetHashCode();
+			return (item.ToString() + GetDeclareTypeString(item)).GetHashCode();
+		}
+	}
 }
