dde2ecad00cded0d92af5731b7d9b687cc8629ab
mterwoord_cp <7cd3fd84a0151ea055c2f79e4d2eef9576fe9afesxUZAwxD>
2010-12-29 13:28:18 +0000

    keyboard works now

--- source2/Kernel/System/Hardware/Core/Cosmos.Core/PIC.cs
+++ source2/Kernel/System/Hardware/Core/Cosmos.Core/PIC.cs
@@ -37,8 +37,58 @@ namespace Cosmos.Core {
             // won't need either of those for a very long time, so we just mask it
             // completely. For more info:
             // http://en.wikipedia.org/wiki/Intel_8259#Spurious_Interrupts
-            Init(Master, 0x20, 4, 0xFF | 0x08);
-            Init(Slave, 0x28, 2, 0xFF);
+            
+            //Init(Master, 0x20, 4, 0xFD | 0x08);
+            //Init(Slave, 0x28, 2, 0xFF);
+            Remap(0x20, 0xFD | 0x08, 0x28, 0xFF);
+        }
+
+        private void Remap(byte masterStart, byte masterMask, byte slaveStart, byte slaveMask)
+        {
+            #region consts
+            // source: osdev.org
+            const byte ICW1_ICW4 = 0x01; // ICW4 (not) needed
+            const byte ICW1_SINGLE = 0x02; // Single (cascade) mode
+            const byte ICW1_INTERVAL4 = 0x04; // Call address interval 4 (8)
+            const byte ICW1_LEVEL = 0x08; // Level triggered (edge) mode
+            const byte ICW1_INIT = 0x10;
+
+            const byte ICW4_8086 = 0x01; // 8086/88 mode
+            const byte ICW4_AUTO = 0x02; // auto (normal) EOI
+            const byte ICW4_BUF_SLAVE = 0x08; // buffered mode/slave
+            const byte ICW4_BUF_MASTER = 0x0C; // buffered mode/master
+            const byte ICW4_SFNM = 0x10; // special fully nested (not)
+ 
+            #endregion
+
+            var xOldMasterMask = Master.Data.Byte;
+            var xOldSlaveMask = Slave.Data.Byte;
+            Master.Cmd.Byte = ICW1_INIT + ICW1_ICW4;
+            IOPort.Wait();
+            Slave.Cmd.Byte = ICW1_INIT + ICW1_ICW4;
+            IOPort.Wait();
+            Master.Data.Byte = masterStart;
+            IOPort.Wait();
+            Slave.Data.Byte = slaveStart;
+            IOPort.Wait();
+
+            // magic:
+            Master.Data.Byte = 4;
+            IOPort.Wait();
+            Slave.Data.Byte = 2;
+            IOPort.Wait();
+
+            // set modes:
+            Master.Data.Byte = ICW4_8086;
+            IOPort.Wait();
+            Slave.Data.Byte = ICW4_8086;
+            IOPort.Wait();
+
+            // set masks:
+            Master.Data.Byte = masterMask;
+            IOPort.Wait();
+            Slave.Data.Byte = slaveMask;
+            IOPort.Wait();
         }
 
         protected void Init(IOGroup.PIC aPIC, byte aBase, byte aIDunno, byte aMask){
@@ -47,7 +97,7 @@ namespace Cosmos.Core {
             // for 32 bit mode.
             // The only way to remap them however is to completely reinitialize the PICs.
 
-            byte xOldMask = Master.Data.Byte;
+            byte xOldMask = aPIC.Data.Byte;
 
             //#define ICW1_ICW4	0x01		/* ICW4 (not) needed */
             //#define ICW1_SINGLE	0x02		/* Single (cascade) mode */
@@ -65,7 +115,7 @@ namespace Cosmos.Core {
             Master.Data.Byte = aIDunno;
             IOPort.Wait();
 
-            //#define ICW4_AUTO	0x02		/* Auto (normal) EOI */
+            //#define ICW4_AUTO	0x02		/C:\Data\Cosmos\source2\Kernel\System\Hardware\Core\Cosmos.Core\CPU.cs* Auto (normal) EOI */
             //#define ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
             //#define ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
             //#define ICW4_SFNM	0x10		/* Special fully nested (not) */
