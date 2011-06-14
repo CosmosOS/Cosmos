7fc40826ea2f9888427f312c5e30eb0149c1bcfd
blah38621_cp <36925b492f9af1e8e676baa1ba817f9639f49ec7y44YxFx5>
2011-06-14 18:12:31 +0000

    Added FloatSine and FloatCosine instructions. This is to facilitate the needs of Orvid.Graphics.

---
+++ source2/Compiler/Cosmos.Compiler.Assembler.X86/x87/FloatCosine.cs
@@ -0,0 +1,7 @@
+﻿namespace Cosmos.Compiler.Assembler.X86.x87
+{
+    [OpCode("fcos")]
+    public class FloatCosine : Instruction
+    {
+    }
+}
---
+++ source2/Compiler/Cosmos.Compiler.Assembler.X86/x87/FloatSine.cs
@@ -0,0 +1,7 @@
+﻿namespace Cosmos.Compiler.Assembler.X86.x87
+{
+    [OpCode("fsin")]
+    public class FloatSine : Instruction
+    {
+    }
+}
