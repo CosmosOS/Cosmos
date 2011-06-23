d06894cf1668e7c45bd82fc7306e2c0fb2d0a60f
Trivalik_cp <42497cfff885d3ca0e6fda54fb6262dd42101bd5sx56jUzf>
2011-06-23 19:38:55 +0000

    fix 64-bit mul tested with ulong

--- source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Mul.cs
+++ source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Mul.cs
@@ -1,6 +1,7 @@
 using System;
 using CPUx86 = Cosmos.Compiler.Assembler.X86;
 using Cosmos.Compiler.Assembler.X86;
+using Label = Cosmos.Compiler.Assembler.Label;
 namespace Cosmos.IL2CPU.X86.IL
 {
     [Cosmos.IL2CPU.OpCode(ILOpCode.Code.Mul)]
@@ -25,12 +26,65 @@ namespace Cosmos.IL2CPU.X86.IL
                 }
                 else
                 {
-                    new CPUx86.Pop { DestinationReg = CPUx86.Registers.EAX };
-                    new CPUx86.Add { DestinationReg = CPUx86.Registers.ESP, SourceValue = 4 };
-                    new CPUx86.Multiply { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, Size = 32 };
-                    new CPUx86.Add { DestinationReg = CPUx86.Registers.ESP, SourceValue = 8 };
-                    new CPUx86.Push { DestinationValue = 0 };
-                    new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
+					// div of both == LEFT_LOW * RIGHT_LOW + ((LEFT_LOW * RIGHT_HIGH + RIGHT_LOW * LEFT_HIGH) << 32)
+					string BaseLabel = GetLabel(aMethod, aOpCode) + "__";
+					string Simple32Multiply = BaseLabel + "Simple32Multiply";
+					string MoveReturnValue = BaseLabel + "MoveReturnValue";
+
+					// right value
+					// low
+					//  SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true
+					// high
+					//  SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true, SourceDisplacement = 4
+
+					// left value
+					// low
+					//  SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true, SourceDisplacement = 8
+					// high
+					//  SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true, SourceDisplacement = 12
+
+					// compair LEFT_HIGH, RIGHT_HIGH , on zero only simple multiply is used
+					//mov RIGHT_HIGH to eax, is useable on Full 64 multiply
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true, SourceDisplacement = 4 };
+					new CPUx86.Compare { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true, SourceDisplacement = 12 };
+					new CPUx86.ConditionalJump { Condition = ConditionalTestEnum.Zero, DestinationLabel = Simple32Multiply };
+					// Full 64 Multiply
+					
+					// eax contains already RIGHT_HIGH
+					// multiply with LEFT_LOW
+					new CPUx86.Multiply { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = 8, Size = 32 };
+					// save result of LEFT_LOW * RIGHT_HIGH
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.ECX, SourceReg = CPUx86.Registers.EAX };
+
+					//mov RIGHT_LOW to eax
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true};
+					// multiply with LEFT_HIGH
+					new CPUx86.Multiply { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = 12, Size = 32 };
+					// add result of LEFT_LOW * RIGHT_HIGH + RIGHT_LOW + LEFT_HIGH
+					new CPUx86.Add { DestinationReg = CPUx86.Registers.ECX, SourceReg = CPUx86.Registers.EAX };
+
+					//mov RIGHT_LOW to eax
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true };
+					// multiply with LEFT_LOW
+					new CPUx86.Multiply { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = 8, Size = 32 };
+					// add LEFT_LOW * RIGHT_HIGH + RIGHT_LOW + LEFT_HIGH to high dword of last result
+					new CPUx86.Add { DestinationReg = CPUx86.Registers.EDX, SourceReg = CPUx86.Registers.ECX };
+
+					new CPUx86.Jump { DestinationLabel = MoveReturnValue };
+
+					new Label(Simple32Multiply);
+					//mov RIGHT_LOW to eax
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true };
+					// multiply with LEFT_LOW
+					new CPUx86.Multiply { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = 8, Size = 32 };
+
+					new Label(MoveReturnValue);
+					// move high result to left high
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = 12, SourceReg = CPUx86.Registers.EDX };
+					// move low result to left low
+					new CPUx86.Move { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = 8, SourceReg = CPUx86.Registers.EAX };
+					// pop right 64 value
+					new CPUx86.Add { DestinationReg = CPUx86.Registers.ESP, SourceValue = 8 };
                 }
             }
             else
@@ -52,34 +106,5 @@ namespace Cosmos.IL2CPU.X86.IL
                 }
             }
         }
-
-
-        // using System;
-        // using System.IO;
-        // 
-        // 
-        // using CPU = Cosmos.Compiler.Assembler.X86;
-        // 
-        // namespace Cosmos.IL2CPU.IL.X86 {
-        // 	[OpCode(OpCodeEnum.Mul)]
-        // 	public class Mul: Op {
-        // 	    private string mNextLabel;
-        // 	    private string mCurLabel;
-        // 	    private uint mCurOffset;
-        // 	    private MethodInformation mMethodInformation;
-        // 		public Mul(ILReader aReader, MethodInformation aMethodInfo)
-        // 			: base(aReader, aMethodInfo) {
-        // 		    mMethodInformation = aMethodInfo;
-        // 		    mCurOffset = aReader.Position;
-        // 		    mCurLabel = IL.Op.GetInstructionLabel(aReader);
-        //             mNextLabel = IL.Op.GetInstructionLabel(aReader.NextPosition);
-        // 		}
-        // 		public override void DoAssemble() {
-        // 			Multiply(Assembler, GetServiceProvider(),
-        //                 mCurLabel, mMethodInformation, mCurOffset, mNextLabel);
-        // 		}
-        // 	}
-        // }
-
     }
 }
