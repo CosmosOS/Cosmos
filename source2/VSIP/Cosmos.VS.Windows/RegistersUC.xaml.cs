1415a8529f5f89e9e6f0b0a2a568bf53bdfb0af0
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2011-07-10 04:08:45 +0000

    Register window improvement

--- source2/VSIP/Cosmos.VS.Windows/RegistersUC.xaml.cs
+++ source2/VSIP/Cosmos.VS.Windows/RegistersUC.xaml.cs
@@ -23,55 +23,55 @@ namespace Cosmos.Cosmos_VS_Windows
         public RegistersUC()
         {
             InitializeComponent();
-            lablEAX.Content = "--------";
-            lablAX.Content = "----";
-            lablAH.Content = "--";
-            lablAL.Content = "--";
-            lablEBX.Content = "--------";
-            lablBX.Content = "----";
-            lablBH.Content = "--";
-            lablBL.Content = "--";
-            lablECX.Content = "--------";
-            lablCX.Content = "----";
-            lablCH.Content = "--";
-            lablCL.Content = "--";
-            lablEDX.Content = "--------";
-            lablDX.Content = "----";
-            lablDH.Content = "--";
-            lablDL.Content = "--";
-            lablEBP.Content = "--------";
-            lablESI.Content = "--------";
-            lablEDI.Content = "--------";
-            lablESP.Content = "--------";
-            lablEIP.Content = "--------";
-            lablFlags.Content = "--------";
+            runEAX.Text = "0x--------";
+            runAX.Text = "0x----";
+            runAH.Text = "0x--";
+            runAL.Text = "0x--";
+            runEBX.Text = "0x--------";
+            runBX.Text = "0x----";
+            runBH.Text = "0x--";
+            runBL.Text = "0x--";
+            runECX.Text = "0x--------";
+            runCX.Text = "0x----";
+            runCH.Text = "0x--";
+            runCL.Text = "0x--";
+            runEDX.Text = "0x--------";
+            runDX.Text = "0x----";
+            runDH.Text = "0x--";
+            runDL.Text = "0x--";
+            runEBP.Text = "0x--------";
+            runESI.Text = "0x--------";
+            runEDI.Text = "0x--------";
+            runESP.Text = "0x--------";
+            runEIP.Text = "0x-------";
+            runFlags.Text = "--------";
       }
 
-        protected void UpdateRegisters(byte[] aData, int aOffset, Label a32, Label a16, Label a8Hi, Label a8Lo)
+        protected void UpdateRegisters(byte[] aData, int aOffset, Run a32, Run a16, Run a8Hi, Run a8Lo)
         {
             byte x8Lo = aData[aOffset];
             byte x8Hi = aData[aOffset + 1];
-            SetLabel(a8Lo, x8Lo.ToString("X2"));
-            SetLabel(a8Hi, x8Hi.ToString("X2"));
-            SetLabel(a16, x8Hi.ToString("X2") + x8Lo.ToString("X2"));
+            SetText(a8Lo, "0x" + x8Lo.ToString("X2"));
+            SetText(a8Hi, "0x" + x8Hi.ToString("X2"));
+            SetText(a16, "0x" + x8Hi.ToString("X2") + x8Lo.ToString("X2"));
             UpdateRegister32(aData, aOffset, a32);
         }
 
-        protected void UpdateRegister32(byte[] aData, int aOffset, Label a32)
+        protected void UpdateRegister32(byte[] aData, int aOffset, Run a32)
         {
             UInt32 x32 = (UInt32)
               (aData[aOffset + 3] << 24 |
               aData[aOffset + 2] << 16 |
               aData[aOffset + 1] << 8 |
               aData[aOffset]);
-            SetLabel(a32, x32.ToString("X8"));
+            SetText(a32, "0x" + x32.ToString("X8"));
         }
 
-        protected void SetLabel(Label aLabel, string aContent)
+        protected void SetText(Run aLabel, string aText)
         {
-            string xOldContent = (string)aLabel.Content;
-            aLabel.Content = aContent;
-            aLabel.Foreground = (xOldContent == aContent) ? Brushes.Black : Brushes.Red;
+            string xOldContent = (string)aLabel.Text;
+            aLabel.Text = aText;
+            aLabel.Foreground = (xOldContent == aText) ? Brushes.Black : Brushes.Red;
         }
 
         public void Update(byte[] aData)
@@ -92,15 +92,15 @@ namespace Cosmos.Cosmos_VS_Windows
             // ESP 32
             // EIP 36
             //
-            UpdateRegisters(aData, 28, lablEAX, lablAX, lablAH, lablAL);
-            UpdateRegisters(aData, 16, lablEBX, lablBX, lablBH, lablBL);
-            UpdateRegisters(aData, 24, lablECX, lablCX, lablCH, lablCL);
-            UpdateRegisters(aData, 20, lablEDX, lablDX, lablDH, lablDL);
-            UpdateRegister32(aData, 8, lablEBP);
-            UpdateRegister32(aData, 4, lablESI);
-            UpdateRegister32(aData, 0, lablEDI);
-            UpdateRegister32(aData, 32, lablESP);
-            UpdateRegister32(aData, 36, lablEIP);
+            UpdateRegisters(aData, 28, runEAX, runAX, runAH, runAL);
+            UpdateRegisters(aData, 16, runEBX, runBX, runBH, runBL);
+            UpdateRegisters(aData, 24, runECX, runCX, runCH, runCL);
+            UpdateRegisters(aData, 20, runEDX, runDX, runDH, runDL);
+            UpdateRegister32(aData, 8, runEBP);
+            UpdateRegister32(aData, 4, runESI);
+            UpdateRegister32(aData, 0, runEDI);
+            UpdateRegister32(aData, 32, runESP);
+            UpdateRegister32(aData, 36, runEIP);
           //TODO: Flags
         }
     }
