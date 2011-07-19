09dd37f7e5f92807deef44c31fd9424f01cc4f05
Sentinel209_cp <5b554accd9e292fcb886b205d8d4c32c5d52ce2df5Ng6vpx>
2011-07-19 22:57:29 +0000

    Assembly toolbar - copy button

--- source2/VSIP/Cosmos.VS.Windows/AssemblyUC.xaml.cs
+++ source2/VSIP/Cosmos.VS.Windows/AssemblyUC.xaml.cs
@@ -16,67 +16,91 @@ using System.Windows.Threading;
 using Cosmos.VS.Debug;
 using System.Threading;
 
-namespace Cosmos.Cosmos_VS_Windows {
-  public partial class AssemblyUC : UserControl {
+namespace Cosmos.Cosmos_VS_Windows
+{
+  public partial class AssemblyUC : UserControl
+  {
     static public byte[] mData = new byte[0];
     string mCode = "";
     bool mFilter = true;
 
-    public AssemblyUC() {
+    public AssemblyUC()
+    {
       InitializeComponent();
 
       mitmCopy.Click += new RoutedEventHandler(mitmCopy_Click);
       Update(mData);
     }
 
-    void mitmCopy_Click(object sender, RoutedEventArgs e) {
+    void mitmCopy_Click(object sender, RoutedEventArgs e)
+    {
       Clipboard.SetText(mCode);
     }
 
-    protected void Display(bool aFilter) {
+    protected void Display(bool aFilter)
+    {
       tblkSource.Inlines.Clear();
-      if (mData.Length == 0) {
+      if (mData.Length == 0)
+      {
         return;
       }
 
       mCode = Encoding.ASCII.GetString(mData);
 
       string[] xLines = mCode.Split('\n');
-      foreach (string xLine in xLines) {
+      foreach (string xLine in xLines)
+      {
         string xDisplayLine = xLine;
         string xTestLine = xLine.Trim().ToUpper();
         var xParts = xTestLine.Split(' ');
 
-        if (aFilter) {
-          if (xTestLine == "INT3") {
+        if (aFilter)
+        {
+          if (xTestLine == "INT3")
+          {
             continue;
-          } else if (xTestLine.Length == 0) {
+          }
+          else if (xTestLine.Length == 0)
+          {
             continue;
-          } else {
-            if (xParts.Length > 1) {
-              if (xParts[1] == ";ASM") {
+          }
+          else
+          {
+            if (xParts.Length > 1)
+            {
+              if (xParts[1] == ";ASM")
+              {
                 continue;
               }
             }
           }
-          
+
           xDisplayLine = xDisplayLine.Trim();
-          if (xParts[0].EndsWith(":")) {
+          if (xParts[0].EndsWith(":"))
+          {
             // Insert a blank line before labels, but not if its the top line
-            if (tblkSource.Inlines.Count > 0) {
+            if (tblkSource.Inlines.Count > 0)
+            {
               tblkSource.Inlines.Add(new LineBreak());
             }
-          } else {
+          }
+          else
+          {
             xDisplayLine = "\t" + xDisplayLine;
           }
         }
 
         var xRun = new Run(xDisplayLine);
-        if (xParts[0].EndsWith(":")) {
+        if (xParts[0].EndsWith(":"))
+        {
           xRun.Foreground = Brushes.Black;
-        } else if (xTestLine.StartsWith(";")) {
+        }
+        else if (xTestLine.StartsWith(";"))
+        {
           xRun.Foreground = Brushes.Green;
-        } else {
+        }
+        else
+        {
           xRun.Foreground = Brushes.Blue;
         }
 
@@ -85,7 +109,8 @@ namespace Cosmos.Cosmos_VS_Windows {
       }
     }
 
-    public void Update(byte[] aData) {
+    public void Update(byte[] aData)
+    {
       mData = aData;
       Display(mFilter);
     }
@@ -98,13 +123,13 @@ namespace Cosmos.Cosmos_VS_Windows {
 
     private void asmFCopyButton_Click(object sender, RoutedEventArgs e)
     {
-
+      Clipboard.SetText(mCode);
     }
 
     private void asmStepButton_Click(object sender, RoutedEventArgs e)
     {
 
     }
- 
+
   }
 }
