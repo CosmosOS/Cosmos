facf434a5d67b56472b8bb2400071ce94bb3fb2e
blah38621_cp <36925b492f9af1e8e676baa1ba817f9639f49ec7y44YxFx5>
2011-09-17 21:44:48 +0000

    Added a nice range of tests for IL2CPU, currently working on getting everything that needs plugging, plugged. The tests originate from Mono, and not even MS.Net passes all of them. (un-supprisingly, seeing as their mono's tests, MS.Net fails more of them than Mono 2.10.1) There are also tests in there that are designed to test every CIL OP-Code. Not all of those are enabled, as some have to be written in IL.

--- source2/Users/Orvid/IL2CPU Tester/TestKernel/Kernel.cs
+++ source2/Users/Orvid/IL2CPU Tester/TestKernel/Kernel.cs
@@ -15,48 +15,16 @@ namespace TestKernel
         }
         protected override void BeforeRun() { }
 
-        protected override void Run()
+        public void DoRun()
         {
-            #region Byte Conversions
-
-            #endregion
-
-
-            //byte r = 255;
-            //sbyte r = -128;
-            //char r = '\u0127';
-            //ushort r = 65535;
-            //short r = -32768;
-            //uint r = 4294967295;
-            //int r = -2147483648;
-            //ulong r = 18446744073709551615;
-            //long r = 9223372036854775807;
-            float r = 123.7f;
-            //double r = 123.7;
-            Console.WriteLine("Just one more test.");
-            Console.WriteLine("Raw Byte: " + r.ToString());
-            Console.WriteLine("Byte->Byte: " + (unchecked((byte)r)).ToString());
-            Console.WriteLine("Byte->SByte: " + (unchecked((sbyte)r)).ToString());
-            Console.WriteLine("Byte->Char: " + (unchecked((char)r)).ToString());
-            Console.WriteLine("Byte->UShort: " + (unchecked((ushort)r)).ToString());
-            Console.WriteLine("Byte->Short: " + (unchecked((short)r)).ToString());
-            Console.WriteLine("Byte->UInt: " + (unchecked((uint)r)).ToString());
-            Console.WriteLine("Byte->Int: " + (unchecked((int)r)).ToString());
-            Console.WriteLine("Byte->ULong: " + (unchecked((ulong)r)).ToString());
-            Console.WriteLine("Byte->Long: " + (unchecked((long)r)).ToString());
-            Console.WriteLine("Byte->Float: " + (unchecked((float)r)).ToString());
-            Console.WriteLine("Byte->Double: " + (unchecked((double)r)).ToString());
-            if (unchecked((decimal)r) != 123.7m)
-            {
-
-            }
-
-            while (true) { } // Prevent the check from being run again, and display results.
+            this.Run();
         }
 
-        public static void WriteError(string s)
+        protected override void Run()
         {
-            Console.WriteLine("Error: " + s);
+            IL2CPUTester.TestDriver.RunTests();
+
+            //while (true) { } // Prevent the check from being run again, and display results.
         }
     }
 }
--- source2/Users/Orvid/StructTest/SecondaryKernel.cs
+++ source2/Users/Orvid/StructTest/SecondaryKernel.cs
@@ -27,6 +27,10 @@ namespace StructTest
 
         protected override void Run()
         {
+            //Cosmos.Hardware.Drivers.PCI.Video.VMWareSVGAII v = new Cosmos.Hardware.Drivers.PCI.Video.VMWareSVGAII();
+            //v.Clear(0x00000000);
+            //v.Fill(30, 30, 30, 30, 0x88888888);
+            //v.Update(0, 0, 800, 600);
             //Monitor m = new Monitor();
             //byte r = 255;
             //sbyte r = -128;
@@ -36,26 +40,36 @@ namespace StructTest
             //uint r = 4294967295;
             //int r = -2147483648;
             //ulong r = 18446744073709551615;
-            //long r = 9223372036854775807;
-            float r = 123.7f;
+            //long r = -9223372036854775808;
+            //float r = 123.7f;
             //double r = 123.7;
-            Console.WriteLine("Just one more test.");
-            Console.WriteLine("Raw Byte: " + r.ToString());
-            Console.WriteLine("Byte->Byte: " + (unchecked((byte)r)).ToString());
-            Console.WriteLine("Byte->SByte: " + (unchecked((sbyte)r)).ToString());
-            Console.WriteLine("Byte->Char: " + (unchecked((char)r)).ToString());
-            Console.WriteLine("Byte->UShort: " + (unchecked((ushort)r)).ToString());
-            Console.WriteLine("Byte->Short: " + (unchecked((short)r)).ToString());
-            Console.WriteLine("Byte->UInt: " + (unchecked((uint)r)).ToString());
-            Console.WriteLine("Byte->Int: " + (unchecked((int)r)).ToString());
-            Console.WriteLine("Byte->ULong: " + (unchecked((ulong)r)).ToString());
-            Console.WriteLine("Byte->Long: " + (unchecked((long)r)).ToString());
-            Console.WriteLine("Byte->Float: " + (unchecked((float)r)).ToString());
-            Console.WriteLine("Byte->Double: " + (unchecked((double)r)).ToString());
-            Console.WriteLine("Byte->Decimal: " + (unchecked((decimal)r)).ToString());
+            double d;
+            d = 0;
+            if (d > 39.0d)
+            {
+                Console.WriteLine("Greater");
+            }
+            else
+            {
+                Console.WriteLine("Less Than");
+            }
+            //Console.WriteLine("Just one more test.");
+            //Console.WriteLine("Raw Byte: " + r.ToString());
+            //Console.WriteLine("Byte->Byte: " + (unchecked((byte)r)).ToString());
+            //Console.WriteLine("Byte->SByte: " + (unchecked((sbyte)r)).ToString());
+            //Console.WriteLine("Byte->Char: " + (unchecked((char)r)).ToString());
+            //Console.WriteLine("Byte->UShort: " + (unchecked((ushort)r)).ToString());
+            //Console.WriteLine("Byte->Short: " + (unchecked((short)r)).ToString());
+            //Console.WriteLine("Byte->UInt: " + (unchecked((uint)r)).ToString());
+            //Console.WriteLine("Byte->Int: " + (unchecked((int)r)).ToString());
+            //Console.WriteLine("Byte->ULong: " + (unchecked((ulong)r)).ToString());
+            //Console.WriteLine("Byte->Long: " + (unchecked((long)r)).ToString());
+            //Console.WriteLine("Byte->Float: " + (unchecked((float)r)).ToString());
+            //Console.WriteLine("Byte->Double: " + (unchecked((double)r)).ToString());
+            //Console.WriteLine("Byte->Decimal: " + (unchecked((decimal)r)).ToString());
             while (true)
             {
-
+                //Console.WriteLine(Cosmos.Hardware.RTC.Hour.ToString() + ":" + Cosmos.Hardware.RTC.Minute.ToString() + ":" + Cosmos.Hardware.RTC.Second.ToString());
             }
         }
     }
