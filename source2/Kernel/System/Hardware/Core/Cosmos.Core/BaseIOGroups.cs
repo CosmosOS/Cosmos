b6a63f6e627d0541a31cd0813c1deb70271568a9
HKS_cp <804d57068da1b85c21a2aa7102263f0b712556b1DWcdp7Ps>
2012-01-23 14:14:28 +0000

    Added pcspeaker to IOgroups

--- source2/Kernel/System/Hardware/Core/Cosmos.Core/BaseIOGroups.cs
+++ source2/Kernel/System/Hardware/Core/Cosmos.Core/BaseIOGroups.cs
@@ -11,6 +11,7 @@ namespace Cosmos.Core {
         // at least we have isolation between the consumers that use these.
         public readonly IOGroup.Keyboard Keyboard = new IOGroup.Keyboard();
         public static readonly IOGroup.Mouse Mouse = new IOGroup.Mouse();
+        public static readonly IOGroup.PCSpeaker PCSpeaker = new IOGroup.PCSpeaker();
         public readonly IOGroup.PIT PIT = new IOGroup.PIT();
         public readonly IOGroup.TextScreen TextScreen = new IOGroup.TextScreen();
         public readonly IOGroup.ATA ATA1 = new IOGroup.ATA(false);
