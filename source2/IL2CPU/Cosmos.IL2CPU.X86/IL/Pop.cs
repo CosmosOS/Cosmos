53a1874a79f053b433f2ce6898e571d4af60580c
blah38621_cp <36925b492f9af1e8e676baa1ba817f9639f49ec7y44YxFx5>
2012-02-04 18:37:31 +0000

    Pop now throws an exception if the stack is empty.

--- source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Pop.cs
+++ source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Pop.cs
@@ -3,21 +3,26 @@ using CPUx86 = Cosmos.Assembler.x86;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
-    [Cosmos.IL2CPU.OpCode( ILOpCode.Code.Pop )]
+    [Cosmos.IL2CPU.OpCode(ILOpCode.Code.Pop)]
     public class Pop : ILOp
     {
-        public Pop( Cosmos.Assembler.Assembler aAsmblr )
-            : base( aAsmblr )
+        public Pop(Cosmos.Assembler.Assembler aAsmblr)
+            : base(aAsmblr)
         {
         }
 
-        public override void Execute( MethodInfo aMethod, ILOpCode aOpCode )
+        public override void Execute(MethodInfo aMethod, ILOpCode aOpCode)
         {
-          // todo: implement exception support.
-          if (Assembler.Stack.Count > 0) {
-			  var xItem = Assembler.Stack.Pop();
-        new CPUx86.Add { DestinationReg = CPUx86.Registers.ESP, SourceValue = ILOp.Align((uint)xItem.Size, 4) };
-          }
+            // todo: implement exception support.
+            if (Assembler.Stack.Count > 0)
+            {
+                var xItem = Assembler.Stack.Pop();
+                new CPUx86.Add { DestinationReg = CPUx86.Registers.ESP, SourceValue = ILOp.Align((uint)xItem.Size, 4) };
+            }
+            else
+            {
+                throw new Exception("Cosmos.IL2CPU.X86->IL->Pop.cs: Cannot pop from the stack, because there isn't anything on the stack to pop!");
+            }
         }
 
     }
