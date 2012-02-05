718c5de7847014ede6c81616b0e65753f303c61e
Trivalik_cp <42497cfff885d3ca0e6fda54fb6262dd42101bd5sx56jUzf>
2012-02-05 20:06:52 +0000

    add TODO for sub_ovf*

--- source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Sub_Ovf.cs
+++ source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Sub_Ovf.cs
@@ -1,47 +1,20 @@
 using System;
+using CPUx86 = Cosmos.Assembler.x86;
+using Cosmos.Assembler.x86;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
 	[Cosmos.IL2CPU.OpCode(ILOpCode.Code.Sub_Ovf)]
 	public class Sub_Ovf: ILOp
 	{
-		public Sub_Ovf(Cosmos.Assembler.Assembler aAsmblr):base(aAsmblr)
-		{
+		public Sub_Ovf(Cosmos.Assembler.Assembler aAsmblr):base(aAsmblr) {
 		}
 
-    public override void Execute(MethodInfo aMethod, ILOpCode aOpCode) {
-      throw new NotImplementedException();
-    }
-
-    
-		// using System;
-		// using System.IO;
-		// 
-		// 
-		// using CPU = Cosmos.Assembler.x86;
-		// 
-		// namespace Cosmos.IL2CPU.IL.X86 {
-		// 	[Cosmos.Assembler.OpCode(OpCodeEnum.Sub_Ovf)]
-		// 	public class Sub_Ovf: Op {
-		// 		private string mCurrentLabel;
-		// 	    private string mNextLabel;
-		// 	    private uint mCurrentOffset;
-		// 	    private MethodInformation mCurrentMethodInfo;
-		//         public Sub_Ovf(ILReader aReader, MethodInformation aMethodInfo)
-		//             : base(aReader, aMethodInfo)
-		//         {
-		//             mCurrentLabel = GetInstructionLabel(aReader);
-		//             mCurrentOffset = aReader.Position;
-		//             mNextLabel = GetInstructionLabel(aReader.NextPosition);
-		//             mCurrentMethodInfo = aMethodInfo;
-		//         }
-		// 
-		// 	    public override void DoAssemble() {
-		//             EmitNotImplementedException(Assembler, GetServiceProvider(), "Sub_Ovf instruction not yet implemented",
-		//                 mCurrentLabel, mCurrentMethodInfo, mCurrentOffset, mNextLabel);
-		// 		}
-		// 	}
-		// }
-		
+		public override void Execute(MethodInfo aMethod, ILOpCode aOpCode) {
+			if (Assembler.Stack.Peek().IsFloat) {
+				throw new NotImplementedException("Sub_Ovf: TODO need to call Sub IL");
+			}
+			throw new NotImplementedException();
+		}
 	}
 }
