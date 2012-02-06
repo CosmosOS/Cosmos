0a692f743557d1be7df97f8d7235529ea6fbbcbd
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2012-02-06 17:09:54 +0000

    Profile compiles now.

--- source2/IL2CPU/Cosmos.IL2CPU.Profiler/Assembler.cs
+++ source2/IL2CPU/Cosmos.IL2CPU.Profiler/Assembler.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 
 namespace Cosmos.IL2CPU.Profiler {
-  public class Assembler : Cosmos.IL2CPU.AppAssembler {
+  public class Assembler : Cosmos.IL2CPU.X86.AppAssembler {
 
+    public Assembler() : base(0) {
+    }
+    
     protected override void InitILOps() {
-      var xILOp = new ILOp(this);
+      var xILOp = new ILOp(this.Assembler);
       // Don't change the type in the foreach to a var, its necessary as it is now
       // to typecast it, so we can then recast to an int.
       foreach (ILOpCode.Code xCode in Enum.GetValues(typeof(ILOpCode.Code))) {
