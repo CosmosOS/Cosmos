ce9180d15a4bd0ecae202df7eb77e2dffb80aa9c
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2012-02-06 16:45:58 +0000

    Readding Cosmos compiler profiler. Still needs fixed up to match current code.

---
+++ source2/IL2CPU/Cosmos.IL2CPU.Profiler/Assembler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.IL2CPU.Profiler {
+  public class Assembler : Cosmos.IL2CPU.Assembler {
+
+    protected override void InitILOps() {
+      var xILOp = new ILOp(this);
+      // Don't change the type in the foreach to a var, its necessary as it is now
+      // to typecast it, so we can then recast to an int.
+      foreach (ILOpCode.Code xCode in Enum.GetValues(typeof(ILOpCode.Code))) {
+        int xCodeValue = (int)xCode;
+        if (xCodeValue <= 0xFF) {
+          mILOpsLo[xCodeValue] = xILOp;
+        } else {
+          mILOpsHi[xCodeValue & 0xFF] = xILOp;
+        }
+      }
+    }
+
+  }
+}
---
+++ source2/IL2CPU/Cosmos.IL2CPU.Profiler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Cosmos.IL2CPU.Profiler {
+  class Program {
+
+    // This program profiles the scanning engine.
+    // In the future it may profile other aspects as well.
+
+    static void Main(string[] args) {
+      DoScan();
+    }
+
+    private static void DoScan() {
+      var xSW = new Stopwatch();
+      xSW.Start();
+
+      var xAsmblr = new Assembler();
+      var xScanner = new ILScanner(xAsmblr);
+      //TODO: Add plugs into the scanning equation to profile scanning them too
+      var xEntryPoint = typeof(Program).GetMethod("ScannerEntryPoint", BindingFlags.NonPublic | BindingFlags.Static);
+      xScanner.Execute(xEntryPoint);
+
+      xSW.Stop();
+      Console.WriteLine("Total time : {0}", xSW.Elapsed);
+      Console.WriteLine("Method count: {0}", xScanner.MethodCount);
+      //Console.WriteLine("Instruction count: {0}", xScanner.InstructionCount);
+    }
+
+    // This is a dummy entry point for the scanner to start at.
+    // Its not even a Cosmos app, just a standard Windows console app,
+    // but that fine for the scanner profiling as it does 
+    // not actually compile it.
+    private static void ScannerEntryPoint() {
+      Console.WriteLine("Hello, World!");
+      var xInt = 0;
+      object xObj = xInt;
+      xObj.ToString();
+    }
+
+  }
+}
