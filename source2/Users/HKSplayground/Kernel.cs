ef77da1c492abd106a97f4d9ccd0e9801d0f7eaf
HKS_cp <804d57068da1b85c21a2aa7102263f0b712556b1DWcdp7Ps>
2012-02-14 09:32:32 +0000

    Added my own playground

---
+++ source2/Users/HKSplayground/Kernel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace HKSplayground
+{
+    public class Kernel : Sys.Kernel
+    {
+        byte[] ba = {1,2,3};
+
+        protected override void BeforeRun()
+        {
+            Cosmos.Hardware2.PCIBus.Init();
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+        int i = 1;
+        protected override void Run()
+        {
+            while (true)
+            {
+                if (i == 1)
+                {
+                    Cosmos.Hardware2.Network.Devices.AMDPCNetII.AMDPCNet.FindAll();
+                    i++;
+                }
+                else
+                {
+                }
+            }
+        }
+
+    }
+}
