13af305d80f31881053eb6ee109e09450dcbea3e
Trivalik_cp <42497cfff885d3ca0e6fda54fb6262dd42101bd5sx56jUzf>
2012-02-16 20:31:17 +0000

    optimize Ldflda

--- source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Ldflda.cs
+++ source2/IL2CPU/Cosmos.IL2CPU.X86/IL/Ldflda.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using CPUx86 = Cosmos.Assembler.x86;
 using Cosmos.Assembler;
+
 namespace Cosmos.IL2CPU.X86.IL
 {
     [Cosmos.IL2CPU.OpCode( ILOpCode.Code.Ldflda )]
@@ -32,71 +33,19 @@ namespace Cosmos.IL2CPU.X86.IL
           if (xNeedsGC) {
             xExtraOffset = 12;
           }
-          uint xOffset;// = 0;
 
           var xActualOffset = xFieldInfo.Offset + xExtraOffset;
           var xSize = xFieldInfo.Size;
 
-          new CPUx86.Pop { DestinationReg = CPUx86.Registers.EAX };
-
-          new CPUx86.Add { DestinationReg = CPUx86.Registers.EAX, SourceValue = (uint)(xActualOffset) };
           Assembler.Stack.Pop();
-          Assembler.Stack.Push(new StackContents.Item(4, xType));
-          if(aDerefValue && xFieldInfo.IsExternalValue )
-          {
-              new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX, DestinationIsIndirect = true };
-          }
-          else
-          {
-            new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
-          }
+		  Assembler.Stack.Push(new StackContents.Item(4, xType));
+			if(aDerefValue && xFieldInfo.IsExternalValue) {
+				new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, 
+					SourceReg = CPUx86.Registers.ESP, SourceIsIndirect = true, SourceDisplacement = (int)xActualOffset };
+				new CPUx86.Mov { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, SourceReg = CPUx86.Registers.EAX };
+			}
+			else
+				new CPUx86.Add { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, SourceValue = (uint)(xActualOffset) };
         }
-
-
-        // using System;
-        // using System.Collections.Generic;
-        // using System.IO;
-        // 
-        // 
-        // using CPUx86 = Cosmos.Assembler.x86;	    
-        // using System.Reflection;
-        // using Cosmos.IL2CPU.Compiler;
-        // 
-        // namespace Cosmos.IL2CPU.IL.X86 {
-        // 	[Cosmos.Assembler.OpCode(OpCodeEnum.Ldflda)]
-        // 	public class Ldflda: Op {
-        //         private Type mType;
-        // 		private TypeInformation mTypeInfo;
-        // 		private TypeInformation.Field mField;
-        //         private string mFieldId;
-        //         //public static void ScanOp(ILReader aReader, MethodInformation aMethodInfo, SortedList<string, object> aMethodData)
-        //         //{
-        //         //    FieldInfo xField = aReader.OperandValueField;
-        //         //    if (xField == null)
-        //         //    {
-        //         //        throw new Exception("Field not found!");
-        //         //    }
-        //         //    Engine.RegisterType(xField.DeclaringType);
-        //         //    Engine.RegisterType(xField.FieldType);
-        //         //}
-        // 
-        // 		public Ldflda(ILReader aReader, MethodInformation aMethodInfo)
-        // 			: base(aReader, aMethodInfo) {
-        // 			FieldInfo xField = aReader.OperandValueField;
-        // 			if (xField == null) {
-        // 					throw new Exception("Field not found!");
-        // 			}
-        // 			mFieldId = xField.GetFullName();
-        //             mType = xField.DeclaringType;
-        // 		}
-        // 
-        // 		public override void DoAssemble() {
-        //             mTypeInfo = GetService<IMetaDataInfoService>().GetTypeInfo(mType);
-        //             mField = mTypeInfo.Fields[mFieldId];
-        // 			Ldflda(Assembler, mTypeInfo, mField);
-        // 		}
-        // 	}
-        // }
-
     }
 }
