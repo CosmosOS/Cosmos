2ed92546d95ad2ff03d7506caf5604eee5a44258
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2012-06-09 14:24:31 +0000

    X# Formatter

--- source2/VSIP/Cosmos.VS.XSharp/Formatter/Classifier.cs
+++ source2/VSIP/Cosmos.VS.XSharp/Formatter/Classifier.cs
@@ -7,46 +7,58 @@ using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
 
 namespace Cosmos.VS.XSharp {
-  /// This class causes a classifier to be added to the set of classifiers. Since 
-  /// the content type is set to "text", this classifier applies to all text files
+  [Export(typeof(EditorFormatDefinition))]
+  [ClassificationType(ClassificationTypeNames = "XSharp")]
+  [Name("XSharp")]
+  [UserVisible(true)]
+  [Order(Before = Priority.Default)]
+  internal sealed class XSharpFormat : ClassificationFormatDefinition {
+    public XSharpFormat() {
+      this.DisplayName = "XSharp"; //human readable version of the name
+      this.BackgroundColor = Colors.BlueViolet;
+      this.TextDecorations = System.Windows.TextDecorations.Underline;
+    }
+  }
+
+  // This class causes a classifier to be added to the set of classifiers
   [Export(typeof(IClassifierProvider))]
   [ContentType("XSharp")]
   internal class XSharpProvider : IClassifierProvider {
-    /// Import the classification registry to be used for getting a reference
-    /// to the custom classification type later.
+    // Import the classification registry to be used for getting a reference
+    // to the custom classification type later.
     [Import]
     internal IClassificationTypeRegistryService ClassificationRegistry = null; // Set via MEF
 
-    public IClassifier GetClassifier(ITextBuffer buffer) {
-      return buffer.Properties.GetOrCreateSingletonProperty<XSharp>(delegate { return new XSharp(ClassificationRegistry); });
+    public IClassifier GetClassifier(ITextBuffer aBuffer) {
+      return aBuffer.Properties.GetOrCreateSingletonProperty<XSharp>(delegate { 
+        return new XSharp(ClassificationRegistry); 
+      });
     }
   }
 
   // Classifier that classifies all text as an instance of the OrinaryClassifierType
   class XSharp : IClassifier {
-    IClassificationType _classificationType;
+    IClassificationType mClassificationType;
 
-    internal XSharp(IClassificationTypeRegistryService registry) {
-      _classificationType = registry.GetClassificationType("XSharp");
+    internal XSharp(IClassificationTypeRegistryService aRegistry) {
+      mClassificationType = aRegistry.GetClassificationType("XSharp");
     }
 
-    /// This method scans the given SnapshotSpan for potential matches for this classification.
-    /// In this instance, it classifies everything and returns each span as a new ClassificationSpan.
-    /// <param name="trackingSpan">The span currently being classified</param>
-    /// <returns>A list of ClassificationSpans that represent spans identified to be of this classification</returns>
-    public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span) {
+    // This method scans the given SnapshotSpan for potential matches for this classification.
+    // In this instance, it classifies everything and returns each span as a new ClassificationSpan.
+    // <param name="trackingSpan">The span currently being classified</param>
+    // <returns>A list of ClassificationSpans that represent spans identified to be of this classification</returns>
+    public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan aSpan) {
       //create a list to hold the results
-      var classifications = new List<ClassificationSpan>();
-      classifications.Add(new ClassificationSpan(new SnapshotSpan(span.Snapshot, new Span(span.Start, span.Length)),
-                                                     _classificationType));
-      return classifications;
+      var xClassifications = new List<ClassificationSpan>();
+      xClassifications.Add(new ClassificationSpan(new SnapshotSpan(aSpan.Snapshot, new Span(aSpan.Start, aSpan.Length)), mClassificationType));
+      return xClassifications;
     }
 
-#pragma warning disable 67
+    //#pragma warning disable 67
     // This event gets raised if a non-text change would affect the classification in some way,
-    // for example typing /* would cause the classification to change in C# without directly
-    // affecting the span.
+    // for example typing /* would cause the classification to change in C# without directly affecting the span.
     public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
-#pragma warning restore 67
+    //#pragma warning restore 67
   }
 }
--- source2/VSIP/Cosmos.VS.XSharp/Formatter/Registration.cs
+++ source2/VSIP/Cosmos.VS.XSharp/Formatter/Registration.cs
@@ -4,22 +4,6 @@ using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
 
 namespace Cosmos.VS.XSharp {
-  /// Defines an editor format for the XSharp type that has a purple background
-  /// and is underlined.
-  [Export(typeof(EditorFormatDefinition))]
-  [ClassificationType(ClassificationTypeNames = "XSharp")]
-  [Name("XSharp")]
-  [UserVisible(true)]
-  [Order(Before = Priority.Default)] //set the priority to be after the default classifiers
-  internal sealed class XSharpFormat : ClassificationFormatDefinition {
-    /// Defines the visual format for the "Cosmos.VS.XSharp" classification type
-    public XSharpFormat() {
-      this.DisplayName = "XSharp"; //human readable version of the name
-      this.BackgroundColor = Colors.BlueViolet;
-      this.TextDecorations = System.Windows.TextDecorations.Underline;
-    }
-  }
-
   internal static class XSharpClassificationDefinition {
     /// Defines the "XSharp" classification type.
     [Export(typeof(ClassificationTypeDefinition))]
