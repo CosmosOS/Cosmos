db5fb694004ad479586dd759d10ff3b67e0f0e10
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2012-06-25 01:29:05 +0000

    ISO

--- source2/Build/Cosmos.Build.Common/IsoMaker.cs
+++ source2/Build/Cosmos.Build.Common/IsoMaker.cs
@@ -14,20 +14,23 @@ namespace Cosmos.Build.Common {
       }
     }
 
-    static public void Generate(string aBuildPath, string xInputPathname, string aIso) {
-      string xPath = Path.Combine(aBuildPath, @"ISO\");
-      RemoveFile(aIso);
-      RemoveFile(Path.Combine(xPath, "output.bin"));
+    static public void Generate(string aBuildPath, string xInputPathname, string aIsoPathname) {
+      string xPath = Path.Combine(aBuildPath, @"ISO");
+      RemoveFile(aIsoPathname);
+      
+      // We copy and rename in the process to output.bin becaue the .cfg is currently
+      // hardcoded to output.bin.
+      string xOutputBin = Path.Combine(xPath, "output.bin");
+      RemoveFile(xOutputBin);
+      File.Copy(xInputPathname, xOutputBin);
 
-      File.Copy(xInputPathname, Path.Combine(xPath, "output.bin"));
-      File.SetAttributes(Path.Combine(xPath, "isolinux.bin"), FileAttributes.Normal);
+      string xIsoLinux = Path.Combine(xPath, "isolinux.bin");
+      File.SetAttributes(xIsoLinux, FileAttributes.Normal);
 
       var xOptions = new Options() {
         BootLoadSize = 4,
-        // Why does this use CurrentDir? Is this some MS build thing? Why not above where it deletes it then?
-        // TOOD: Investigate and fix this
-        IsoFileName = Path.Combine(Environment.CurrentDirectory, aIso),
-        BootFileName = Path.Combine(xPath, "isolinux.bin"),
+        IsoFileName = aIsoPathname,
+        BootFileName = xIsoLinux,
         BootInfoTable = true
       };
       // TODO - Use move or see if we can do this without copying first the output.bin as they will start to get larger
