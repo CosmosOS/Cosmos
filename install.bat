97be91192bb42158c4c445215fc8bd5de46b2c6c
kudzu_cp <6d05c8c8ef5431987001abfdb2eadc9593ac9498>
2012-07-17 02:52:09 +0000

    install.bat now passes params to builder

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ del source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.*
 
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "source2\Builder.sln" /maxcpucount /verbosity:normal /nologo /p:Configuration=Debug /p:Platform=x86
 
-source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe
+source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 pause
