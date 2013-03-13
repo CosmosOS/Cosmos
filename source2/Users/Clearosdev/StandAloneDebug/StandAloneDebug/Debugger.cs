4cb0fe5ad6b463172f4961d183a8b1ac410066fb
clearosdev_cp <fe3c3f05928459428007c4c42e0e05599494e9adrQMm2sAF>
2013-03-13 23:52:03 +0000

    I added a user playground for myself and started prototyping a stand alone debugger. Right now it just deals with command line arguments.

---
+++ source2/Users/Clearosdev/StandAloneDebug/StandAloneDebug/Debugger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StandAloneDebug
+{
+    class Debugger
+    {
+        public static void Run()
+        {
+            if (Program.type == Program.types.Vmware)
+            {
+                //execute vmware debugger
+            }
+            else if (Program.type == Program.types.Broche)
+            {
+                //execute broche debugger`
+            }
+        }
+
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+            if (Program.printtofile == true)
+            {
+                Program.output += message + '\n';
+            }
+        }
+    }
+}
