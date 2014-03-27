67cec81c4bef3bc0f5821df344e3d9bf734e8627
Trivalik_cp <42497cfff885d3ca0e6fda54fb6262dd42101bd5sx56jUzf>
2014-03-27 19:20:57 +0000

    install.bat: activate multi cpu for build the Builder, use -NOTASK if Windows 8.1 is detected, because UAC run works

--- install.bat
+++ install.bat
@@ -1,11 +1,16 @@
 @REM Delete so if builder fails it wont exist
 del source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.*
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "source2\Builder.sln" /nologo /p:Configuration=Debug /p:Platform=x86
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "source2\Builder.sln" /nologo /maxcpucount /p:Configuration=Debug /p:Platform=x86
 
 @if not exist source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe goto error
 
-start source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe -bat %1 %2 %3 %4 %5 %6 %7 %8 %9
+@for /f "tokens=4-6 delims=.] " %%i in ('ver') do set VERSION=%%i.%%j.%%k
+
+@rem Windows 8.1 detect UAC, no task needed
+@if "%version%" == "6.3.9600" set TASK=-NOTASK
+
+start source2\Build\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe -bat %TASK% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 @exit
 
