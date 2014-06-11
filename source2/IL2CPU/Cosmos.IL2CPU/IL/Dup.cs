b4e746fcf8a501995ddb0c2bcb659f8a448869a2
AtomOS_cp <5a25c3af533aeff42bf19dd158c40bd93df49730ThEKjtWF>
2014-06-11 05:38:33 +0000

    Made Dup IL working for stack content size greater than 4

--- source2/IL2CPU/Cosmos.IL2CPU/IL/Dup.cs
+++ source2/IL2CPU/Cosmos.IL2CPU/IL/Dup.cs
@@ -3,21 +3,22 @@ using CPUx86 = Cosmos.Assembler.x86;
 
 namespace Cosmos.IL2CPU.X86.IL
 {
-    [Cosmos.IL2CPU.OpCode( ILOpCode.Code.Dup )]
+    [Cosmos.IL2CPU.OpCode(ILOpCode.Code.Dup)]
     public class Dup : ILOp
     {
-        public Dup( Cosmos.Assembler.Assembler aAsmblr )
-            : base( aAsmblr )
+        public Dup(Cosmos.Assembler.Assembler aAsmblr)
+            : base(aAsmblr)
         {
         }
 
-        public override void Execute( MethodInfo aMethod, ILOpCode aOpCode )
+        public override void Execute(MethodInfo aMethod, ILOpCode aOpCode)
         {
             var xStackContent = Assembler.Stack.Peek();
-            for( int i = 0; i < ( ( xStackContent.Size / 4 ) + ( xStackContent.Size % 4 == 0 ? 0 : 1 ) ); i++ )
+            var StackSize = (int)((xStackContent.Size / 4) + (xStackContent.Size % 4 == 0 ? 0 : 1));
+
+            for (int i = StackSize; i > 0; i--)
             {
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ESP, SourceDisplacement = i * 4, SourceIsIndirect = true };
-                new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
+                new CPUx86.Push { DestinationReg = CPUx86.Registers.ESP, DestinationIsIndirect = true, DestinationDisplacement = (int)((StackSize - 1) * 4) };
             }
             Assembler.Stack.Push(xStackContent.Size, xStackContent.ContentType);
         }
