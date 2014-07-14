ececfdbfc88ab92e94477895cab60191c8c5b760
Trivalik_cp <42497cfff885d3ca0e6fda54fb6262dd42101bd5sx56jUzf>
2014-07-14 18:54:02 +0000

    Code.inc: remove old visual studio check, fix express installation of zip files

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ del source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.*
 
 @for /f "tokens=4-6 delims=.] " %%i in ('ver') do set VERSION=%%i.%%j.%%k
 
-start source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe -bat %TASK% %1 %2 %3 %4 %5 %6 %7 %8 %9
+start source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe -bat %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 @exit
 
