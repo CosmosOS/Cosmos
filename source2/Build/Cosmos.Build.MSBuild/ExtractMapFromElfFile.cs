336c3afa8aee2a01ee2d4aa15c49d393edf35146
jonathonchase_cp <b75470b9425e41e9e7e62afbe8a600f743a8f7f8bBEYcVFp>
2014-07-18 16:51:27 +0000

    Guid.NewGuid() Replaced with prefixed Guid_NewGuid in ExtractMapFromElfFile

--- source2/Build/Cosmos.Build.MSBuild/ExtractMapFromElfFile.cs
+++ source2/Build/Cosmos.Build.MSBuild/ExtractMapFromElfFile.cs
@@ -14,163 +14,187 @@ using System.Data.SQLite;
 using Cosmos.Debug.Common;
 
 namespace Cosmos.Build.MSBuild {
-  public class ExtractMapFromElfFile : BaseToolTask {
-    [Required]
-    public string InputFile { get; set; }
+    public class ExtractMapFromElfFile : BaseToolTask {
+        [Required]
+        public string InputFile { get; set; }
 
-    [Required]
-    public string DebugInfoFile { get; set; }
+        [Required]
+        public string DebugInfoFile { get; set; }
 
-    [Required]
-    public string WorkingDir { get; set; }
+        [Required]
+        public string WorkingDir { get; set; }
 
-    [Required]
-    public string CosmosBuildDir { get; set; }
+        [Required]
+        public string CosmosBuildDir { get; set; }
 
-    #region Old NASM Map parser
-    //public static List<Label> ParseMapFile(String buildPath) {
-    //  var xSourceStrings = File.ReadAllLines(Path.Combine(buildPath, "main.map"));
-    //  var xSource = new List<Label>();
-    //  uint xIndex = 0;
-    //  for (xIndex = 0; xIndex < xSourceStrings.Length; xIndex++) {
-    //    if (xSourceStrings[xIndex].StartsWith("Real ")) {
-    //      // further check it:
-    //      //Virtual   Name"))
-    //      if (!xSourceStrings[xIndex].Substring(4).TrimStart().StartsWith("Virtual ")
-    //          || !xSourceStrings[xIndex].EndsWith(" Name")) {
-    //        continue;
-    //      }
-    //      xIndex++;
-    //      break;
-    //    }
-    //  }
-    //  for (; xIndex < xSourceStrings.Length; xIndex++) {
-    //    string xLine = xSourceStrings[xIndex];
-    //    var xLineParts = xLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        #region Old NASM Map parser
+        //public static List<Label> ParseMapFile(String buildPath) {
+        //  var xSourceStrings = File.ReadAllLines(Path.Combine(buildPath, "main.map"));
+        //  var xSource = new List<Label>();
+        //  uint xIndex = 0;
+        //  for (xIndex = 0; xIndex < xSourceStrings.Length; xIndex++) {
+        //    if (xSourceStrings[xIndex].StartsWith("Real ")) {
+        //      // further check it:
+        //      //Virtual   Name"))
+        //      if (!xSourceStrings[xIndex].Substring(4).TrimStart().StartsWith("Virtual ")
+        //          || !xSourceStrings[xIndex].EndsWith(" Name")) {
+        //        continue;
+        //      }
+        //      xIndex++;
+        //      break;
+        //    }
+        //  }
+        //  for (; xIndex < xSourceStrings.Length; xIndex++) {
+        //    string xLine = xSourceStrings[xIndex];
+        //    var xLineParts = xLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-    //    if (xLineParts.Length == 3) {
-    //      uint xAddress = UInt32.Parse(xLineParts[0], System.Globalization.NumberStyles.HexNumber);
-    //      xSource.Add(new Label() {
-    //        LABELNAME = xLineParts[2],
-    //        ADDRESS = xAddress
-    //      });
-    //    }
-    //  }
-    //  return xSource;
-    //}
-    #endregion
+        //    if (xLineParts.Length == 3) {
+        //      uint xAddress = UInt32.Parse(xLineParts[0], System.Globalization.NumberStyles.HexNumber);
+        //      xSource.Add(new Label() {
+        //        LABELNAME = xLineParts[2],
+        //        ADDRESS = xAddress
+        //      });
+        //    }
+        //  }
+        //  return xSource;
+        //}
+        #endregion
 
-    public override bool Execute()
-    {
-        var sw = Stopwatch.StartNew();
-        try
+        public override bool Execute()
         {
-            // Important! A given address can have more than one label.
-            // Do NOT filter by duplicate addresses as this causes serious lookup problems.
-            string xFile = RunObjDump();
-
-            using (var xDebugInfo = new DebugInfo(DebugInfoFile))
+            var sw = Stopwatch.StartNew();
+            try
             {
-                // In future instead of loading all labels, save indexes to major labels but not IL.ASM labels.
-                // Then code can find major lables, and use position markers into the map file to parse in between 
-                // as needed.
-                using (var xMapReader = new StreamReader(xFile))
+                // Important! A given address can have more than one label.
+                // Do NOT filter by duplicate addresses as this causes serious lookup problems.
+                string xFile = RunObjDump();
+
+                using (var xDebugInfo = new DebugInfo(DebugInfoFile))
                 {
-                    var xLabels = new List<Label>();
-                    bool xListStarted = false;
-                    string xLine;
-                    while ((xLine = xMapReader.ReadLine()) != null)
+                    // In future instead of loading all labels, save indexes to major labels but not IL.ASM labels.
+                    // Then code can find major lables, and use position markers into the map file to parse in between 
+                    // as needed.
+                    using (var xMapReader = new StreamReader(xFile))
                     {
-                        if (String.IsNullOrEmpty(xLine))
+                        var xLabels = new List<Label>();
+                        bool xListStarted = false;
+                        string xLine;
+                        while ((xLine = xMapReader.ReadLine()) != null)
                         {
-                            continue;
-                        }
-                        else if (!xListStarted)
-                        {
-                            // Find start of the data
-                            if (xLine == "SYMBOL TABLE:")
+                            if (String.IsNullOrEmpty(xLine))
                             {
-                                xListStarted = true;
+                                continue;
                             }
-                            continue;
-                        }
+                            else if (!xListStarted)
+                            {
+                                // Find start of the data
+                                if (xLine == "SYMBOL TABLE:")
+                                {
+                                    xListStarted = true;
+                                }
+                                continue;
+                            }
 
-                        uint xAddress;
-                        try
-                        {
-                            xAddress = UInt32.Parse(xLine.Substring(0, 8), Globalization.NumberStyles.HexNumber);
-                        }
-                        catch (Exception ex)
-                        {
-                            LogError("Error processing line '" + xLine + "' " + ex.Message);
-                            throw;
-                        }
+                            uint xAddress;
+                            try
+                            {
+                                xAddress = UInt32.Parse(xLine.Substring(0, 8), Globalization.NumberStyles.HexNumber);
+                            }
+                            catch (Exception ex)
+                            {
+                                LogError("Error processing line '" + xLine + "' " + ex.Message);
+                                throw;
+                            }
 
-                        string xSection = xLine.Substring(17, 5);
-                        if (xSection != ".text" && xSection != ".data")
-                        {
-                            continue;
-                        }
-                        string xLabel = xLine.Substring(32);
-                        if (xLabel == xSection)
-                        {
-                            // Non label, skip
-                            continue;
-                        }
+                            string xSection = xLine.Substring(17, 5);
+                            if (xSection != ".text" && xSection != ".data")
+                            {
+                                continue;
+                            }
+                            string xLabel = xLine.Substring(32);
+                            if (xLabel == xSection)
+                            {
+                                // Non label, skip
+                                continue;
+                            }
 
-                        Guid xGuid;
-                        // See if label has an embedded GUID. If so, use it.
-                        if (xLabel.StartsWith("GUID_"))
-                        {
-                            xGuid = new Guid(xLabel.Substring(5));
-                        }
-                        else
-                        {
-                            xGuid = Guid.NewGuid();
-                        }
+                            Guid xGuid;
+                            // See if label has an embedded GUID. If so, use it.
+                            if (xLabel.StartsWith("GUID_"))
+                            {
+                                xGuid = new Guid(xLabel.Substring(5));
+                            }
+                            else
+                            {
+                                xGuid = Guid_NewGuid();
+                            }
 
-                        xLabels.Add(new Label()
-                        {
-                            ID = xGuid,
-                            Name = xLabel,
-                            Address = xAddress
-                        });
-                        xDebugInfo.AddLabels(xLabels);
+                            xLabels.Add(new Label()
+                            {
+                                ID = xGuid,
+                                Name = xLabel,
+                                Address = xAddress
+                            });
+                            xDebugInfo.AddLabels(xLabels);
+                        }
+                        xDebugInfo.AddLabels(xLabels, true);
                     }
-                    xDebugInfo.AddLabels(xLabels, true);
                 }
+
+                return true;
+            }
+            catch (Exception E)
+            {
+                LogError("An error occurred: {0}", E.ToString());
+                return false;
+            }
+            finally
+            {
+                sw.Stop();
+                Log.LogMessage(MessageImportance.High, "Extracting Map file took {0}", sw.Elapsed);
+            }
+        }
+
+        private string RunObjDump() {
+            var xMapFile = Path.ChangeExtension(InputFile, "map");
+            File.Delete(xMapFile);
+            if (File.Exists(xMapFile)) {
+                throw new Exception("Could not delete " + xMapFile);
             }
 
-            return true;
+            var xTempBatFile = Path.Combine(WorkingDir, "ExtractElfMap.bat");
+            File.WriteAllText(xTempBatFile, "@ECHO OFF\r\n\"" + Path.Combine(CosmosBuildDir, @"tools\cygwin\objdump.exe") + "\" --wide --syms \"" + InputFile + "\" > \"" + Path.GetFileName(xMapFile) + "\"");
+
+            if (!ExecuteTool(WorkingDir, xTempBatFile, "", "objdump")) {
+                throw new Exception("Error extracting map from " + InputFile);
+            }
+            File.Delete(xTempBatFile);
+
+            return xMapFile;
         }
-        catch (Exception E)
-        {
-            LogError("An error occurred: {0}", E.ToString());
-            return false;
-        }
-        finally
-        {
-            sw.Stop();
-            Log.LogMessage(MessageImportance.High, "Extracting Map file took {0}", sw.Elapsed);
+        private static UInt64 mLastGuid = 0;
+        private static Guid Guid_NewGuid() {
+            // Old code: 
+            //return Guid.NewGuid();
+            // Do NOT use Guid.NewGuid(). During compilation we're generating
+            // about 60 milion guids. Guid.NewGuid slows down compilation significantly (about half the time!)
+
+            mLastGuid++;
+            var bytes = new byte[16];
+            bytes[0] = (byte)(mLastGuid >> 56);
+            bytes[1] = (byte)(mLastGuid >> 48);
+            bytes[2] = (byte)(mLastGuid >> 40);
+            bytes[3] = (byte)(mLastGuid >> 32);
+            bytes[4] = (byte)(mLastGuid >> 24);
+            bytes[5] = (byte)(mLastGuid >> 16);
+            bytes[6] = (byte)(mLastGuid >> 8);
+            bytes[7] = (byte)(mLastGuid);
+            //Adds an additional element to help prevent collisions between this function and it's equivelant in
+            //Cosmos.Debug.Common.DebugInfo
+            bytes[15] = (byte)(1);
+
+            var r = new Guid(bytes);
+            return r;
         }
     }
-
-    private string RunObjDump() {
-      var xMapFile = Path.ChangeExtension(InputFile, "map");
-      File.Delete(xMapFile);
-      if (File.Exists(xMapFile)) {
-        throw new Exception("Could not delete " + xMapFile);
-      }
-
-      var xTempBatFile = Path.Combine(WorkingDir, "ExtractElfMap.bat");
-      File.WriteAllText(xTempBatFile, "@ECHO OFF\r\n\"" + Path.Combine(CosmosBuildDir, @"tools\cygwin\objdump.exe") + "\" --wide --syms \"" + InputFile + "\" > \"" + Path.GetFileName(xMapFile) + "\"");
-
-      if (!ExecuteTool(WorkingDir, xTempBatFile, "", "objdump")) {
-        throw new Exception("Error extracting map from " + InputFile);
-      }
-      File.Delete(xTempBatFile);
-
-      return xMapFile;
-    }
-  }
 }
