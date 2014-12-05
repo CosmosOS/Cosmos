7c4a29b6f6727094b33b83e6f3a5c1b52985c2b8
Matthijs ter Woord <matthijsterwoord@gmail.com>
2014-12-05 16:02:06 +0100

    Rings verification
    - add ring attribute and marking to kernel libraries

---
+++ source/Cosmos.Common/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Common
+{
+    public enum Ring
+    {
+        User = 0,
+        System = 1,
+        HAL = 2,
+        Core = 3,
+    }
+}
