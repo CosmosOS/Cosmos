fe9dac7e13cc289dafb8dcb96e3949dda0bfd6d8
Matthijs ter Woord <matthijs@terwoord.eu>
2015-01-15 12:24:17 +0100

    Missing file.

---
+++ Users/Matthijs/DebugCompiler/TestCompilerExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.IL2CPU;
+
+namespace DebugCompiler
+{
+    public class TestCompilerExtension: CompilerExtensionBase
+    {
+        public override bool TryCreateAppAssembler(byte debugCom, string assemblerLog, out AppAssembler result)
+        {
+            return base.TryCreateAppAssembler(debugCom, assemblerLog, out result);
+        }
+    }
+}
