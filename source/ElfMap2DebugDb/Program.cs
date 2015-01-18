b235faa3d6ae2a49c39df921905b0051ee83d1e3
Andrey Kurdyumov <kant2002@gmail.com>
2015-01-19 01:03:11 +0600

    Add application which perform similar task as ExtractMapFromElfFile This application is used for the performance tuning of the execution process.

---
+++ source/ElfMap2DebugDb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Cosmos.Debug.Common;
+
+namespace ElfMap2DebugDb
+{
+    /// <summary>
+    /// Application which is populate debug database with symbols from the ELF map file.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Entry point for the application.
+        /// </summary>
+        /// <param name="args">Parameters passed from the command line.</param>
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                PrintHelp();
+                return;
+            }
+
+            var xMapFile = args[0];
+            var xDebugDatabase = args[1];
+            ObjDump.ExtractMapSymbolsForElfFile(xDebugDatabase, xMapFile);
+        }
+
+        /// <summary>
+        /// Prints help page for the application.
+        /// </summary>
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: ElfMap2DebugDb.exe <mapFile> <debugDatabaseFile>");
+        }
+    }
+}
