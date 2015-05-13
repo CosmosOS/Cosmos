8ea60202a3a436271ad2ea57f4c9f8ffe170f396
Andrey Kurdyumov <kant2002@gmail.com>
2015-05-13 14:47:18 +0600

    Add the helper class

---
+++ Tests/Cosmos.Common.Tests/ToHexStringTest.cs
@@ -0,0 +1,47 @@
+﻿namespace Cosmos.Common.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Cosmos.Common.Extensions;
+
+    [TestClass]
+    public class ToHexStringTest
+    {
+        /// <summary>
+        /// Test to hex conversion method.
+        /// </summary>
+        [TestMethod]
+        public void TestToHexUnit32()
+        {
+            uint testValue = 32;
+            Assert.AreEqual("00000020", testValue.ToHex());
+            Assert.AreEqual("00000020", testValue.ToHex(8));
+            Assert.AreEqual("0000020", testValue.ToHex(7));
+            Assert.AreEqual("0020", testValue.ToHex(4));
+            Assert.AreEqual("20", testValue.ToHex(2));
+        }
+
+        /// <summary>
+        /// Test to hex conversion method.
+        /// </summary>
+        [TestMethod]
+        public void TestToHexUnit16()
+        {
+            ushort testValue = 34;
+            Assert.AreEqual("0022", testValue.ToHex());
+            Assert.AreEqual("0022", testValue.ToHex(4));
+            Assert.AreEqual("22", testValue.ToHex(2));
+        }
+
+        /// <summary>
+        /// Test to hex conversion method.
+        /// </summary>
+        [TestMethod]
+        public void TestToHexUnit8()
+        {
+            byte testValue = 34;
+            Assert.AreEqual("22", testValue.ToHex());
+            Assert.AreEqual("0022", testValue.ToHex(4));
+            Assert.AreEqual("22", testValue.ToHex(2));
+        }
+    }
+}
---
+++ source/Cosmos.Common/NumberHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace Cosmos.Common
+{
+  using System;
+  using Cosmos.Common.Extensions;
+
+  /// <summary>
+  /// Helper class for working with numbers.
+  /// </summary>
+  public static class NumberHelper
+  {
+    /// <summary>
+    /// Write number to console.
+    /// </summary>
+    /// <param name="aValue">A value to print.</param>
+    /// <param name="aZeroFill">A value indicating whether strarting zeros should be present.</param>
+    public static void WriteNumber(uint aValue, bool aZeroFill)
+    {
+      if (aZeroFill)
+      {
+        Console.WriteLine("0x" + aValue.ToHex());
+      }
+      else
+      {
+        Console.WriteLine("0x" + aValue.ToHex().TrimStart('0'));
+      }
+    }
+  }
+}
