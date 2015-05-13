c26d29d1133cbb8697a8951b20eb2a3e01f344f4
Andrey Kurdyumov <kant2002@gmail.com>
2015-05-13 15:13:46 +0600

    Use shared code where possible

--- source/Cosmos.Core/Heap.cs
+++ source/Cosmos.Core/Heap.cs
@@ -106,6 +106,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Cosmos.Common;
 
 namespace Cosmos.Core
 {
@@ -149,77 +150,6 @@ namespace Cosmos.Core
             CPU.ZeroFill(aStartAddress, aLength);
         }
 
-        private static void WriteNumber(uint aNumber, byte aBits)
-        {
-            //uint xValue = aNumber;
-            //byte xCurrentBits = aBits;
-            //Console.Write("0x");
-            //while (xCurrentBits >= 4)
-            //{
-            //    xCurrentBits -= 4;
-            //    byte xCurrentDigit = (byte)((xValue >> xCurrentBits) & 0xF);
-            //    string xDigitString = null;
-            //    switch (xCurrentDigit)
-            //    {
-            //        case 0:
-            //            xDigitString = "0";
-            //            goto default;
-            //        case 1:
-            //            xDigitString = "1";
-            //            goto default;
-            //        case 2:
-            //            xDigitString = "2";
-            //            goto default;
-            //        case 3:
-            //            xDigitString = "3";
-            //            goto default;
-            //        case 4:
-            //            xDigitString = "4";
-            //            goto default;
-            //        case 5:
-            //            xDigitString = "5";
-            //            goto default;
-            //        case 6:
-            //            xDigitString = "6";
-            //            goto default;
-            //        case 7:
-            //            xDigitString = "7";
-            //            goto default;
-            //        case 8:
-            //            xDigitString = "8";
-            //            goto default;
-            //        case 9:
-            //            xDigitString = "9";
-            //            goto default;
-            //        case 10:
-            //            xDigitString = "A";
-            //            goto default;
-            //        case 11:
-            //            xDigitString = "B";
-            //            goto default;
-            //        case 12:
-            //            xDigitString = "C";
-            //            goto default;
-            //        case 13:
-            //            xDigitString = "D";
-            //            goto default;
-            //        case 14:
-            //            xDigitString = "E";
-            //            goto default;
-            //        case 15:
-            //            xDigitString = "F";
-            //            goto default;
-            //        default:
-            //            if (xDigitString == null)
-            //            {
-            //                System.Diagnostics.Debugger.Break();
-            //            }
-            //            Console.Write(xDigitString);
-            //            break;
-            //    }
-            //}
-        }
-
         //private static bool mDebugDisplayInitialized = false;
 
         // this method displays the used/total memory of the heap on the first line of the text screen
@@ -235,10 +165,10 @@ namespace Cosmos.Core
             //        Console.CursorLeft = 0;
             //        Console.CursorTop = 0;
             //        Console.Write("[Heap Usage: ");
-            //        WriteNumber(mStartAddress,
+            //        NumberHelper.WriteNumber(mStartAddress,
             //                    32);
             //        Console.Write("/");
-            //        WriteNumber(mEndOfRam,
+            //        NumberHelper.WriteNumber(mEndOfRam,
             //                    32);
             //        Console.Write("] bytes");
             //        while (Console.CursorLeft < (Console.WindowWidth-1))
@@ -254,7 +184,7 @@ namespace Cosmos.Core
             //        int xOldPositionTop = Console.CursorTop;
             //        Console.CursorLeft = 13;
             //        Console.CursorTop = 0;
-            //        WriteNumber(mStartAddress,
+            //        NumberHelper.WriteNumber(mStartAddress,
             //                    32);
             //        Console.CursorLeft = xOldPositionLeft;
             //        Console.CursorTop = xOldPositionTop;
@@ -270,7 +200,7 @@ namespace Cosmos.Core
             if ((xTemp + aLength) > (mStart + mLength))
             {
                 Console.WriteLine("Too large memory block allocated!");
-                WriteNumber(aLength, 32);
+                NumberHelper.WriteNumber(aLength, 32);
                 while (true)
                     ;
             }
--- source/Cosmos.HAL/PciDevice.cs
+++ source/Cosmos.HAL/PciDevice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Cosmos.Core.IOGroup;
+using Cosmos.Common.Extensions;
 
 namespace Cosmos.HAL
 {
@@ -301,71 +302,7 @@ namespace Cosmos.HAL
 
         private static string ToHex(uint aNumber, byte aBits)
         {
-            string ret = "";
-            uint xValue = aNumber;
-            byte xCurrentBits = aBits;
-            ret += "0x";
-            while (xCurrentBits >= 4)
-            {
-                xCurrentBits -= 4;
-                byte xCurrentDigit = (byte)((xValue >> xCurrentBits) & 0xF);
-                string xDigitString = null;
-                switch (xCurrentDigit)
-                {
-                    case 0:
-                        xDigitString = "0";
-                        goto default;
-                    case 1:
-                        xDigitString = "1";
-                        goto default;
-                    case 2:
-                        xDigitString = "2";
-                        goto default;
-                    case 3:
-                        xDigitString = "3";
-                        goto default;
-                    case 4:
-                        xDigitString = "4";
-                        goto default;
-                    case 5:
-                        xDigitString = "5";
-                        goto default;
-                    case 6:
-                        xDigitString = "6";
-                        goto default;
-                    case 7:
-                        xDigitString = "7";
-                        goto default;
-                    case 8:
-                        xDigitString = "8";
-                        goto default;
-                    case 9:
-                        xDigitString = "9";
-                        goto default;
-                    case 10:
-                        xDigitString = "A";
-                        goto default;
-                    case 11:
-                        xDigitString = "B";
-                        goto default;
-                    case 12:
-                        xDigitString = "C";
-                        goto default;
-                    case 13:
-                        xDigitString = "D";
-                        goto default;
-                    case 14:
-                        xDigitString = "E";
-                        goto default;
-                    case 15:
-                        xDigitString = "F";
-                        goto default;
-                    default:
-                        ret += xDigitString;
-                        break;
-                }
-            }
-            return ret;
+            return "0x" + aNumber.ToHex(aBits / 4);
         }
     }
 }
