243417857ea4838618dd62821c633ef108a84c40
Matthijs ter Woord <matthijs@terwoord.eu>
2015-05-22 17:20:35 +0200

    Add plug for UIntPtr.ToString

---
+++ source/Cosmos.IL2CPU/CustomImplementation/System/UIntPtrImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Cosmos.IL2CPU.Plugs;
+
+namespace Cosmos.IL2CPU.CustomImplementation.System
+{
+  [Plug(Target = typeof(UIntPtr))]
+  public static class UIntPtrImpl
+  {
+    //  //[PlugMethod(Signature="System_String___System_IntPtr_ToString____")]
+    public static string ToString(UIntPtr aThis)
+    {
+      return "<UIntPtr>";
+    }
+    //}
+  }
+}
