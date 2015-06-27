21c0e3537f0aa3b65e55584519cce3014bec2130
Matthijs ter Woord <matthijsterwoord@gmail.com>
2015-06-27 16:37:59 +0200

    Test added for #126.

--- Tests/Cosmos.TestRunner/Program.cs
+++ Tests/Cosmos.TestRunner/Program.cs
@@ -14,10 +14,9 @@ namespace Cosmos.TestRunner.Console
             var xEngine = new Engine();
 
             xEngine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
-            //xEngine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
 
             // known bugs, therefor disabled for now:
-            //xEngine.AddKernel(typeof(Cosmos.Compiler.Tests.Interfaces.Kernel.Kernel).Assembly.Location);
+            //xEngine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
 
             xEngine.OutputHandler = new OutputHandlerXml(@"c:\data\CosmosTests.xml");
             //xEngine.OutputHandler = new OutputHandlerConsole();
---
+++ Tests/Staging/VGACompilerCrash/Kernel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.HAL;
+using Sys = Cosmos.System;
+
+namespace VGACompilerCrash
+{
+    public class Kernel : Sys.Kernel
+    {
+      public static VGAScreen screen = new VGAScreen();
+
+      public void startGraphics()
+      {
+        //screen.SetGraphicsMode(VGAScreen.ScreenSize.Size320x200,VGAScreen.ColorDepth.BitDepth8);
+        //screen.Clear(0);
+      }
+
+      protected override void BeforeRun()
+      {
+        Console.WriteLine("Successfully Loaded.");
+      }
+
+      protected override void Run()
+      {
+        Console.WriteLine("Welcome. Please login with the administrative credentials provided to you.");
+        //LOGIN
+        string username = "";
+        string password = "";
+        bool loggedIn = false;
+        int attempts = 0;
+        while (!loggedIn)
+        {
+          Console.Write("Username:");
+          username = Console.ReadLine();
+          Console.Write("Password:");
+          password = Console.ReadLine();
+          if (username == "root" && password == "password")
+            loggedIn = true;
+          else { attempts++; Console.WriteLine("Error: password mismatch. Try again."); }
+          if (attempts >= 3)
+          {
+            Console.WriteLine("Too many attempts. Please power down."); //add ACPI.Shutdown later
+            while (!loggedIn) { }
+          }
+        }
+        //END LOGIN
+        while (loggedIn)
+        {
+          Console.Write(">: ");
+          var input = Console.ReadLine();
+          if (input == "startx")
+          {
+            startGraphics();
+          }
+        }
+      }
+    }
+}
