d285220f1df34d8aa8c6e161c5c77c76a0ec22ef
Matthijs ter Woord <matthijsterwoord@gmail.com>
2015-07-23 00:21:14 +0200

    Building using vs2015 works now. Most likely doesn't work with 2013 anymore.

--- source/XSharp.VS/Package.cs
+++ source/XSharp.VS/Package.cs
@@ -17,36 +17,36 @@ namespace XSharp.VS {
   /// The minimum requirement for a class to be considered a valid package for Visual Studio
   /// is to implement the IVsPackage interface and register itself with the shell.
   /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-  /// to do it: it derives from the Package class that provides the implementation of the 
-  /// IVsPackage interface and uses the registration attributes defined in the framework to 
+  /// to do it: it derives from the Package class that provides the implementation of the
+  /// IVsPackage interface and uses the registration attributes defined in the framework to
   /// register itself and its components with the shell.
   //
   // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is a package.
-  [PackageRegistration(UseManagedResourcesOnly = true)]
-  // This attribute is used to register the informations needed to show the this package
-  // in the Help/About dialog of Visual Studio.
-  [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-  [Guid(Guids.guidCosmos_VS_XSharpPkgString)]
+  //[PackageRegistration(UseManagedResourcesOnly = true)]
+  //// This attribute is used to register the informations needed to show the this package
+  //// in the Help/About dialog of Visual Studio.
+  //[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+  //[Guid(Guids.guidCosmos_VS_XSharpPkgString)]
 
-  // Registering a Language Service (MPF)
-  // http://msdn.microsoft.com/en-us/library/bb166498
-  [ProvideServiceAttribute(typeof(XSharpLanguageService), ServiceName = "X# Language Service")]
-  [ProvideLanguageServiceAttribute(typeof(XSharpLanguageService), "X#",
-    106, // Resource ID of localized language name
-    CodeSense = false, // IntelliSense
-    RequestStockColors = true, 
-    EnableCommenting = false, // Commenting out code
-    EnableAsyncCompletion = true // Background parsing
-  )]
-  [ProvideLanguageExtensionAttribute(typeof(XSharpLanguageService), ".xs")]
+  //// Registering a Language Service (MPF)
+  //// http://msdn.microsoft.com/en-us/library/bb166498
+  //[ProvideServiceAttribute(typeof(XSharpLanguageService), ServiceName = "X# Language Service")]
+  //[ProvideLanguageServiceAttribute(typeof(XSharpLanguageService), "X#",
+  //  106, // Resource ID of localized language name
+  //  CodeSense = false, // IntelliSense
+  //  RequestStockColors = true,
+  //  EnableCommenting = false, // Commenting out code
+  //  EnableAsyncCompletion = true // Background parsing
+  //)]
+  //[ProvideLanguageExtensionAttribute(typeof(XSharpLanguageService), ".xs")]
 
   public sealed class Cosmos_VS_XSharpPackage : Package, IOleComponent {
     uint mComponentID;
-    
+
     // Default constructor of the package.
-    // Inside this method you can place any initialization code that does not require 
-    // any Visual Studio service because at this point the package object is created but 
-    // not sited yet inside Visual Studio environment. The place to do all the other 
+    // Inside this method you can place any initialization code that does not require
+    // any Visual Studio service because at this point the package object is created but
+    // not sited yet inside Visual Studio environment. The place to do all the other
     // initialization is the Initialize method.
     public Cosmos_VS_XSharpPackage() {
     }
