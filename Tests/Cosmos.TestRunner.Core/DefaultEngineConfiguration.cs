9e33bac82294f36cb589a00a9ca11cec332ebd68
Matthijs ter Woord <matthijsterwoord@gmail.com>
2015-07-23 11:42:46 -0400

    .

--- Tests/Cosmos.TestRunner.Core/DefaultEngineConfiguration.cs
+++ Tests/Cosmos.TestRunner.Core/DefaultEngineConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿﻿using System;
+﻿using System.Linq;
 
 namespace Cosmos.TestRunner.Core
 {
@@ -14,17 +15,28 @@ namespace Cosmos.TestRunner.Core
             engine.AllowedSecondsInKernel = 120;
 
             // If you want to exclude a testing platform, modify uncomment and modify the following line
-            engine.RunTargets.Remove(RunTargetEnum.Bochs);
+            engine.RunTargets.Remove(RunTargetEnum.VMware);
 
             // if you're working on the compiler (or other lower parts), you can choose to run the compiler in process
             // 1 thing to keep in mind though, is that this only works with 1 kernel at a time!
-            engine.RunIL2CPUInProcess = true;
+            engine.RunIL2CPUInProcess = false;
 
             engine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
-            //engine.AddKernel(typeof(SimpleStructsAndArraysTest.Kernel).Assembly.Location);
-            //engine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
+            engine.AddKernel(typeof(SimpleStructsAndArraysTest.Kernel).Assembly.Location);
+            engine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
 
             // known bugs, therefor disabled for now:
+
+            // end of known bugs
+
+            // double check: this check is in the engine, but lets put it here as well
+            if (engine.RunIL2CPUInProcess)
+            {
+                if (engine.KernelsToRun.Count() > 1)
+                {
+                    throw new InvalidOperationException("Can only run 1 kernel if IL2CPU is ran in-process!");
+                }
+            }
         }
     }
 }
