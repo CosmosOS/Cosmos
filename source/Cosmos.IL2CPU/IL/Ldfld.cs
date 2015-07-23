c80eb38c8af34d0b43446f99dc634a863fab8f86
Matthijs ter Woord <matthijsterwoord@gmail.com>
2015-07-23 12:04:48 -0400

    Reformat

--- source/Cosmos.IL2CPU/IL/Ldfld.cs
+++ source/Cosmos.IL2CPU/IL/Ldfld.cs
@@ -36,90 +36,97 @@ namespace Cosmos.IL2CPU.X86.IL
     ///
     /// This is typically checked when Microsoft Intermediate Language (MSIL) instructions are converted to native code, not at run time.
     /// </remarks>
-    [Cosmos.IL2CPU.OpCode( ILOpCode.Code.Ldfld )]
-    public class Ldfld : ILOp
+    [Cosmos.IL2CPU.OpCode(ILOpCode.Code.Ldfld)]
+    public class Ldfld: ILOp
     {
-        public Ldfld( Cosmos.Assembler.Assembler aAsmblr )
-            : base( aAsmblr )
+        public Ldfld(Cosmos.Assembler.Assembler aAsmblr)
+            : base(aAsmblr)
         {
         }
 
-        public override void Execute( MethodInfo aMethod, ILOpCode aOpCode )
+        public override void Execute(MethodInfo aMethod, ILOpCode aOpCode)
         {
-          var xOpCode = (ILOpCodes.OpField)aOpCode;
-          DoExecute(Assembler, xOpCode.Value.DeclaringType, xOpCode.Value.GetFullName(), true, DebugEnabled);
+            var xOpCode = (ILOpCodes.OpField)aOpCode;
+            DoExecute(Assembler, xOpCode.Value.DeclaringType, xOpCode.Value.GetFullName(), true, DebugEnabled);
         }
 
-        public static int GetFieldOffset(Type aDeclaringType, string aFieldId) {
-          int xExtraOffset = 0;
-          var xFieldInfo = ResolveField(aDeclaringType, aFieldId, true);
-          bool xNeedsGC = aDeclaringType.IsClass && !aDeclaringType.IsValueType;
-          if (xNeedsGC) {
-            xExtraOffset = 12;
-          }
-          return (int)(xExtraOffset + xFieldInfo.Offset);
+        public static int GetFieldOffset(Type aDeclaringType, string aFieldId)
+        {
+            int xExtraOffset = 0;
+            var xFieldInfo = ResolveField(aDeclaringType, aFieldId, true);
+            bool xNeedsGC = aDeclaringType.IsClass && !aDeclaringType.IsValueType;
+            if (xNeedsGC)
+            {
+                xExtraOffset = 12;
+            }
+            return (int)(xExtraOffset + xFieldInfo.Offset);
         }
 
-        public static void DoExecute(Cosmos.Assembler.Assembler Assembler, Type aDeclaringType, string xFieldId, bool aDerefExternalField, bool debugEnabled) {
-          var xOffset = GetFieldOffset(aDeclaringType, xFieldId);
-          var xFields = GetFieldsInfo(aDeclaringType, false);
-          var xFieldInfo = (from item in xFields
-                            where item.Id == xFieldId
-                            select item).Single();
-        new Comment("Field: " + xFieldInfo.Id);
-        new Comment("Type: " + xFieldInfo.FieldType.ToString());
-        new Comment("Size: " + xFieldInfo.Size);
-        new Comment("Offset: " + xOffset + " (includes object header)");
+        public static void DoExecute(Cosmos.Assembler.Assembler Assembler, Type aDeclaringType, string xFieldId, bool aDerefExternalField, bool debugEnabled)
+        {
+            var xOffset = GetFieldOffset(aDeclaringType, xFieldId);
+            var xFields = GetFieldsInfo(aDeclaringType, false);
+            var xFieldInfo = (from item in xFields
+                              where item.Id == xFieldId
+                              select item).Single();
+            new Comment("Field: " + xFieldInfo.Id);
+            new Comment("Type: " + xFieldInfo.FieldType.ToString());
+            new Comment("Size: " + xFieldInfo.Size);
+            new Comment("Offset: " + xOffset + " (includes object header)");
             DoNullReferenceCheck(Assembler, debugEnabled, 0);
 
-          new CPUx86.Pop { DestinationReg = CPUx86.Registers.ECX };
+            new CPUx86.Pop {DestinationReg = CPUx86.Registers.ECX};
 
 #if DOTNETCOMPATIBLE
-			// pushed size is always 4 or 8
+    // pushed size is always 4 or 8
 			var xSize = ILOp.Align(xFieldInfo.Size, 4);
 #else
-			var xSize = xFieldInfo.Size;
+            var xSize = xFieldInfo.Size;
 #endif
 
-          new CPUx86.Add { DestinationReg = CPUx86.Registers.ECX, SourceValue = (uint)(xOffset) };
+            new CPUx86.Add {DestinationReg = CPUx86.Registers.ECX, SourceValue = (uint)(xOffset)};
 
-          if( xFieldInfo.IsExternalValue && aDerefExternalField)
-          {
-              new CPUx86.Mov { DestinationReg = CPUx86.Registers.ECX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true };
-          }
+            if (xFieldInfo.IsExternalValue && aDerefExternalField)
+            {
+                new CPUx86.Mov {DestinationReg = CPUx86.Registers.ECX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true};
+            }
 
-          for (int i = 1; i <= (xSize / 4); i++) {
-            new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true, SourceDisplacement = (int)(xSize - (i * 4)) };
-            new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
-          }
-          switch (xSize % 4) {
-            case 1: {
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, SourceValue = 0 };
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.AL, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true };
-                new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
-                break;
-              }
-            case 2: {
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, SourceValue = 0 };
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.AX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true };
-                new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
-                break;
-              }
+            for (int i = 1; i <= (xSize / 4); i++)
+            {
+                new CPUx86.Mov {DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true, SourceDisplacement = (int)(xSize - (i * 4))};
+                new CPUx86.Push {DestinationReg = CPUx86.Registers.EAX};
+            }
+            switch (xSize % 4)
+            {
+                case 1:
+                {
+                    new CPUx86.Mov {DestinationReg = CPUx86.Registers.EAX, SourceValue = 0};
+                    new CPUx86.Mov {DestinationReg = CPUx86.Registers.AL, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true};
+                    new CPUx86.Push {DestinationReg = CPUx86.Registers.EAX};
+                    break;
+                }
+                case 2:
+                {
+                    new CPUx86.Mov {DestinationReg = CPUx86.Registers.EAX, SourceValue = 0};
+                    new CPUx86.Mov {DestinationReg = CPUx86.Registers.AX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true};
+                    new CPUx86.Push {DestinationReg = CPUx86.Registers.EAX};
+                    break;
+                }
 
-            case 3: //For Release
-                    {
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, SourceValue = 0 };
-                new CPUx86.Mov { DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true };
-                new CPUx86.ShiftRight { DestinationReg = CPUx86.Registers.EAX, SourceValue = 8 };
-                new CPUx86.Push { DestinationReg = CPUx86.Registers.EAX };
-                break;
-              }
-            case 0: {
-                      break;
-                    }
-            default:
-              throw new Exception(string.Format("Remainder size {0:D} {1:D} not supported!", xFieldInfo.FieldType.ToString(), xSize));
-          }
+                case 3: //For Release
+                    new CPUx86.Mov {DestinationReg = CPUx86.Registers.EAX, SourceValue = 0};
+                    new CPUx86.Mov {DestinationReg = CPUx86.Registers.EAX, SourceReg = CPUx86.Registers.ECX, SourceIsIndirect = true};
+                    new CPUx86.ShiftRight {DestinationReg = CPUx86.Registers.EAX, SourceValue = 8};
+                    new CPUx86.Push {DestinationReg = CPUx86.Registers.EAX};
+                    break;
+
+                case 0:
+                {
+                    break;
+                }
+                default:
+                    throw new Exception(string.Format("Remainder size {0:D} {1:D} not supported!", xFieldInfo.FieldType.ToString(), xSize));
+            }
         }
     }
 }
