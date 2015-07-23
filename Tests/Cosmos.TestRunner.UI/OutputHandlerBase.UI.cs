6202c49d7af0ef7b9d18ed96216dff79dd8bca4e
Geramy L. Loveless <gloveless@jqluv.com>
2015-07-23 07:38:42 -0700

    Basic Stubbing Finished.
    Did some basic stubbing of the methods and now going to start
    implementing the meat.

---
+++ Tests/Cosmos.TestRunner.UI/OutputHandlerBase.UI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.TestRunner.Core;
+
+namespace Cosmos.TestRunner.UI
+{
+    public partial class MainWindow : OutputHandlerBase
+    {
+        public override void OnExecuteKernelStart(string assemblyName)
+        {
+
+        }
+        public override void OnExecuteKernelEnd(string assemblyName)
+        {
+
+        }
+        public override void OnLogDebugMessage(string message)
+        {
+
+        }
+        public override void OnLogMessage(string message)
+        {
+
+        }
+        public override void OnLogError(string message)
+        {
+
+        }
+        public override void OnExecutionStart()
+        {
+
+        }
+        public override void OnExecutionEnd()
+        {
+
+        }
+        public override void OnUnhandledException(Exception exception)
+        {
+
+        }
+        public override void OnTaskStart(string taskName)
+        {
+
+        }
+        public override void OnTaskEnd(string taskName)
+        {
+
+        }
+        public override void OnSetKernelTestResult(bool succeeded, string message)
+        {
+
+        }
+
+        public override void OnRunConfigurationStart(RunConfiguration configuration)
+        {
+
+        }
+        public override void OnRunConfigurationEnd(RunConfiguration configuration)
+        {
+
+        }
+
+        public override void OnSetKernelSucceededAssertionsCount(int succeededAssertions)
+        {
+
+        }
+    }
+}
