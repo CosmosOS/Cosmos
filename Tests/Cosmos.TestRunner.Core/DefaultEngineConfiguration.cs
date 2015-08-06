611c4aa9bfede79dfefdb5784e356c04c8e0db42
Matthijs ter Woord <matthijsterwoord@gmail.com>
2015-08-06 08:55:47 -0400

    Restore default test configuration

--- Tests/Cosmos.TestRunner.Core/DefaultEngineConfiguration.cs
+++ Tests/Cosmos.TestRunner.Core/DefaultEngineConfiguration.cs
@@ -15,19 +15,19 @@ namespace Cosmos.TestRunner.Core
             engine.AllowedSecondsInKernel = 180;
 
             // If you want to test only specific platforms, add them to the list, like next line. By default, all platforms are ran.
-            engine.RunTargets.Add(RunTargetEnum.Bochs);
+            //engine.RunTargets.Add(RunTargetEnum.Bochs);
 
             // if you're working on the compiler (or other lower parts), you can choose to run the compiler in process
             // 1 thing to keep in mind though, is that this only works with 1 kernel at a time!
             engine.RunIL2CPUInProcess = false;
             engine.RunWithGDB = false;
 
-            //engine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
-            //engine.AddKernel(typeof(SimpleStructsAndArraysTest.Kernel).Assembly.Location);
-            //engine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
+            engine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
+            engine.AddKernel(typeof(SimpleStructsAndArraysTest.Kernel).Assembly.Location);
+            engine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
 
             // known bugs, therefor disabled for now:
-            engine.AddKernel(typeof(BoxingTests.Kernel).Assembly.Location);
+            //engine.AddKernel(typeof(BoxingTests.Kernel).Assembly.Location);
             // end of known bugs
 
             // double check: this check is in the engine, but lets put it here as well
