488889f8535d18770d44a3bc43c9def6460f5e38
zdimension <zippedfire@free.fr>
2015-08-12 02:21:43 +0200

    Added a Power class containing a WORKING reboot function

---
+++ source/Cosmos.HAL/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.Core;
+
+namespace Cosmos.HAL
+{
+    public class Power
+    {
+        public static void Reboot()
+        {
+            byte good = 0x02;
+            while ((good & 0x02) != 0)
+                good = new IOPort(0x64).Byte;
+            new IOPort(0x64).Byte = 0xFE;
+            Core.Global.CPU.Halt();
+        }
+    }
+}
---
+++ source/Cosmos.System/Power.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.System
+{
+    public class Power
+    {
+        public static void Reboot()
+        {
+            HAL.Power.Reboot();
+        }
+    }
+}
