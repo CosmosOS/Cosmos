e3e32744f5fe16fb95a5ea1eaa47a934319f4266
Matthijs ter Woord <matthijsterwoord@gmail.com>
2015-09-04 14:11:35 +0200

    Merging.

---
+++ Tests/Cosmos.Compiler.Tests.Bcl/Kernel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.TestRunner;
+using Sys = Cosmos.System;
+
+namespace Cosmos.Compiler.Tests.Bcl
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            System.Collections.Generic.ListTest.Execute();
+
+            TestController.Completed();
+        }
+    }
+}
--- Tests/Cosmos.TestRunner.Core/DefaultEngineConfiguration.cs
+++ Tests/Cosmos.TestRunner.Core/DefaultEngineConfiguration.cs
@@ -22,10 +22,11 @@ namespace Cosmos.TestRunner.Core
             //engine.RunIL2CPUInProcess = true;
             engine.RunWithGDB = false;
 
-            engine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
-            engine.AddKernel(typeof(SimpleStructsAndArraysTest.Kernel).Assembly.Location);
-            engine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
-            engine.AddKernel(typeof(Cosmos.Compiler.Tests.SingleEchoTest.Kernel).Assembly.Location);
+            //engine.AddKernel(typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel).Assembly.Location);
+            //engine.AddKernel(typeof(SimpleStructsAndArraysTest.Kernel).Assembly.Location);
+            //engine.AddKernel(typeof(VGACompilerCrash.Kernel).Assembly.Location);
+            //engine.AddKernel(typeof(Cosmos.Compiler.Tests.SingleEchoTest.Kernel).Assembly.Location);
+            engine.AddKernel(typeof(Cosmos.Compiler.Tests.Bcl.Kernel).Assembly.Location);
 
             // known bugs, therefor disabled for now:
             //engine.AddKernel(typeof(BoxingTests.Kernel).Assembly.Location);
