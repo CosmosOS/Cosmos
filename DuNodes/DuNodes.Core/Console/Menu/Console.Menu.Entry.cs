39a62e7bb960df47682c19514f624655c28a99d4
Maldus0 <mura.sylvain@neovisiontech.fr>
2015-11-04 11:03:52 +0100

    Menu support and config command
    Changelog :
    - Clean Menu
    - Rewrite all menu system (lite system)
    - Make config command in order to reformat or change keylayout (save
    settings on exit)

--- DuNodes/DuNodes.Core/Console/Menu/Console.Menu.Entry.cs
+++ DuNodes/DuNodes.Core/Console/Menu/Console.Menu.Entry.cs
@@ -1,26 +1,5 @@
-/*
-Copyright (c) 2012-2013, dewitcher Team
-All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice
-   this list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+using System.Collections.Generic;
 
 namespace DuNodes.System.Console
 {
@@ -28,10 +7,29 @@ namespace DuNodes.System.Console
     {
         public partial class Menu
         {
-            public abstract class Entry
+            public class Entry
             {
-                public string text;
-                public abstract void Execute();
+                public string text { get; set; }
+                public string executeValue { get; set; }
+                public bool isExecute = false;
+                public bool isBack = false;
+                public List<Entry> InnerEntries { get; set; }
+                public bool isExit = false;
+
+                public Entry()
+                {
+                    InnerEntries =  new List<Entry>();
+                }
+
+                public Entry(string text, bool isExecuter = false, string executeValue = "", bool isBack = false,
+                    bool isExit = false)
+                {
+                    this.text = text;
+                    this.isExit = isExit;
+                    this.isBack = isBack;
+                    isExecute = isExecuter;
+                    this.executeValue = executeValue;
+                }
             }
         }
     }
