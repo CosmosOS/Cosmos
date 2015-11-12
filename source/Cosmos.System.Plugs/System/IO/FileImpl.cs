e699cdc70b135c015f4b697221afd5547acab272
Cyber <cybersurvivor44@gmail.com>
2015-11-11 21:49:57 -0500

    Ongoing FAT Work
    Added ReadAllBytes, WriteAllBytes, and Copy to FileImpl

--- source/Cosmos.System.Plugs/System/IO/FileImpl.cs
+++ source/Cosmos.System.Plugs/System/IO/FileImpl.cs
@@ -11,6 +11,8 @@ namespace Cosmos.System.Plugs.System.IO
     [Plug(Target = typeof(File))]
     public static class FileImpl
     {
+        
+
         public static bool Exists(string aFile)
         {
             return VFSManager.FileExists(aFile);
@@ -40,6 +42,44 @@ namespace Cosmos.System.Plugs.System.IO
             {
                 var xBuff = aText.GetUtf8Bytes(0, (uint)aText.Length);
                 xFS.Write(xBuff, 0, xBuff.Length);
+            }
+        }
+        public static byte[] ReadAllBytes(string aFile)
+        {
+            FatHelpers.Debug("In FileImpl.ReadAllText");
+            using (var xFS = new FileStream(aFile, FileMode.Open))
+            {
+                var xBuff = new byte[(int)xFS.Length];
+                var xResult = xFS.Read(xBuff, 0, xBuff.Length);
+                if (xResult != xBuff.Length)
+                {
+                    throw new Exception("Couldn't read complete file!");
+                }
+                FatHelpers.Debug("Bytes read");
+                
+                return xBuff;
+            }
+        }
+
+        public static void WriteAllBytes(string aFile, byte[] aBytes)
+        {
+            using (var xFS = new FileStream(aFile, FileMode.Create))
+            {
+                var xBuff = aBytes;
+                
+                xFS.Write(xBuff, 0, xBuff.Length);
+            }
+        }
+        public static void Copy(string srcFile, string destFile)
+        {
+            byte[] xBuff;
+            using (var xFS = new FileStream(srcFile, FileMode.Open))
+            {
+                xBuff = new byte[(int)xFS.Length];
+                var s1 = xFS.Read(xBuff, 0, xBuff.Length);
+                var yFS = new FileStream(destFile, FileMode.Create);
+                yFS.Write(xBuff, 0, xBuff.Length);
+
             }
         }
     }
