b803343748e5c4c5df3fb8fd590954cd73cc142c
fanoI <fanoIlPrimo@gmail.com>
2016-01-15 00:18:24 +0100

    Revert "Last try to not have conflicts..."
    This reverts commit 97a11e633139135cf5a50142a4169ff6d618637e.

--- Demos/Guess/GuessOS.cs
+++ Demos/Guess/GuessOS.cs
@@ -21,10 +21,10 @@ namespace GuessKernel
 
     protected override void BeforeRun()
     {
-      //Cosmos.Core.HMI.Init();
+            Console.WriteLine("Guess Demo");
+            //Cosmos.Core.HMI.Init();
 
-      Console.WriteLine("Guess Demo");
-      Console.WriteLine("Please guess a number from 1 to 100.");
+            Console.WriteLine("Please guess a number from 1 to 100.");
     }
 
     private Debugger mDebugger = new Debugger("User", "Guess");
