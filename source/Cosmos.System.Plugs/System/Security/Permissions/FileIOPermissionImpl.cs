f5d4b50bba4dc75202005624a97e763ea6452b18
fanoI <fanoIlPrimo@gmail.com>
2016-01-31 15:12:16 +0100

    FAT: trying to partially unplug (call .NET code) for the Path class
    - PathImpl.cs: only the methods NormalizePath(), GetTempPath(),
    GetInvalidFileNameChars(), GetTempFileName(), GetInvalidPathChars() and
    GetRandomFileName are needed to implement
    - Added plug for FileIOPermission (called by the original Path
    implementation)

---
+++ source/Cosmos.System.Plugs/System/Security/Permissions/FileIOPermissionImpl.cs
@@ -0,0 +1,21 @@
+﻿using Cosmos.IL2CPU.Plugs;
+using System;
+
+namespace Cosmos.System.Plugs.System.Security.Permissions
+{
+    [Plug(Target = typeof(global::System.Security.Permissions.FileIOPermission))]
+    public static class FileIOPermissionPlug
+    {
+        public static void QuickDemand(global::System.Security.Permissions.FileIOPermissionAccess access, string fullPath, bool checkForDuplicates, bool needFullPath)
+        {
+        }
+        
+        // This shows a compiler bug: trying to compile this method crashes the compiler!
+        public static global::System.Security.SecurityElement ToXml(global::System.Security.Permissions.FileIOPermission aThis)
+        {
+            throw new NotImplementedException("FileIOPermission ToXml()");
+            // It is indifferent if it returns or throw...
+            //return null;
+        }
+    }
+}
