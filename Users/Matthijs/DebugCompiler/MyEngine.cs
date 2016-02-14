8f22ca1493056ce3939c8a0a1afc5d00c13c7598
Matthijs ter Woord <matthijsterwoord@gmail.com>
2016-02-14 14:32:30 +0100

    Missing files due to error in gitignore file.

---
+++ Users/Matthijs/DebugCompiler/MyEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DebugCompiler
+{
+    [TestFixture]
+    public class RunKernels
+    {
+        [Test]
+        public void Test([ValueSource(typeof(MySource), "ProvideData")] Type kernelToRun)
+        {
+            Assert.IsNotNull(kernelToRun);
+            Assert.Fail("Cannot run kernel '" + kernelToRun.FullName + "'!");
+        }
+    }
+}
---
+++ Users/Matthijs/DebugCompiler/MySource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cosmos.TestRunner.Core;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace DebugCompiler
+{
+    public class MySource: IEnumerable<ITestFixtureData>
+    {
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<ITestFixtureData> GetEnumerator()
+        {
+            yield return new TestFixtureData("A");
+            yield return new TestFixtureData("B");
+            yield return new TestFixtureData("C");
+        }
+
+        public static IEnumerable<Type> ProvideData()
+        {
+            return TestKernelSets.GetStableKernelTypes();
+        }
+    }
+}
