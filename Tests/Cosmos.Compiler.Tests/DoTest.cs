016bc6f680a164dcd598bb2b6e3b1ad3160e1b77
Matthijs ter Woord <matthijsterwoord@gmail.com>
2016-02-15 12:13:00 +0100

    Disable reporting in VS.

--- Tests/Cosmos.Compiler.Tests/DoTest.cs
+++ Tests/Cosmos.Compiler.Tests/DoTest.cs
@@ -8,7 +8,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Cosmos.Compiler.Tests
 {
     [TestClass]
-    [UseReporter(typeof(VisualStudioReporter))]
     public class DoTest : BaseTest
     {
         [TestMethod]
