83ad5f34cfafb5a590026b90ecfd4d9d9cb87f2e
Charles Betros <charles.betros@gmail.com>
2016-03-24 08:52:02 -0500

    Fixed some issues with wtong types on the stack.

--- source/Cosmos.IL2CPU/ILOpCodes/OpVar.cs
+++ source/Cosmos.IL2CPU/ILOpCodes/OpVar.cs
@@ -6,104 +6,117 @@ using System.Text;
 
 namespace Cosmos.IL2CPU.ILOpCodes
 {
-  public class OpVar : ILOpCode
-  {
-    public readonly UInt16 Value;
-
-    public OpVar(Code aOpCode, int aPos, int aNextPos, UInt16 aValue, ExceptionHandlingClause aCurrentExceptionHandler)
-      : base(aOpCode, aPos, aNextPos, aCurrentExceptionHandler)
+    public class OpVar : ILOpCode
     {
-      Value = aValue;
-    }
+        public readonly UInt16 Value;
 
-    public override int GetNumberOfStackPops(MethodBase aMethod)
-    {
-      switch (OpCode)
-      {
-        case Code.Ldloc:
-        case Code.Ldloca:
-        case Code.Ldarg:
-        case Code.Ldarga:
-          return 0;
-        case Code.Stloc:
-        case Code.Starg:
-          return 1;
-        default:
-          throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
-      }
-    }
+        public OpVar(Code aOpCode, int aPos, int aNextPos, UInt16 aValue,
+            ExceptionHandlingClause aCurrentExceptionHandler)
+            : base(aOpCode, aPos, aNextPos, aCurrentExceptionHandler)
+        {
+            Value = aValue;
+        }
 
-    public override int GetNumberOfStackPushes(MethodBase aMethod)
-    {
-      switch (OpCode)
-      {
-        case Code.Stloc:
-        case Code.Starg:
-          return 0;
-        case Code.Ldloc:
-        case Code.Ldloca:
-        case Code.Ldarg:
-        case Code.Ldarga:
-          return 1;
-        default:
-          throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
-      }
-    }
-
-    protected override void DoInitStackAnalysis(MethodBase aMethod)
-    {
-      base.DoInitStackAnalysis(aMethod);
-
-      switch (OpCode)
-      {
-        case Code.Ldloc:
-        case Code.Ldloca:
-          var xBody = aMethod.GetMethodBody();
-          if (xBody != null)
-          {
-            StackPushTypes[0] = xBody.LocalVariables[Value].LocalType;
-            if (StackPushTypes[0].IsEnum)
+        public override int GetNumberOfStackPops(MethodBase aMethod)
+        {
+            switch (OpCode)
             {
-              StackPushTypes[0] = StackPushTypes[0].GetEnumUnderlyingType();
+                case Code.Ldloc:
+                case Code.Ldloca:
+                case Code.Ldarg:
+                case Code.Ldarga:
+                    return 0;
+                case Code.Stloc:
+                case Code.Starg:
+                    return 1;
+                default:
+                    throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
             }
-            else if ((OpCode == Code.Ldloca) && (!StackPushTypes[0].IsValueType))
+        }
+
+        public override int GetNumberOfStackPushes(MethodBase aMethod)
+        {
+            switch (OpCode)
             {
-              StackPushTypes[0] = StackPushTypes[0].MakePointerType();
+                case Code.Stloc:
+                case Code.Starg:
+                    return 0;
+                case Code.Ldloc:
+                case Code.Ldloca:
+                case Code.Ldarg:
+                case Code.Ldarga:
+                    return 1;
+                default:
+                    throw new NotImplementedException("OpCode '" + OpCode + "' not implemented!");
             }
-          }
-          return;
-        case Code.Ldarg:
-        case Code.Ldarga:
-          var xArgIndexCorrection = 0;
-          if (!aMethod.IsStatic)
-          {
-            if (Value == 0)
+        }
+
+        protected override void DoInitStackAnalysis(MethodBase aMethod)
+        {
+            base.DoInitStackAnalysis(aMethod);
+
+            var xArgIndexCorrection = 0;
+            var xBody = aMethod.GetMethodBody();
+            var xParams = aMethod.GetParameters();
+            switch (OpCode)
             {
-              StackPushTypes[0] = aMethod.DeclaringType;
-              if (StackPushTypes[0].IsEnum)
-              {
-                StackPushTypes[0] = StackPushTypes[0].GetEnumUnderlyingType();
-              }
-              else if ((OpCode == Code.Ldarga) || ((OpCode == Code.Ldarg) && (StackPushTypes[0].IsValueType)))
-              {
-                StackPushTypes[0] = StackPushTypes[0].MakePointerType();
-              }
-              return;
+                case Code.Ldloc:
+                    if (xBody != null)
+                    {
+                        StackPushTypes[0] = xBody.LocalVariables[Value].LocalType;
+                        if (StackPushTypes[0].IsEnum)
+                        {
+                            StackPushTypes[0] = StackPushTypes[0].GetEnumUnderlyingType();
+                        }
+                    }
+                    return;
+                case Code.Ldloca:
+                    if (xBody != null)
+                    {
+                        StackPushTypes[0] = xBody.LocalVariables[Value].LocalType.MakePointerType();
+                    }
+                    return;
+                case Code.Ldarg:
+                    if (!aMethod.IsStatic)
+                    {
+                        if (Value == 0)
+                        {
+                            StackPushTypes[0] = aMethod.DeclaringType;
+                            if (StackPushTypes[0].IsEnum)
+                            {
+                                StackPushTypes[0] = StackPushTypes[0].GetEnumUnderlyingType();
+                            }
+                            else if (StackPushTypes[0].IsValueType)
+                            {
+                                StackPushTypes[0] = StackPushTypes[0].MakePointerType();
+                            }
+                            return;
+                        }
+                        xArgIndexCorrection = -1;
+                    }
+                    StackPushTypes[0] = xParams[Value + xArgIndexCorrection].ParameterType;
+                    if (StackPushTypes[0].IsEnum)
+                    {
+                        StackPushTypes[0] = StackPushTypes[0].GetEnumUnderlyingType();
+                    }
+                    return;
+                case Code.Ldarga:
+                    if (!aMethod.IsStatic)
+                    {
+                        if (Value == 0)
+                        {
+                            if (StackPushTypes[0].IsValueType)
+                            {
+                                StackPushTypes[0] = StackPushTypes[0].MakePointerType();
+                            }
+                            return;
+                        }
+                        xArgIndexCorrection = -1;
+                    }
+                    StackPushTypes[0] = xParams[Value + xArgIndexCorrection].ParameterType;
+                    StackPushTypes[0] = StackPushTypes[0].MakePointerType();
+                    return;
             }
-            xArgIndexCorrection = -1;
-          }
-          var xParams = aMethod.GetParameters();
-          StackPushTypes[0] = xParams[Value + xArgIndexCorrection].ParameterType;
-          if (StackPushTypes[0].IsEnum)
-          {
-            StackPushTypes[0] = StackPushTypes[0].GetEnumUnderlyingType();
-          }
-          else if (OpCode == Code.Ldarga)
-          {
-            StackPushTypes[0] = StackPushTypes[0].MakePointerType();
-          }
-          return;
-      }
+        }
     }
-  }
 }
