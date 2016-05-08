d604882e17c7eb2fa44187af0e0f4ac8f8b1bf0c
Seeker1437 <williams.darnell56@yahoo.com>
2016-05-08 09:55:22 -0400

    Corrects EqualityComparer Plug

--- source/Cosmos.System.Plugs/System/Collections/Generic/EqualityComparerImpl.cs
+++ source/Cosmos.System.Plugs/System/Collections/Generic/EqualityComparerImpl.cs
@@ -1,18 +1,15 @@
-﻿using System;
+﻿using Cosmos.IL2CPU.Plugs;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Cosmos.IL2CPU.Plugs;
 
 namespace Cosmos.System.Plugs.System.Collections.Generic
 {
     [Plug(Target = typeof(EqualityComparer<>))]
-    public static class EqualityComparerImpl
+    public static class EqualityComparerImpl<T>
     {
-        public static EqualityComparer<> CreateCompare(this EqualityComparer<> equalityComparer)
+        public static EqualityComparer<T> CreateCompare()
         {
-            equalityComparer;
+            throw new NotImplementedException();
         }
     }
 }
