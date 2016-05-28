d692b56e300e905d7e07750f8efd86c1de037621
fanoI <fanoIlPrimo@gmail.com>
2016-05-28 14:43:48 +0200

    Re-added all SSE instruction files...

---
+++ source/Cosmos.Assembler/x86/SSE2/ConvertTruncatePD2DQ.cs
@@ -0,0 +1,7 @@
+﻿namespace Cosmos.Assembler.x86.SSE
+{
+    [Cosmos.Assembler.OpCode("CVTTPD2DQ")]
+    public class ConvertPD2DQAndTruncate : InstructionWithDestinationAndSource
+    {
+    }
+}
---
+++ source/Cosmos.Assembler/x86/SSE2/Psrld.cs
@@ -0,0 +1,7 @@
+﻿namespace Cosmos.Assembler.x86.SSE
+{
+    [Cosmos.Assembler.OpCode("PSRLD")]
+    public class Psrld : InstructionWithDestinationAndSource
+    {
+    }
+}
