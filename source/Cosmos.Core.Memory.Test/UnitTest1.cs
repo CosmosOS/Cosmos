12f4bd314a7f21ae9ea591d6f6a80db465407608
Kudzu <chad-ng@hower.org>
2016-06-12 16:13:03 -0400

    Heap and RAT

--- source/Cosmos.Core.Memory.Test/UnitTest1.cs
+++ source/Cosmos.Core.Memory.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Native = System.UInt32;
 
 namespace Cosmos.Core.Memory.Test {
   [TestClass]
@@ -9,7 +10,8 @@ namespace Cosmos.Core.Memory.Test {
       var xRAM = new byte[128 * 1024 * 1024];
       xRAM[0] = 1;
       fixed (byte* xPtr = xRAM) {
-        CHeap.Init(xPtr, (UInt32)xRAM.LongLength);
+        Heap.Init(xPtr, (UInt32)xRAM.LongLength);
+        Native xFreePages = Heap.GetPageCount(Heap.PageType.Empty);
       }
     }
   }
