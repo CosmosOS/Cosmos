0cdc16cba05cb9048986d8543063b84461f5b14f
Matthijs ter Woord <matthijsterwoord@gmail.com>
2016-06-13 12:12:23 -0400

    Attempt to fix appveyor build.

--- Users/Matthijs/DebugCompiler/MyEngine.cs
+++ Users/Matthijs/DebugCompiler/MyEngine.cs
@@ -17,14 +17,14 @@ namespace DebugCompiler
             var xEngine = new Engine();
             // Sets the time before an error is registered. For example if set to 60 then if a kernel runs for more than 60 seconds then
             // that kernel will be marked as a failiure and terminated
-            xEngine.AllowedSecondsInKernel = 30;
+            xEngine.AllowedSecondsInKernel = 300;
 
             // If you want to test only specific platforms, add them to the list, like next line. By default, all platforms are run.
             xEngine.RunTargets.Add(RunTargetEnum.Bochs);
 
             // If you're working on the compiler (or other lower parts), you can choose to run the compiler in process
             // one thing to keep in mind though, is that this only works with 1 kernel at a time!
-            xEngine.RunIL2CPUInProcess = true;
+            xEngine.RunIL2CPUInProcess = false;
             xEngine.TraceAssembliesLevel = TraceAssemblies.User;
 
             xEngine.EnableStackCorruptionChecks = true;
