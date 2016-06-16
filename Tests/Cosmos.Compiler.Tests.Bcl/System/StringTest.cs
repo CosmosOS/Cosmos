cd7dbf7d74d25d4d9b0dd8fc4c41bd4cae26fefd
MichaelTheShifter <michaelshiftos@gmail.com>
2016-06-16 14:18:19 -0400

    Added more string tests
    Added String.Split and $ tests.

--- Tests/Cosmos.Compiler.Tests.Bcl/System/StringTest.cs
+++ Tests/Cosmos.Compiler.Tests.Bcl/System/StringTest.cs
@@ -21,6 +21,19 @@ namespace Cosmos.Compiler.Tests.Bcl.System
             string substr = str.Substring(0, 6);
             Assert.IsTrue((substr == expected), "Substring is not equal to the expected result, result should be \"Cosmos\". Substrings are broken or MichaelTheShifter made an off-by-one error.");
 
+
+            int value1 = 1;
+            string value2 = " = 4";
+            string expected_res = "1 + 3 = 4";
+            Assert.IsTrue(($"{value1} + 3 = {value2}" == expected_res), "String $ operator does not work.");
+
+            string split_in = "Cosmos is the best.";
+            string[] split_expected = { "Cosmos", "is", "the", "best" };
+            Assert.IsTrue((split_in.Split(' ') == split_expected), "String.Split(char) doesn't work.");
+            Assert.IsTrue((split_in.Split(new[] { ' '}) == split_expected), "String.Split(char[]) doesn't work.");
+            Assert.IsTrue((split_in.Split(new[] { " " }, StringSplitOptions.None) == split_expected), "String.Split(string[], StringSplitOptions) doesn't work.");
+
+
         }
     }
 }
