62c8c470c01e7d3bc122b137a522e6c59b764533
MichaelTheShifter <michaelshiftos@gmail.com>
2016-06-17 09:00:34 -0400

    re-added sha1 test kernel
    "MY HANDS ARE TYPING WORDS"

---
+++ Tests/Cosmos.Compiler.Tests.Encryption/Kernel.cs
@@ -0,0 +1,31 @@
+﻿using Cosmos.TestRunner;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace Cosmos.Compiler.Tests.Encryption
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Starting tests.");
+        }
+
+        protected override void Run()
+        {
+            TestSHA1();
+            TestController.Completed();
+        }
+
+        public void TestSHA1()
+        {
+            byte[] data = new byte[256];
+            byte[] result;
+            var shaM = new SHA1Managed();
+            result = shaM.ComputeHash(data);
+        }
+    }
+}
