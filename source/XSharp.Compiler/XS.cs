cf4025b697f927580c4a6e91cfc735aef5994d54
Matthijs ter Woord <matthijsterwoord@gmail.com>
2016-06-17 14:34:35 -0400

    .

--- source/XSharp.Compiler/XS.cs
+++ source/XSharp.Compiler/XS.cs
@@ -766,14 +766,14 @@ namespace XSharp.Compiler
       Do<SubWithCarry>(register, valueToAdd, destinationDisplacement: destinationDisplacement, destinationIsIndirect: destinationIsIndirect);
     }
 
-    public static void And(Register register, uint value)
+    public static void And(Register register, uint value, bool destinationIsIndirect = false, int? destinationDisplacement = null, RegisterSize? size = null)
     {
-      Do<And>(register, value);
+      Do<And>(register, value, destinationIsIndirect: destinationIsIndirect, destinationDisplacement: destinationDisplacement, size: size);
     }
 
-    public static void And(Register register, Register value, bool destinationIsIndirect = false, int? destinationDisplacement = null)
+    public static void And(Register register, Register value, bool destinationIsIndirect = false, int? destinationDisplacement = null, RegisterSize? size = null)
     {
-      Do<And>(register, value, destinationIsIndirect: destinationIsIndirect, destinationDisplacement: destinationDisplacement);
+      Do<And>(register, value, destinationIsIndirect: destinationIsIndirect, destinationDisplacement: destinationDisplacement, size: size);
     }
 
     public static void Xor(string destination, Register source, bool destinationIsIndirect = false, int? destinationDisplacement = null, bool sourceIsIndirect = false, int? sourceDisplacement = null, RegisterSize? size = null)
