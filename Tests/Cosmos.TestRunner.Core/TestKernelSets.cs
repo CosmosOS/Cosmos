a37de93ccbcc0f6a4421bfa85a16ababa2b02ca2
Matthijs ter Woord <matthijsterwoord@gmail.com>
2016-06-24 10:15:22 -0400

    Enable all tests again.

--- Tests/Cosmos.TestRunner.Core/TestKernelSets.cs
+++ Tests/Cosmos.TestRunner.Core/TestKernelSets.cs
@@ -7,17 +7,17 @@ namespace Cosmos.TestRunner.Core
     {
         public static IEnumerable<Type> GetStableKernelTypes()
         {
-            //yield return typeof(VGACompilerCrash.Kernel);
-            ////yield return typeof(Cosmos.Compiler.Tests.Encryption.Kernel);
-            //yield return typeof(Cosmos.Compiler.Tests.Bcl.Kernel);
-            //yield return typeof(Cosmos.Compiler.Tests.SingleEchoTest.Kernel);
-            //yield return typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel);
-            //yield return typeof(SimpleStructsAndArraysTest.Kernel);
-            //yield return typeof(Cosmos.Compiler.Tests.Exceptions.Kernel);
-            //yield return typeof(Cosmos.Compiler.Tests.LinqTests.Kernel);
-            //yield return typeof(Cosmos.Compiler.Tests.MethodTests.Kernel);
-            //yield return typeof(Cosmos.Kernel.Tests.Fat.Kernel);
-            yield return typeof(FrotzKernel.Kernel);
+            yield return typeof(VGACompilerCrash.Kernel);
+            //yield return typeof(Cosmos.Compiler.Tests.Encryption.Kernel);
+            yield return typeof(Cosmos.Compiler.Tests.Bcl.Kernel);
+            yield return typeof(Cosmos.Compiler.Tests.SingleEchoTest.Kernel);
+            yield return typeof(Cosmos.Compiler.Tests.SimpleWriteLine.Kernel.Kernel);
+            yield return typeof(SimpleStructsAndArraysTest.Kernel);
+            yield return typeof(Cosmos.Compiler.Tests.Exceptions.Kernel);
+            yield return typeof(Cosmos.Compiler.Tests.LinqTests.Kernel);
+            yield return typeof(Cosmos.Compiler.Tests.MethodTests.Kernel);
+            yield return typeof(Cosmos.Kernel.Tests.Fat.Kernel);
+            //yield return typeof(FrotzKernel.Kernel);
         }
     }
 }
