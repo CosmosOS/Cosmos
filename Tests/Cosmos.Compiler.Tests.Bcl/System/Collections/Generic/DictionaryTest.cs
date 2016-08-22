709741d5690e995e67097495776727c03ff4b3c4
fanoI <fanoIlPrimo@gmail.com>
2016-08-22 17:18:40 +0200

    Added file DictionaryTests.cs with preliminary not passing test of Dictionary BCL class

---
+++ Tests/Cosmos.Compiler.Tests.Bcl/System/Collections/Generic/DictionaryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cosmos.TestRunner;
+
+
+namespace Cosmos.Compiler.Tests.Bcl.System.Collections.Generic
+{
+    public static class DictionaryTest
+    {
+        public static void Execute()
+        {
+            Dictionary<String, Object> commands = new Dictionary<String, Object>()
+            {
+                {"echo", "ECHO"},
+                {"reboot", "REBOOT" },
+                {"shutdown", "SHUTDOWN"}
+            };
+
+            Assert.IsTrue(commands.ContainsKey("echo"), "Dictionary ContainsKey does not work");
+        }
+    }
+}
