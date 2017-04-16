0fef242e8ed42fe79dfc43a972f97a38f3cda802
Charles Betros <charles.betros@gmail.com>
2017-04-16 18:44:53 -0500

    Add dependencies and includes to vsix.

---
+++ source/Cosmos.VS.DebugEngine/CosmosDebugEnginePackage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace Cosmos.VS.DebugEngine
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(CosmosDebugEnginePackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class CosmosDebugEnginePackage : Package
+    {
+        /// <summary>
+        /// CosmosDebugEnginePackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "85ecdef4-8c09-4a50-b5b9-7e8ae08adbca";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CosmosDebugEnginePackage"/> class.
+        /// </summary>
+        public CosmosDebugEnginePackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
