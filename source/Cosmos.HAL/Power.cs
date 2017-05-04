1574fdae1c5f0f4691b0862a2955a45a1bb3c097
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-05-04 23:14:52 +0200

    Update Power.cs

--- source/Cosmos.HAL/Power.cs
+++ source/Cosmos.HAL/Power.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,11 @@ namespace Cosmos.HAL
         public static void Reboot()
         {
             Core.Global.CPU.Reboot();
+        }
+
+        public static void Shutdown()
+        {
+            Core.Global.CPU.Shutdown();
         }
     }
 }
