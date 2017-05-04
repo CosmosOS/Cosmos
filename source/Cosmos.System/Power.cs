33e4d0282a33bd17cc89353ca7c5ec1c0598a8f7
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-05-04 23:12:23 +0200

    Shutdown method in Cosmos.System added.

--- source/Cosmos.System/Power.cs
+++ source/Cosmos.System/Power.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,10 @@ namespace Cosmos.System
         public static void Reboot()
         {
             HAL.Power.Reboot();
+        }
+        public static void Shutdown()
+        {
+            HAL.Power.Shutdown();
         }
     }
 }
