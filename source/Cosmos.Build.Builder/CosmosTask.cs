2d8f089eebacbcc99031fc570556be827e5030bc
Kudzu <ziloni@gmail.com>
2017-06-22 19:13:24 -0400

    Builder

--- source/Cosmos.Build.Builder/CosmosTask.cs
+++ source/Cosmos.Build.Builder/CosmosTask.cs
@@ -82,8 +82,6 @@ namespace Cosmos.Build.Builder {
       return mExceptionList;
     }
 
-    create bin cache if not exist
-
     protected void MSBuild(string aSlnFile, string aBuildCfg) {
       string xMSBuild = Path.Combine(Paths.VSPath, "MSBuild", "15.0", "Bin", "msbuild.exe");
       string xParams = $"{Quoted(aSlnFile)} " +
--- source/Cosmos.Build.Builder/FileMgr.cs
+++ source/Cosmos.Build.Builder/FileMgr.cs
@@ -5,6 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Cosmos.Build.Builder {
-  public class FileMgr {
+  public class FileMgr : IDisposable {
+
+    // Dummy pattern to allow scoping via using.
+    // Hacky, but for what we are doing its fine and the GC
+    // effects are negligible in our usage.
+    protected virtual void Dispose(bool aDisposing) {
+    }
+
+    public void Dispose() {
+      Dispose(true);
+    }
+
   }
 }
