58f6f43e8a188826c8831e9f9c792b552da5cd2e
Kudzu <ziloni@gmail.com>
2017-07-03 19:30:50 -0400

    renamer

--- source/Tools/ProjectRenamer/MainWindow.xaml.cs
+++ source/Tools/ProjectRenamer/MainWindow.xaml.cs
@@ -201,7 +201,7 @@ namespace ProjectRenamer {
                 return;
             }
 
-            //RenameProj();
+            RenameProj();
             FixCsprojs(); // After RenameProj()
             FixCs();
             FixIss();
