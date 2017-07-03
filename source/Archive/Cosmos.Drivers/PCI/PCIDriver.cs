416e9795dccbeedf3c365362f1756be153465e7c
Kudzu <ziloni@gmail.com>
2017-07-03 11:27:32 -0400

    Directory changes

---
+++ source/Archive/Cosmos.Drivers/PCI/PCIDriver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cosmos.Drivers.PCI
+{
+    /// <summary>
+    /// Represents a generic PCI driver.
+    /// </summary>
+    public abstract class PCIDriver : Driver
+    {
+    }
+}
