4fb7e6c32173d3145c16bca1290da456eb0bfc6e
Kudzu <ziloni@gmail.com>
2017-07-03 13:38:43 -0400

    Rename tool

--- source/Tools/ProjectRenamer/MainWindow.xaml.cs
+++ source/Tools/ProjectRenamer/MainWindow.xaml.cs
@@ -17,14 +17,39 @@ using IO = System.IO;
 namespace ProjectRenamer {
     public partial class MainWindow : Window {
         string mCosmosDir;
+        List<string> mSlnList = new List<string>();
 
         public MainWindow() {
             InitializeComponent();
+
+            tboxRenameOldName.Text = "Cosmos.Core.Plugs";
+            tboxRenameNewName.Text = "Cosmos.Core_Plugs";
+
+            mCosmosDir = IO.Path.GetFullPath(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\.."));
+            tblkCosmosDir.Text = tblkCosmosDir.Text + mCosmosDir;
+
+            AddSLN(@"Build.sln");
+            AddSLN(@"Builder.sln");
+            AddSLN(@"Compiler.sln");
+            AddSLN(@"Cosmos.sln");
+            AddSLN(@"IDE.sln");
+            AddSLN(@"Kernel.sln");
+            AddSLN(@"XSharp.sln");
+            AddSLN(@"source\Tools\Tools.sln");
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            mCosmosDir = IO.Path.GetFullPath(IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\.."));
-            tblkCosmosDir.Text = mCosmosDir;
+        void AddSLN(string aBaseName) {
+            mSlnList.Add(IO.Path.Combine(mCosmosDir, aBaseName + ".sln"));
+        }
+
+        private void tboxRenameNewName_GotFocus(object sender, RoutedEventArgs e) {
+            if (tboxRenameNewName.Text.Trim() == "") {
+                tboxRenameNewName.Text = tboxRenameOldName.Text.Trim();
+            }
+        }
+
+        private void butnRename_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }
