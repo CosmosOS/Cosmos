6f4b6098b4aacd3293c0921eaaffea1664f6dda4
José Pedro <jpedrolmail@gmail.com>
2017-07-14 14:17:49 +0100

    Label fixes.

--- source/Cosmos.Core_Asm/CPU/CPUUpdateIDTAsm.cs
+++ source/Cosmos.Core_Asm/CPU/CPUUpdateIDTAsm.cs
@@ -121,7 +121,7 @@ namespace Cosmos.Core_Asm
 
             // reload interrupt list
             XS.Set(EAX, "_NATIVE_IDT_Pointer");
-            XS.Set("static_field__Cosmos_Core_Common_CPU_mInterruptsEnabled", 1, destinationIsIndirect: true, size: RegisterSize.Byte8);
+            XS.Set("static_field__Cosmos_Core_CPU_mInterruptsEnabled", 1, destinationIsIndirect: true, size: RegisterSize.Byte8);
             XS.LoadIdt(EAX, isIndirect: true);
             // Reenable interrupts
             XS.EnableInterrupts();
