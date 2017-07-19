a3a47bcbed04f711968d8f61cf7e3dedc5cdd45c
Kudzu <czhower@users.noreply.github.com>
2017-07-19 13:08:06 -0400

    gen3

--- source/KernelGen3/Cosmos.Platform/TempHack.cs
+++ source/KernelGen3/Cosmos.Platform/TempHack.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Cosmos.Platform {
     static public class TempHack {
+        // Need to hack up Cosmos a bit
+        // Remove hard ref from System to HAL.
+        // Cant dyn load yet, so use IL2CPU to drag it in dynamically to prevent compile time usage
+        // Dont have reflection yet, but can make a Cosmos specific method to at least allow activationon by type.
         static public void Init() {
             SATA.InitHAL();
         }
