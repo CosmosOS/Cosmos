0cee95b9480b807144bb73383e661428c310150f
invisal <invisal@gmail.com>
2017-07-21 10:25:54 +0700

    Add EqualityHelper.DoubleAreEqual()

--- Tests/Cosmos.Compiler.Tests.Bcl/System/MathTest.cs
+++ Tests/Cosmos.Compiler.Tests.Bcl/System/MathTest.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Cosmos.TestRunner;
+using Cosmos.Compiler.Tests.Bcl.Helper;
 
 namespace Cosmos.Compiler.Tests.Bcl.System
 {
@@ -9,30 +10,19 @@ namespace Cosmos.Compiler.Tests.Bcl.System
     {
         public static void Execute()
         {
-            double acceptedError = 0.000001;
             double result;
-            double expectedResult;
-            double resultError;
 
             // Test with small number
             result = Math.Sqrt(16);
-            expectedResult = 4;
-            resultError = Math.Abs(result - expectedResult);
-
-            Assert.IsTrue((resultError < acceptedError), "Sqrt does not produce accurate result with small input");
+            Assert.IsTrue(EqualityHelper.DoublesAreEqual(result, 4), "Sqrt does not produce accurate result with small input");
 
             // Test with large number
             result = Math.Sqrt(2432146.513);
-            expectedResult = 1559.53406920143;
-            resultError = Math.Abs(result - expectedResult);
-
-            Assert.IsTrue((resultError < acceptedError), "Sqrt does not produce accurate result with large input");
+            Assert.IsTrue(EqualityHelper.DoublesAreEqual(result, 1559.53406920143), "Sqrt does not produce accurate result with large input");
 
             // Test with zero
             result = Math.Sqrt(0);
-            expectedResult = 0;
-
-            Assert.IsTrue((result == expectedResult), "Sqrt of zero must be zero");
+            Assert.IsTrue((result == 0), "Sqrt of zero must be zero");
 
             // Test with negative number
             result = Math.Sqrt(-433);
