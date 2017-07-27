e401916fdd2a735ce61c8adf4b5c11acdd2990e4
Kudzu <chad@hower.org>
2017-07-27 16:20:26 -0400

    g3

--- Demos/GuessG3/Boot.cs
+++ Demos/GuessG3/Boot.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
 using Sys = Cosmos.System;
 
  // Beware Demo Kernels are not recompiled when its dependencies changes!
  // To force recompilation right click on on the Cosmos icon of the demo solution and do "Build".
-namespace Guess {
+namespace KernelGen3 {
     public class Boot : Sys.Boot {
 
         protected override void Run() {
-            Console.WriteLine("Booted Kernel Gen3!");
+            //Console.WriteLine("Booted Kernel Gen3!");
+            Cosmos.CPU.Temp.ShowText();
+
             while (true) {
             }
         }
---
+++ source/Kernel/Cosmos.CPU/Temp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.CPU {
+    public static class Temp {
+        public static void ShowText() {
+
+        }
+    }
+}
