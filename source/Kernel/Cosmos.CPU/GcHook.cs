03a2a5febe1f4c87e91ded091bef4c91be47e273
Kudzu <chad@hower.org>
2017-07-27 14:14:54 -0400

    g3

---
+++ source/Kernel/Cosmos.CPU/GcHook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Cosmos.IL2CPU.API;
+
+namespace Cosmos.CPU {
+    [DebuggerStepThrough]
+    public static class GcHook {
+        private static void AcquireLock() {
+            throw new NotImplementedException();
+        }
+
+        private static void ReleaseLock() {
+            throw new NotImplementedException();
+        }
+
+        [PlugMethod(PlugRequired = true)]
+        public static uint AllocNewObject(uint aSize) {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This function gets the pointer to the memory location of where it's stored.
+        /// </summary>
+        /// <param name="aObject"></param>
+        public static unsafe void IncRefCount(uint aObject) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This function gets the pointer to the memory location of where it's stored.
+        /// </summary>
+        /// <param name="aObject"></param>
+        public static unsafe void DecRefCount(uint aObject) {
+            throw new NotImplementedException();
+        }
+    }
+}
