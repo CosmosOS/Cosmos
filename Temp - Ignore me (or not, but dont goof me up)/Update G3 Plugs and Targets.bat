967b36f5a8050145d1c744986cba3b0caccaa5bd
Kudzu <chad@hower.org>
2017-07-29 16:13:37 -0400

    g3

--- Temp - Ignore me (or not, but dont goof me up)/Update G3 Plugs and Targets.bat
+++ Temp - Ignore me (or not, but dont goof me up)/Update G3 Plugs and Targets.bat
@@ -1,6 +1,7 @@
 cd "C:\Users\zilon\AppData\Roaming\Cosmos User Kit\Kernel\"
 copy "C:\source\Cosmos\source\Kernel\Cosmos.System3_Plugs\bin\Debug\netstandard1.5\Cosmos.System3_Plugs.dll"
 copy "C:\source\Cosmos\source\Kernel\00-CPU\Cosmos.CPU_Plugs\bin\Debug\netstandard1.5\Cosmos.CPU_Plugs.dll"
+copy "C:\source\Cosmos\source\Kernel\00-CPU\Cosmos.CPU_Asm\bin\Debug\netstandard1.5\Cosmos.CPU_Asm.dll"
 
 cd C:\source\Cosmos\source\Cosmos.Build.MSBuild
 copy cosmos.targets "C:\Users\zilon\AppData\Roaming\Cosmos User Kit\Build\VSIP"
--- source/Kernel/00-CPU/Cosmos.CPU_Asm/Properties/AssemblyInfo.cs
+++ source/Kernel/00-CPU/Cosmos.CPU_Asm/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using Cosmos.IL2CPU.API;
 
-[assembly: Ring(Ring.Core)]
+//[assembly: Ring(Ring.Core)]
