c6099032cc27ec61d3cde45efd98642b42109432
Kudzu <chad@hower.org>
2017-07-29 14:01:28 -0400

    g3

---
+++ source/Kernel/00-CPU/Cosmos.CPU_Asm/CPU/CPUInitFloatAsm.cs
@@ -0,0 +1,12 @@
+using Cosmos.Assembler;
+using XSharp.Common;
+
+namespace Cosmos.CPU_Asm {
+    public class CPUInitFloatAsm : AssemblerMethod
+    {
+        public override void AssembleNew(Cosmos.Assembler.Assembler aAssembler, object aMethodInfo)
+        {
+            XS.FPU.FloatInit();
+        }
+    }
+}
