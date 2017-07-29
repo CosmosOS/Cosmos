4bef8ab35da8eb8be03e532ded838714c0af28fd
Kudzu <chad@hower.org>
2017-07-29 17:02:27 -0400

    g3

--- Temp - Ignore me (or not, but dont goof me up)/Update G3 Plugs and Targets.bat
+++ Temp - Ignore me (or not, but dont goof me up)/Update G3 Plugs and Targets.bat
@@ -1,5 +1,5 @@
 cd "C:\Users\zilon\AppData\Roaming\Cosmos User Kit\Kernel\"
-copy "C:\source\Cosmos\source\Kernel\Cosmos.System3_Plugs\bin\Debug\netstandard1.5\Cosmos.System3_Plugs.dll"
+copy "C:\source\Cosmos\source\Kernel\Cosmos.System_Plugs\bin\Debug\netstandard1.5\Cosmos.System_Plugs.dll"
 copy "C:\source\Cosmos\source\Kernel\00-CPU\Cosmos.CPU_Plugs\bin\Debug\netstandard1.5\Cosmos.CPU_Plugs.dll"
 copy "C:\source\Cosmos\source\Kernel\00-CPU\Cosmos.CPU_Asm\bin\Debug\netstandard1.5\Cosmos.CPU_Asm.dll"
 
