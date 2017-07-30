0322b14ff970168e1ab4c91c1a4c76272c4314a9
Kudzu <chad@hower.org>
2017-07-30 14:46:45 -0400

    g3

--- source/Kernel-X86/00-CPU/Cosmos.CPU/Processor.cs
+++ source/Kernel-X86/00-CPU/Cosmos.CPU/Processor.cs
@@ -3,80 +3,67 @@ using Cosmos.IL2CPU.API;
 
 namespace Cosmos.CPU {
     // Non hardware class, only used by core and hardware drivers for ports etc.
-    public class Processor
-    {
+    public class Processor {
         // Amount of RAM in MB's.
         // needs to be static, as Heap needs it before we can instantiate objects
         [PlugMethod(PlugRequired = true)]
-        public static uint GetAmountOfRAM()
-        {
+        public static uint GetAmountOfRAM() {
             throw new NotImplementedException();
         }
 
         // needs to be static, as Heap needs it before we can instantiate objects
         [PlugMethod(PlugRequired = true)]
-        public static uint GetEndOfKernel()
-        {
+        public static uint GetEndOfKernel() {
             throw new NotImplementedException();
         }
 
         [PlugMethod(PlugRequired = true)]
-        public void UpdateIDT(bool aEnableInterruptsImmediately)
-        {
+        public void UpdateIDT(bool aEnableInterruptsImmediately) {
             throw new NotImplementedException();
         }
 
         [PlugMethod(PlugRequired = true)]
-        public void InitFloat()
-        {
+        public void InitFloat() {
             throw new NotImplementedException();
         }
 
         [PlugMethod(PlugRequired = true)]
-        public void InitSSE()
-        {
+        public void InitSSE() {
             throw new NotImplementedException();
         }
 
         [PlugMethod(PlugRequired = true)]
-        public static void ZeroFill(uint aStartAddress, uint aLength)
-        {
+        public static void ZeroFill(uint aStartAddress, uint aLength) {
             throw new NotImplementedException();
         }
 
         [PlugMethod(PlugRequired = true)]
-        public void Halt()
-        {
+        public void Halt() {
             throw new NotImplementedException();
         }
 
-        public void Reboot()
-        {
+        public void Reboot() {
             // Disable all interrupts
             DisableInterrupts();
 
             var myPort = new IOPort(0x64);
-            while ((myPort.Byte & 0x02) != 0)
-            {
+            while ((myPort.Byte & 0x02) != 0) {
             }
             myPort.Byte = 0xFE;
             Halt(); // If it didn't work, Halt the Processor
         }
 
-        private static void DoEnableInterrupts()
-        {
+        private static void DoEnableInterrupts() {
             throw new NotImplementedException();
         }
 
-        private static void DoDisableInterrupts()
-        {
+        private static void DoDisableInterrupts() {
             throw new NotImplementedException();
         }
 
         public static bool mInterruptsEnabled;
 
-        public static void EnableInterrupts()
-        {
+        public static void EnableInterrupts() {
             mInterruptsEnabled = true;
             DoEnableInterrupts();
         }
@@ -85,8 +72,7 @@ namespace Cosmos.CPU {
         /// Returns if the interrupts were actually enabled
         /// </summary>
         /// <returns></returns>
-        public static bool DisableInterrupts()
-        {
+        public static bool DisableInterrupts() {
             DoDisableInterrupts();
             var xResult = mInterruptsEnabled;
             mInterruptsEnabled = false;
