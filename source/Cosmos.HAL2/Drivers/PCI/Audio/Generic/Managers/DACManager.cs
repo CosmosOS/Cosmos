a58e3f2fa0c1251792755982b90c4fd8fbac7928
José Pedro <jpedrolmail@gmail.com>
2017-07-30 18:38:20 +0100

    g3

---
+++ source/Cosmos.HAL2/Drivers/PCI/Audio/Generic/Managers/DACManager.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Cosmos.Hardware2.Audio.Devices.Generic.Managers
+//{
+//    public abstract class DACManager
+//    {
+//        protected Cosmos.Hardware2.Audio.Devices.Generic.Components.DACEntity dacEntity;
+//        public DACManager(Cosmos.Hardware2.Audio.Devices.Generic.Components.DACEntity dacEntity)
+//        {
+//            this.dacEntity = dacEntity;
+//        }
+//        protected abstract bool setDacActiveState(bool state);
+//    }
+
+//}
