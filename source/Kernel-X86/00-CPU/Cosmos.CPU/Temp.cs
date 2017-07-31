b15c40ddcf91508fd665d8fde8520346cb8ad52b
Kudzu <chad@hower.org>
2017-07-31 13:32:14 -0400

    g3

--- source/Kernel-X86/00-CPU/Cosmos.CPU/Temp.cs
+++ source/Kernel-X86/00-CPU/Cosmos.CPU/Temp.cs
@@ -5,7 +5,13 @@ using System.Text;
 namespace Cosmos.CPU {
     public static class Temp {
         public static void ShowText() {
+            unsafe {
+                byte* xTest = (byte*)0xB8000;
+                *xTest = 65;
 
+                xTest = (byte*)0xB8001;
+                *xTest = 90;
+            }
         }
     }
 }
