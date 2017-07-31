945dd1627ea02b1221e5835855ea89a8cccc9cf4
Kudzu <chad@hower.org>
2017-07-31 19:27:12 -0400

    g3

--- Temp - Ignore me (or not, but dont goof me up)/Update G3.bat
+++ Temp - Ignore me (or not, but dont goof me up)/Update G3.bat
@@ -11,6 +11,7 @@ copy "00-CPU\Cosmos.CPU_Asm\bin\Debug\netstandard1.5\Cosmos.CPU_Asm.dll" "%USERP
 copy "80-System\Cosmos.System_Plugs\bin\Debug\netstandard1.5\Cosmos.System_Plugs.dll" "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Kernel\"
 cd ..\..
 
-copy source\IL2CPU\bin\Debug\netcoreapp1.0 "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Build\IL2CPU"
+REM copy source\IL2CPU\bin\Debug\netcoreapp1.0 "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Build\IL2CPU"
+dotnet publish source\IL2CPU -r win7-x86 -o "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Build\IL2CPU"
 
 @timeout 3
