e19dfd6b338b43a67450c36d50081a989b8413d8
Kudzu <chad@hower.org>
2017-07-31 14:49:24 -0400

    g3 bat

--- Temp - Ignore me (or not, but dont goof me up)/Update G3 Plugs and Targets.bat
+++ Temp - Ignore me (or not, but dont goof me up)/Update G3 Plugs and Targets.bat
@@ -1,12 +1,13 @@
-cd "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Kernel\"
-copy "..\source\Kernel-x86\00-CPU\Cosmos.CPU_Plugs\bin\Debug\netstandard1.5\Cosmos.CPU_Plugs.dll"
-copy "..\source\Kernel-x86\00-CPU\Cosmos.CPU_Asm\bin\Debug\netstandard1.5\Cosmos.CPU_Asm.dll"
-copy "..\source\Kernel-x86\80-System\Cosmos.System_Plugs\bin\Debug\netstandard1.5\Cosmos.System_Plugs.dll"
-
-cd C:\source\Cosmos\source\Cosmos.Build.MSBuild
+cd ..\source\Cosmos.Build.MSBuild
 copy cosmos.targets "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Build\VSIP"
-copy cosmos.targets "..\Build\VSIP\"
-copy cosmos.targets "..\Build\VSIP\MSBuild\"
-copy cosmos.targets "..\source\Cosmos.Build.MSBuild\bin\Debug\net462\win7-x86\"
+copy cosmos.targets "..\..\Build\VSIP\"
+copy cosmos.targets "..\..\Build\VSIP\MSBuild\"
+copy cosmos.targets "..\..\source\Cosmos.Build.MSBuild\bin\Debug\net462\win7-x86\"
+cd ..\..
+
+cd source\kernel-x86
+copy "00-CPU\Cosmos.CPU_Plugs\bin\Debug\netstandard1.5\Cosmos.CPU_Plugs.dll" "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Kernel\"
+copy "00-CPU\Cosmos.CPU_Asm\bin\Debug\netstandard1.5\Cosmos.CPU_Asm.dll" "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Kernel\"
+copy "80-System\Cosmos.System_Plugs\bin\Debug\netstandard1.5\Cosmos.System_Plugs.dll" "%USERPROFILE%\AppData\Roaming\Cosmos User Kit\Kernel\"
 
 @timeout 3
