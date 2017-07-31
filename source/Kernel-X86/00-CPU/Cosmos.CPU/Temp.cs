2e57ffd3127db870e94cb873c93d82a35147ca38
Kudzu <chad@hower.org>
2017-07-31 13:46:22 -0400

    g3

--- source/Kernel-X86/00-CPU/Cosmos.CPU/Temp.cs
+++ source/Kernel-X86/00-CPU/Cosmos.CPU/Temp.cs
@@ -8,9 +8,13 @@ namespace Cosmos.CPU {
             unsafe {
                 byte* xTest = (byte*)0xB8000;
                 *xTest = 65;
-
                 xTest = (byte*)0xB8001;
+                *xTest = 0x0A;
+
+                xTest = (byte*)0xB8002;
                 *xTest = 90;
+                xTest = (byte*)0xB8003;
+                *xTest = 0x0A;
             }
         }
     }
