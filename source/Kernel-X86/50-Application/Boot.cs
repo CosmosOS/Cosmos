0fb79d934a9f54798efc237c1c54c17b60e44ea9
José Pedro <jpedrolmail@gmail.com>
2017-08-07 19:29:44 +0100

    g3

--- source/Kernel-X86/50-Application/Boot.cs
+++ source/Kernel-X86/50-Application/Boot.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+
+using Cosmos.IL2CPU.API.Attribs;
 using Sys = Cosmos.System;
 
 namespace KernelGen3 {
     public class Boot : Sys.BootOld {
-
+        [BootEntry]
         protected override void Run() {
             // temp
             Cosmos.Platform.PC.Boot.Init();
             // temp
-            Cosmos.System.Boot.Init();
+            //Cosmos.System.Boot.Init();
 
             Cosmos.Platform.PC.Debug.ShowText();
 
