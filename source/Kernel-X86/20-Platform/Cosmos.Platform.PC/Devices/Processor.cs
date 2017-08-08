3bbeb41f97dfcefa95e205fa18c6e12498ae1892
Kudzu <chad@hower.org>
2017-08-08 10:41:44 -0400

    g3

--- source/Kernel-X86/20-Platform/Cosmos.Platform.PC/Devices/Processor.cs
+++ source/Kernel-X86/20-Platform/Cosmos.Platform.PC/Devices/Processor.cs
@@ -4,5 +4,8 @@ using System.Text;
 
 namespace Cosmos.Platform.PC.Devices {
     public class Processor : HAL.Devices.Processor {
+        public override ulong SetOption(uint aID, ulong aValue = 0) {
+            return base.SetOption(aID, aValue);
+        }
     }
 }
