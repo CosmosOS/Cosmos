240c4e8a0106bda116ebf997c0afa83d0eea6b1a
Arawn Davies <arawn.davies780@gmail.com>
2017-08-13 14:32:57 +0100

    Install-vs2017 checks

--- install-VS2017.bat
+++ install-VS2017.bat
@@ -3,6 +3,20 @@ cls
 
 set NuGet=Build\Tools\nuget.exe
 set VSWhere=Build\Tools\vswhere.exe
+set IL2CPU=..\IL2CPU\IL2CPU.sln
+set XS=..\XSharp\XSharp.sln
+
+echo Finding XSharp repo directory
+IF EXIST %XS% (echo XSharp solution found!) else ( 
+echo XSharp not found!
+goto NOI2CXS 
+)
+
+echo Looking for IL2CPU repo directory...
+IF EXIST %IL2CPU% (echo IL2CPU solution found!) else (
+echo IL2CPU not found!
+goto NOI2CXS )
+
 
 :NuGet
 echo Running NuGet restore
@@ -14,6 +28,7 @@ for /f "usebackq delims=" %%i in (`%VSWhere% -latest -version "[15.0,16.0)" -req
   set InstallDir=%%i
 )
 
+
 if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
   set MSBuild="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
 )
@@ -22,4 +37,10 @@ if exist %MSBuild% (
   %MSBuild% Builder.sln /nologo /maxcpucount /nodeReuse:false /p:Configuration="Debug" /p:Platform="Any CPU"
 )
 
+
 start "Cosmos Builder" "source\Cosmos.Build.Builder\bin\Debug\Cosmos.Build.Builder.exe" "-VSPATH=%InstallDir%" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+
+:NOI2CXS
+echo install-vs2017.bat failed to find the required IL2CPU or XSharp directories
+echo Be sure to see the Cosmos documentation for instructions on building Cosmos
