ba185b2dfd61439244983db6a9f0e8b618450f70
KingLuigi4932 <slmanarendo1950@gmail.com>
2017-09-14 00:06:10 -0700

    [CGS] VmWare Support: Fix Issue

--- Demos/CosmosGraphicSubsystem/Kernel.cs
+++ Demos/CosmosGraphicSubsystem/Kernel.cs
@@ -17,12 +17,12 @@ namespace Cosmos_Graphic_Subsytem
 
             var xSVGAIIDevice = Cosmos.HAL.PCI.GetDevice(0x15AD, 0x0405);
                         
-            var xSVGAIIExists = xVideoDriver.DeviceExists;
+            var xSVGAIIExists = xSVGAIIDevice.DeviceExists;
            /* Check if there is SVGAII Device here to
             * Get on istance of the Canvas that is all the Screen
             */
             if(xSVGAIIExists) canvas = FullScreenCanvas.GetFullScreenCanvas(FullScreenCanvas.VideoDriver.VBEDriver);
-            else canvas = FullScreenCanvas.GetFullScreenCanvas(FullScreenCanas.VideoDriver.VMWareSVGAIIDrier);
+            else canvas = FullScreenCanvas.GetFullScreenCanvas(FullScreenCanvas.VideoDriver.VMWareSVGAIIDrier);
 
             /* Clear the Screen with the color 'Blue' */
             canvas.Clear(Color.Blue);
