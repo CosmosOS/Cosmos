6f7b312755fffe3a965858aa6091d4b29bc85e82
KingLuigi4932 <slmanarendo1950@gmail.com>
2017-12-23 23:01:19 +0300

    Update IDE.cs

--- source/Cosmos.HAL2/BlockDevice/IDE.cs
+++ source/Cosmos.HAL2/BlockDevice/IDE.cs
@@ -7,7 +7,7 @@ namespace Cosmos.HAL.BlockDevice
     {
         private static PCIDevice xDevice = HAL.PCI.GetDeviceClass(0x01, 0x01);
 
-        internal static void InitDriver()
+        public static void InitDriver()
         {
             if (xDevice == null)
             {
