7a2a87c6fb59baaaf4007385aace7162d911ea9b
José Pedro <jpedrolmail@gmail.com>
2017-12-25 23:47:20 +0000

    Cosmos project upgrader.

--- source/Cosmos.VS.ProjectSystem/CosmosProjectSystemPackage.cs
+++ source/Cosmos.VS.ProjectSystem/CosmosProjectSystemPackage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 using Cosmos.VS.ProjectSystem.VS.PropertyPages;
 
@@ -9,11 +10,20 @@ namespace Cosmos.VS.ProjectSystem
     [Guid(PackageGuid)]
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [ProvideObject(typeof(CosmosPropertyPage))]
+    [ProvideProjectFactory(typeof(MigrateCosmosProjectFactory), null, "Cosmos Project Files (*.Cosmos);*.Cosmos", "Cosmos", "Cosmos", null)]
     internal class CosmosProjectSystemPackage : Package
     {
         /// <summary>
         /// The GUID for this package.
         /// </summary>
         public const string PackageGuid = "29194faf-90ce-454b-bc53-08b722f1dadf";
+
+        private IVsProjectFactory _factory;
+
+        protected override void Initialize()
+        {
+            _factory = new MigrateCosmosProjectFactory();
+            RegisterProjectFactory(_factory);
+        }
     }
 }
---
+++ source/Cosmos.VS.ProjectSystem/MigrateCosmosProjectFactory.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Cosmos.VS.ProjectSystem
+{
+    internal class MigrateCosmosProjectFactory : IVsProjectFactory, IVsProjectUpgradeViaFactory, IVsProjectUpgradeViaFactory4
+    {
+        private const string CSharpProjectTypeGuid = "9A19103F-16F7-4668-BE54-9A1E7A4F7556";
+        private const string FSharpProjectTypeGuid = "6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705";
+        private const string VisualBasicProjectTypeGuid = "778DAE3C-4631-46EA-AA77-85C1314464D9";
+
+        private static readonly List<string> CommonProjectProperties = new List<string>()
+        {
+            "TargetFramework",
+            "OutputType",
+            "AppDesignerFolder",
+            "ProjectGuid",
+            "RootNamespace",
+            "AssemblyName"
+        };
+
+        private static readonly List<string> PlugsProjects = new List<string>()
+        {
+            "Cosmos.Core.Plugs.Asm",
+            "Cosmos.Core.Plugs",
+            "Cosmos.Core_Asm",
+            "Cosmos.Core_Plugs",
+            "Cosmos.Debug.Kernel.Plugs.Asm",
+            "Cosmos.System.Plugs",
+            "Cosmos.System_Plugs",
+            "Cosmos.System2.Plugs",
+            "Cosmos.System2_Plugs"
+        };
+
+        #region IVsProjectFactory
+
+        public int CanCreateProject(string pszFilename, uint grfCreateFlags, out int pfCanCreate)
+        {
+            pfCanCreate = !String.IsNullOrEmpty(pszFilename) && !PackageUtilities.IsFileNameInvalid(pszFilename) ? 1 : 0;
+            return VSConstants.S_OK;
+        }
+
+        public int CreateProject(string pszFilename, string pszLocation, string pszName, uint grfCreateFlags,
+            ref Guid iidProject, out IntPtr ppvProject, out int pfCanceled)
+        {
+            ppvProject = IntPtr.Zero;
+            pfCanceled = 0;
+
+            return VSConstants.S_OK;
+        }
+
+        public int SetSite(Microsoft.VisualStudio.OLE.Interop.IServiceProvider psp) => VSConstants.S_OK;
+
+        public int Close() => VSConstants.S_OK;
+
+        #endregion
+
+        #region IVsProjectUpgradeViaFactory
+
+        public int UpgradeProject(string bstrFileName, uint fUpgradeFlag, string bstrCopyLocation,
+            out string pbstrUpgradedFullyQualifiedFileName, IVsUpgradeLogger pLogger, out int pUpgradeRequired,
+            out Guid pguidNewProjectFactory)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var projectDir = new DirectoryInfo(Path.GetDirectoryName(bstrFileName));
+            var projectName = Path.GetFileNameWithoutExtension(bstrFileName);
+
+            string codeProjectPath;
+
+            UpgradeProject_CheckOnly(bstrFileName, pLogger, out pUpgradeRequired, out pguidNewProjectFactory, out var flags);
+
+            using (var projectStream = File.OpenRead(bstrFileName))
+            {
+                var document = XDocument.Load(projectStream);
+                var itemGroups = document.Root.Descendants().Where(e => e.Name == "ItemGroup");
+                var projectReferences = itemGroups.Descendants().Where(e => e.Name == "ProjectReference");
+
+                if (!projectReferences.Any())
+                {
+                    pLogger.LogMessage((uint)__VSUL_ERRORLEVEL.VSUL_ERROR, projectName, bstrFileName, "Invalid project!");
+
+                    pbstrUpgradedFullyQualifiedFileName = bstrFileName;
+                    return VSConstants.VS_E_PROJECTMIGRATIONFAILED;
+                }
+                else if (projectReferences.Count() == 1)
+                {
+                    var codeProject = projectReferences.Single().Attributes().Where(a => a.Name == "Include").Single().Value;
+                    codeProjectPath = Path.IsPathRooted(codeProject) ? codeProject
+                        : Path.GetFullPath(Path.Combine(projectDir.FullName, codeProject));
+                }
+                else
+                {
+                    var projectReferencesWithoutPlugsProjects = projectReferences.Where(
+                        p => !PlugsProjects.Contains(
+                            Path.GetFileNameWithoutExtension(
+                                p.Attributes().Where(
+                                    a => a.Name == "Include").Single().Value), StringComparer.OrdinalIgnoreCase));
+
+                    if (projectReferencesWithoutPlugsProjects.Count() > 1)
+                    {
+                        pLogger.LogMessage((uint)__VSUL_ERRORLEVEL.VSUL_WARNING, projectName, bstrFileName,
+                            "Selecting project reference based on Cosmos project name!");
+
+                        var codeProjectName = projectName.EndsWith("Boot") ? projectName.TrimSuffix("Boot") : projectName;
+                        var codeProjectPathWithoutExtension = Path.Combine(projectDir.FullName, codeProjectName);
+
+                        var possibleCodeProjects = projectReferences.Where(
+                            p => Path.GetFileNameWithoutExtension(
+                                p.Attributes().Where(
+                                    a => a.Name == "Include").Single().Value) == codeProjectName);
+
+                        if (possibleCodeProjects.Count() != 1)
+                        {
+                            pLogger.LogMessage((uint)__VSUL_ERRORLEVEL.VSUL_ERROR, projectName, bstrFileName,
+                                "Cannot find the code project!");
+
+                            pbstrUpgradedFullyQualifiedFileName = bstrFileName;
+                            return VSConstants.VS_E_PROJECTMIGRATIONFAILED;
+                        }
+
+                        var codeProject =
+                            possibleCodeProjects.Single().Attributes().Where(a => a.Name == "Include").Single().Value;
+                        codeProjectPath = Path.IsPathRooted(codeProject) ? codeProject
+                            : Path.GetFullPath(Path.Combine(projectDir.FullName, codeProject));
+                    }
+                    else
+                    {
+                        var codeProject = projectReferencesWithoutPlugsProjects
+                            .Single().Attributes().Where(a => a.Name == "Include").Single().Value;
+                        codeProjectPath = Path.IsPathRooted(codeProject) ? codeProject
+                            : Path.GetFullPath(Path.Combine(projectDir.FullName, codeProject));
+                    }
+                }
+            }
+
+            File.Copy(bstrFileName, Path.Combine(bstrCopyLocation, Path.GetFileName(bstrFileName)));
+            File.Copy(codeProjectPath, Path.Combine(bstrCopyLocation, Path.GetFileName(codeProjectPath)));
+
+            MigrateProject(bstrFileName, codeProjectPath);
+
+            pbstrUpgradedFullyQualifiedFileName = codeProjectPath;
+            return VSConstants.S_OK;
+        }
+
+        public int UpgradeProject_CheckOnly(string bstrFileName, IVsUpgradeLogger pLogger, out int pUpgradeRequired,
+            out Guid pguidNewProjectFactory, out uint pUpgradeProjectCapabilityFlags)
+        {
+            var isCosmos = String.Equals(Path.GetExtension(bstrFileName), ".Cosmos", StringComparison.OrdinalIgnoreCase);
+
+            pUpgradeRequired = isCosmos ? (int)__VSPPROJECTUPGRADEVIAFACTORYREPAIRFLAGS.VSPUVF_PROJECT_ONEWAYUPGRADE
+                : (int)__VSPPROJECTUPGRADEVIAFACTORYREPAIRFLAGS.VSPUVF_PROJECT_NOREPAIR;
+
+            if (isCosmos)
+            {
+                string projectTypeGuid = null;
+
+                if (Directory.GetFiles(Path.GetDirectoryName(bstrFileName), "*.cs", SearchOption.AllDirectories).Any())
+                {
+                    projectTypeGuid = CSharpProjectTypeGuid;
+                }
+
+                if (Directory.GetFiles(Path.GetDirectoryName(bstrFileName), "*.fs", SearchOption.AllDirectories).Any())
+                {
+                    projectTypeGuid = FSharpProjectTypeGuid;
+                }
+
+                if (Directory.GetFiles(Path.GetDirectoryName(bstrFileName), "*.vb", SearchOption.AllDirectories).Any())
+                {
+                    projectTypeGuid = VisualBasicProjectTypeGuid;
+                }
+
+                pguidNewProjectFactory = new Guid(projectTypeGuid);
+                pUpgradeProjectCapabilityFlags = (uint)(__VSPPROJECTUPGRADEVIAFACTORYFLAGS.PUVFF_BACKUPSUPPORTED | __VSPPROJECTUPGRADEVIAFACTORYFLAGS.PUVFF_COPYBACKUP);
+            }
+            else
+            {
+                pguidNewProjectFactory = GetType().GUID;
+                pUpgradeProjectCapabilityFlags = 0;
+            }
+
+            return VSConstants.S_OK;
+        }
+
+        public int GetSccInfo(string bstrProjectFileName, out string pbstrSccProjectName, out string pbstrSccAuxPath,
+            out string pbstrSccLocalPath, out string pbstrProvider) => throw new NotImplementedException();
+
+        #endregion
+
+        #region IVsProjectUpgradeViaFactory4
+
+        public void UpgradeProject_CheckOnly(string pszFileName, IVsUpgradeLogger pLogger, out uint pUpgradeRequired,
+            out Guid pguidNewProjectFactory, out uint pUpgradeProjectCapabilityFlags)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            UpgradeProject_CheckOnly(pszFileName, pLogger, out int upgradeRequired, out pguidNewProjectFactory,
+                out pUpgradeProjectCapabilityFlags);
+
+            pUpgradeRequired = (uint)upgradeRequired;
+        }
+
+        #endregion
+
+        private void MigrateProject(string cosmosProject, string codeProject)
+        {
+            var cosmosProjectProperties = XDocument.Parse(File.ReadAllText(cosmosProject)).Root.Descendants()
+                .Where(d => d.Name == "PropertyGroup").Select(p => p.Descendants()
+                .Where(e => !CommonProjectProperties.Contains(e.Name.LocalName, StringComparer.OrdinalIgnoreCase)));
+
+            var codeProjectStream = File.OpenRead(codeProject);
+
+            var document = XDocument.Load(codeProjectStream);
+            var itemGroups = document.Root.Descendants().Where(e => e.Name == "ItemGroup");
+            var projectReferences = itemGroups.Descendants().Where(
+                e => e.Name == "ProjectReference" && e.Attributes().Where(
+                    a => a.Name == "Include" &&
+                    (a.Value.EndsWith("Cosmos.System.csproj") || a.Value.EndsWith("Cosmos.System2.csproj")
+                    || a.Value.EndsWith("Cosmos.Debug.Kernel.csproj")))
+                    .Count() == 0);
+            var packageReferences = itemGroups.Descendants().Where(
+                e => e.Name == "PackageReference" && e.Attributes().Where(
+                    a => a.Name == "Include" &&
+                    (a.Value == "Cosmos.System" || a.Value == "Cosmos.System2" || a.Value == "Cosmos.Debug.Kernel"))
+                    .Count() == 0);
+            var references = itemGroups.Descendants().Where(e => e.Name == "Reference");
+
+            codeProjectStream.Dispose();
+
+            File.WriteAllText(codeProject, GetProjectTemplate());
+
+            var codeProjectDocument = XDocument.Parse(File.ReadAllText(codeProject));
+
+            codeProjectDocument.Root.Descendants().Where(d => d.Name == "PropertyGroup").LastOrDefault()
+                .Add(cosmosProjectProperties);
+
+            if (projectReferences.Count() > 0)
+            {
+                codeProjectDocument.Root.Add(new XElement("ItemGroup", projectReferences));
+            }
+
+            if (packageReferences.Count() > 0)
+            {
+                codeProjectDocument.Root.Add(new XElement("ItemGroup", packageReferences));
+            }
+
+            if (references.Count() > 0)
+            {
+                codeProjectDocument.Root.Add(new XElement("ItemGroup", references));
+            }
+
+            using (var xmlWriter = XmlWriter.Create(codeProject,
+                new XmlWriterSettings()
+                {
+                    Indent = true,
+                    IndentChars = "    ",
+                    OmitXmlDeclaration = true
+                }))
+            {
+                codeProjectDocument.Save(xmlWriter);
+            }
+
+            File.Delete(cosmosProject);
+        }
+
+        private string GetProjectTemplate()
+        {
+            using (var projectTemplateStream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("Cosmos.VS.ProjectSystem.ProjectTemplate.xml"))
+            {
+                using (var projectTemplateReader = new StreamReader(projectTemplateStream))
+                {
+                    return projectTemplateReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
