1df3dc19f50ccc628f63e260c8dedb19e994e35f
José Pedro <jpedrolmail@gmail.com>
2018-04-03 19:55:11 +0100

    Cosmos property page.

--- source/Cosmos.VS.ProjectSystem/ProjectSystem/VS/PropertyPages/ViewModels/CosmosPropertyPageViewModel.cs
+++ source/Cosmos.VS.ProjectSystem/ProjectSystem/VS/PropertyPages/ViewModels/CosmosPropertyPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.ProjectSystem;
 
 using VSPropertyPages;
@@ -8,32 +9,17 @@ namespace Cosmos.VS.ProjectSystem.VS.PropertyPages.ViewModels
 {
     internal class CosmosPropertyPageViewModel : PropertyPageViewModel
     {
-        public CosmosPropertyPageViewModel(
-            IPropertyManager propertyManager,
-            IProjectThreadingService projectThreadingService)
-            : base(propertyManager, projectThreadingService)
-        {
-        }
+        #region Static Items
 
-        public bool DebugEnabled
-        {
-            get => String.Equals(GetProperty(nameof(DebugEnabled)), "True", StringComparison.OrdinalIgnoreCase);
-            set => SetProperty(nameof(DebugEnabled), value.ToString(), nameof(DebugEnabled));
-        }
+        private static readonly List<int> DebugComItemsList = new List<int>() { 1, 2, 3, 4 };
 
-        public Dictionary<string, string> DebugModeItems { get; } = new Dictionary<string, string>()
+        private static readonly Dictionary<string, string> DebugModeItemsDictionary = new Dictionary<string, string>()
         {
             ["IL"] = "IL",
             ["Source"] = "Source"
         };
 
-        public string DebugMode
-        {
-            get => GetProperty(nameof(DebugMode));
-            set => SetProperty(nameof(DebugMode), value);
-        }
-
-        public Dictionary<string, string> TraceModeItems { get; } = new Dictionary<string, string>()
+        private static readonly Dictionary<string, string> TraceModeItemsDictionary = new Dictionary<string, string>()
         {
             ["None"] = "None",
             ["User"] = "User",
@@ -41,10 +27,48 @@ namespace Cosmos.VS.ProjectSystem.VS.PropertyPages.ViewModels
             ["All"] = "All"
         };
 
+        private static readonly Dictionary<string, string> StackCorruptionDetectionLevelItemsDictionary = new Dictionary<string, string>()
+        {
+            ["AllInstructions"] = "All Instructions",
+            ["MethodFooters"] = "Method Footers"
+        };
+
+        #endregion
+
+        public bool DebugEnabled
+        {
+            get => String.Equals(GetProperty(nameof(DebugEnabled)), "True", StringComparison.OrdinalIgnoreCase);
+            set => SetProperty(nameof(DebugEnabled), value.ToString(), nameof(DebugEnabled));
+        }
+
+        public List<int> DebugComItems => DebugComItemsList;
+
+        public int DebugCom
+        {
+            get => Int32.Parse(GetProperty(nameof(DebugCom)));
+            set => SetProperty(nameof(DebugCom), value.ToString());
+        }
+
+        public Dictionary<string, string> DebugModeItems => DebugModeItemsDictionary;
+
+        public string DebugMode
+        {
+            get => GetProperty(nameof(DebugMode));
+            set => SetProperty(nameof(DebugMode), value);
+        }
+
+        public Dictionary<string, string> TraceModeItems => TraceModeItemsDictionary;
+
         public string TraceMode
         {
             get => GetProperty(nameof(TraceMode));
             set => SetProperty(nameof(TraceMode), value);
+        }
+
+        public bool IgnoreDebugStubAttribute
+        {
+            get => String.Equals(GetProperty(nameof(IgnoreDebugStubAttribute)), "True", StringComparison.OrdinalIgnoreCase);
+            set => SetProperty(nameof(IgnoreDebugStubAttribute), value.ToString(), nameof(IgnoreDebugStubAttribute));
         }
 
         public bool StackCorruptionDetectionEnabled
@@ -53,11 +77,7 @@ namespace Cosmos.VS.ProjectSystem.VS.PropertyPages.ViewModels
             set => SetProperty(nameof(StackCorruptionDetectionEnabled), value.ToString(), nameof(StackCorruptionDetectionEnabled));
         }
 
-        public Dictionary<string, string> StackCorruptionDetectionLevelItems { get; } = new Dictionary<string, string>()
-        {
-            ["AllInstructions"] = "All Instructions",
-            ["MethodFooters"] = "Method Footers"
-        };
+        public Dictionary<string, string> StackCorruptionDetectionLevelItems => StackCorruptionDetectionLevelItemsDictionary;
 
         public string StackCorruptionDetectionLevel
         {
@@ -77,10 +97,20 @@ namespace Cosmos.VS.ProjectSystem.VS.PropertyPages.ViewModels
             set => SetProperty(nameof(VisualStudioDebugPort), value);
         }
 
-        public bool IgnoreDebugStubAttribute
+        public CosmosPropertyPageViewModel(
+            IPropertyManager propertyManager,
+            IProjectThreadingService projectThreadingService)
+            : base(propertyManager, projectThreadingService)
         {
-            get => String.Equals(GetProperty(nameof(IgnoreDebugStubAttribute)), "True", StringComparison.OrdinalIgnoreCase);
-            set => SetProperty(nameof(IgnoreDebugStubAttribute), value.ToString(), nameof(IgnoreDebugStubAttribute));
+        }
+
+        private void SetAndRaiseIfChanged<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
         }
     }
 }
