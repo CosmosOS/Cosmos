e5085e32610179c48feeca36bc45c6380556e499
Og-Rok <og-rok@outlook.com>
2018-04-04 22:41:42 +0100

    Mutex

---
+++ source/Cosmos.Core/Processing/Mutex.cs
@@ -0,0 +1,25 @@
+﻿using IL2CPU.API.Attribs;
+
+namespace Cosmos.Core.Processing
+{
+    public unsafe class Mutex
+    {
+        public int gate;
+
+        [PlugMethod(PlugRequired = true)]
+        public static void MutexLock(int* mtx) { }
+
+        public void Lock()
+        {
+            fixed (int* p = &gate)
+            {
+                MutexLock(p);
+            }
+        }
+
+        public void Unlock()
+        {
+            gate = 0;
+        }
+    }
+}
