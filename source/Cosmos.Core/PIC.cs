e95910f10808162006bd036e984191e62abf5f8b
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-06-03 22:15:02 +0200

    PIC Fix.

--- source/Cosmos.Core/PIC.cs
+++ source/Cosmos.Core/PIC.cs
@@ -44,7 +44,7 @@ namespace Cosmos.Core
             //Init(Master, 0x20, 4, 0xFD | 0x08);
             //Init(Slave, 0x28, 2, 0xFF);
             //for now enable keyboard, mouse(ps2)
-            Remap(0x20, 0xF8 | 0x08, 0x28, 0xEF);
+            Remap(0x20, 0xF8 | 0x08, 0x28, 0xEB);
         }
 
         private void Remap(byte masterStart, byte masterMask, byte slaveStart, byte slaveMask)
