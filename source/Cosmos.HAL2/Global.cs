f9290979734ab7beb6f65a31de004b593f7147d1
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-07-15 16:14:35 +0200

    Conflicts fixed.

--- source/Cosmos.HAL2/Global.cs
+++ source/Cosmos.HAL2/Global.cs
@@ -6,98 +6,89 @@ using Cosmos.Debug.Kernel;
 using Cosmos.HAL.BlockDevice;
 
 namespace Cosmos.HAL
-{
-  public static class Global
   {
-    public static readonly Debugger mDebugger = new Debugger("HAL", "Global");
-
-    static public PIT PIT = new PIT();
-    // Must be static init, other static inits rely on it not being null
-
-    public static TextScreenBase TextScreen = new TextScreen();
-    public static PCI Pci;
-
-    static public void Init(TextScreenBase textScreen)
+    public static class Global
     {
-      if (textScreen != null)
-      {
-        TextScreen = textScreen;
-      }
+        public static readonly Debugger mDebugger = new Debugger("HAL", "Global");
 
-      mDebugger.Send("Before Core.Global.Init");
-      Core.Global.Init();
+        static public PIT PIT = new PIT();
+        // Must be static init, other static inits rely on it not being null
 
-      //TODO Redo this - Global init should be other.
-      // Move PCI detection to hardware? Or leave it in core? Is Core PC specific, or deeper?
-      // If we let hardware do it, we need to protect it from being used by System.
-      // Probably belongs in hardware, and core is more specific stuff like CPU, memory, etc.
-      //Core.PCI.OnPCIDeviceFound = PCIDeviceFound;
+        public static TextScreenBase TextScreen = new TextScreen();
+        public static PCI Pci;
 
-      //TODO: Since this is FCL, its "common". Otherwise it should be
-      // system level and not accessible from Core. Need to think about this
-      // for the future.
+
+        //TODO Redo this - Global init should be other.
+        // Move PCI detection to hardware? Or leave it in core? Is Core PC specific, or deeper?
+        // If we let hardware do it, we need to protect it from being used by System.
+        // Probably belongs in hardware, and core is more specific stuff like CPU, memory, etc.
+        //Core.PCI.OnPCIDeviceFound = PCIDeviceFound;
+
+        //TODO: Since this is FCL, its "common". Otherwise it should be
+        // system level and not accessible from Core. Need to think about this
+        // for the future.
 
         public static PS2Controller PS2Controller = new PS2Controller();
 
         static public void Init(TextScreenBase textScreen)
         {
-            if (textScreen != null)
-            {
-                TextScreen = textScreen;
-            }
+          if (textScreen != null)
+          {
+              TextScreen = textScreen;
+          }
 
-      Console.WriteLine("Finding PCI Devices");
-      mDebugger.Send("PCI Devices");
-      PCI.Setup();
+          Console.WriteLine("Finding PCI Devices");
+          mDebugger.Send("PCI Devices");
+          PCI.Setup();
 
-      Console.WriteLine("Starting ACPI");
-      mDebugger.Send("ACPI Init");
-      ACPI.Start();
+          Console.WriteLine("Starting ACPI");
+          mDebugger.Send("ACPI Init");
+          ACPI.Start();
       
-      Console.WriteLine("Finding ATA Devices");
-      mDebugger.Send("ATA Devices");
-      IDE.InitDriver();
-      AHCI.InitDriver();
-      //EHCI.InitDriver();
+          Console.WriteLine("Finding ATA Devices");
+          mDebugger.Send("ATA Devices");
+          IDE.InitDriver();
+          AHCI.InitDriver();
+          //EHCI.InitDriver();
 
-      Console.WriteLine("Starting Processor Scheduler");
-      mDebugger.Send("Processor Scheduler");
-      Core.Processing.ProcessorScheduler.Initialize();
+          Console.WriteLine("Starting Processor Scheduler");
+          mDebugger.Send("Processor Scheduler");
+          Core.Processing.ProcessorScheduler.Initialize();
 
-      mDebugger.Send("Done initializing Cosmos.HAL.Global");
+          mDebugger.Send("Done initializing Cosmos.HAL.Global");
 
-            Console.WriteLine("Starting ACPI");
-            mDebugger.Send("ACPI Init");
-            ACPI.Start();
+          Console.WriteLine("Starting ACPI");
+          mDebugger.Send("ACPI Init");
+          ACPI.Start();
 
-            // http://wiki.osdev.org/%228042%22_PS/2_Controller#Initialising_the_PS.2F2_Controller
-            // TODO: USB should be initialized before the PS/2 controller
-            // TODO: ACPI should be used to check if a PS/2 controller exists
-            mDebugger.Send("PS/2 Controller Init");
-            PS2Controller.Initialize();
+          // http://wiki.osdev.org/%228042%22_PS/2_Controller#Initialising_the_PS.2F2_Controller
+          // TODO: USB should be initialized before the PS/2 controller
+          // TODO: ACPI should be used to check if a PS/2 controller exists
+          mDebugger.Send("PS/2 Controller Init");
+          PS2Controller.Initialize();
 
-            IDE.InitDriver();
-            AHCI.InitDriver();
-            //EHCI.InitDriver();
+          IDE.InitDriver();
+          AHCI.InitDriver();
+          //EHCI.InitDriver();
 
-    public static void EnableInterrupts()
-    {
-      CPU.EnableInterrupts();
-    }
+        }
 
-    public static bool InterruptsEnabled => CPU.mInterruptsEnabled;
-
-    public static uint SpawnThread(ThreadStart aStart)
-    {
-      return Core.Processing.ProcessContext.StartContext("", aStart, Core.Processing.ProcessContext.Context_Type.THREAD);
-    }
-
-
-    public static uint SpawnThread(ParameterizedThreadStart aStart, object param)
-    {
-      return Core.Processing.ProcessContext.StartContext("", aStart, Core.Processing.ProcessContext.Context_Type.THREAD, param);
+        public static void EnableInterrupts()
+        {
+          CPU.EnableInterrupts();
+        }
 
         public static bool InterruptsEnabled => CPU.mInterruptsEnabled;
+
+        public static uint SpawnThread(ThreadStart aStart)
+        {
+          return Core.Processing.ProcessContext.StartContext("", aStart, Core.Processing.ProcessContext.Context_Type.THREAD);
+        }
+
+        public static uint SpawnThread(ParameterizedThreadStart aStart, object param)
+        {
+          return Core.Processing.ProcessContext.StartContext("", aStart, Core.Processing.ProcessContext.Context_Type.THREAD, param);
+        }
 
         public static IEnumerable<KeyboardBase> GetKeyboardDevices()
         {
@@ -133,5 +124,4 @@ namespace Cosmos.HAL
             return xMouseDevices;
         }
     }
-  }
 }
