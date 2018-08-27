abd73fd75e01739ab1cb1f0754f3c0f7f7e170f7
fanoI <fanoIlPrimo@gmail.com>
2018-08-27 12:52:25 +0200

    Finally HashTable works with ValueTypes!

--- Tests/Kernels/Cosmos.Compiler.Tests.Bcl/System/Collections/HashtableTest.cs
+++ Tests/Kernels/Cosmos.Compiler.Tests.Bcl/System/Collections/HashtableTest.cs
@@ -36,14 +36,68 @@ namespace Cosmos.Compiler.Tests.Bcl.System.Collections
                 Assert.IsTrue((string)k == "One" || (string)k == "Two", "Hashtable key collection returns invalid keys");
             }
 
-            /* This continues to not work: 42 is not a key! Why? */ 
-#if false
+            foreach (DictionaryEntry k in h)
+            {
+                Assert.IsTrue((string)k.Key == "One" || (string)k.Key == "Two", "h enumeration returns invalid keys key");
+                Assert.IsTrue((string)k.Value == "One" || (string)k.Value == "Two", "h enumeration returns invalid values");
+            }
+
             Hashtable h2 = new Hashtable();
 
             h2.Add(42, "FortyTwo");
 
-            Assert.IsTrue(h2.ContainsKey((int)42), "h2.ContainsKey() failed: existing key not found");
-#endif
+            Assert.IsTrue(h2.ContainsKey(42), "h2.ContainsKey() failed: existing key not found");
+            Assert.IsFalse(h2.ContainsKey(43), "h2.ContainsKey() failed: not existing key found");
+
+            Assert.IsTrue((string)h2[42] == "FortyTwo", "h2 indexer failed: existing value not found");
+            Assert.IsTrue((string)h2[43] == null, "h2 indexer failed: not existing value found");
+
+            /* The indexer written in this way should be the same thing of Add("Two", "Two") */
+            h2[43] = "FortyThree";
+
+            Assert.IsTrue((string)h2[43] == "FortyThree", "h2 indexer failed: existing value (II) not found");
+
+            Assert.IsTrue(h2.Count == 2, "h2 Count failed: value != 2"); 
+
+            foreach (var k in h2.Keys)
+            {
+                Assert.IsTrue((int)k == 42 || (int)k == 43, "h2 key collection returns invalid keys");
+            }
+
+            foreach (DictionaryEntry k in h2)
+            {
+                Assert.IsTrue((int)k.Key == 42  || (int)k.Key == 43, "h2 enumeration returns invalid keys key");
+                Assert.IsTrue((string)k.Value == "FortyTwo" || (string)k.Value == "FortyThree", "h2 enumeration returns invalid values");
+            }
+
+            Hashtable h3 = new Hashtable();
+
+            h3.Add('A', 41);
+            Assert.IsTrue(h3.ContainsKey('A'), "h3.ContainsKey() failed: existing key not found");
+
+            Assert.IsTrue((int)h3['A'] == 41, "h3 indexer failed: existing value not found");
+
+            Assert.IsTrue(h3['B'] == null, "h3 indexer failed: not existing value found");
+
+            /* The indexer written in this way should be the same thing of Add("Two", "Two") */
+            h3['B'] = 42;
+
+            Assert.IsTrue((int)h3['B'] == 42, "h3 indexer failed: existing value (II) not found");
+
+            Assert.IsTrue(h3.Count == 2, "h3 Count failed: value != 2");
+
+            foreach (var k in h3.Keys)
+            {
+                Assert.IsTrue((char)k == 'A' || (char)k == 'B', "h3 key collection returns invalid keys");
+            }
+
+            foreach (DictionaryEntry k in h3)
+            {
+                Assert.IsTrue((char)k.Key == 'A' || (char)k.Key == 'B', "h3 enumeration returns invalid keys");
+                Assert.IsTrue((int)k.Value == 41 || (int)k.Value == 42, "h3 enumeration returns invalid values");
+            }
+
+            
         }
     }
 }
