470d47b6c9186d45b25622322e394d15c5256f1d
José Pedro <jpedrolmail@gmail.com>
2018-10-02 22:07:00 +0100

    Code cleanup.

--- source/Cosmos.System2/Power.cs
+++ source/Cosmos.System2/Power.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cosmos.System
+﻿namespace Cosmos.System
 {
-    public class Power
+    public static class Power
     {
         public static void Reboot()
         {
