aacdba363f63b728416849bb00bee6908901e463
valentinbreiz <valentinbreiz@gmail.com>
2018-11-13 14:35:42 +0100

    Update Global.cs

--- source/Cosmos.HAL2/Global.cs
+++ source/Cosmos.HAL2/Global.cs
@@ -1,78 +1,72 @@
 using System;
-using System.Threading;
 using System.Collections.Generic;
+
 using Cosmos.Core;
 using Cosmos.Debug.Kernel;
 using Cosmos.HAL.BlockDevice;
 
 namespace Cosmos.HAL
-  {
+{
     public static class Global
     {
         public static readonly Debugger mDebugger = new Debugger("HAL", "Global");
 
-        static public PIT PIT = new PIT();
+        public static PIT PIT = new PIT();
         // Must be static init, other static inits rely on it not being null
 
         public static TextScreenBase TextScreen = new TextScreen();
         public static PCI Pci;
 
-
-        //TODO Redo this - Global init should be other.
-        // Move PCI detection to hardware? Or leave it in core? Is Core PC specific, or deeper?
-        // If we let hardware do it, we need to protect it from being used by System.
-        // Probably belongs in hardware, and core is more specific stuff like CPU, memory, etc.
-        //Core.PCI.OnPCIDeviceFound = PCIDeviceFound;
-
-        //TODO: Since this is FCL, its "common". Otherwise it should be
-        // system level and not accessible from Core. Need to think about this
-        // for the future.
-
         public static readonly PS2Controller PS2Controller = new PS2Controller();
 
         static public void Init(TextScreenBase textScreen)
         {
-          if (textScreen != null)
-          {
-              TextScreen = textScreen;
-          }
+            if (textScreen != null)
+            {
+                TextScreen = textScreen;
+            }
 
-          Console.WriteLine("Finding PCI Devices");
-          mDebugger.Send("PCI Devices");
-          PCI.Setup();
+            mDebugger.Send("Before Core.Global.Init");
+            Core.Global.Init();
 
-          Console.WriteLine("Starting ACPI");
-          mDebugger.Send("ACPI Init");
-          ACPI.Start();
-      
-          Console.WriteLine("Finding ATA Devices");
-          mDebugger.Send("ATA Devices");
-          IDE.InitDriver();
-          AHCI.InitDriver();
-          //EHCI.InitDriver();
+            //TODO Redo this - Global init should be other.
+            // Move PCI detection to hardware? Or leave it in core? Is Core PC specific, or deeper?
+            // If we let hardware do it, we need to protect it from being used by System.
+            // Probably belongs in hardware, and core is more specific stuff like CPU, memory, etc.
+            //Core.PCI.OnPCIDeviceFound = PCIDeviceFound;
 
-          mDebugger.Send("Done initializing Cosmos.HAL.Global");
+            //TODO: Since this is FCL, its "common". Otherwise it should be
+            // system level and not accessible from Core. Need to think about this
+            // for the future.
 
-          Console.WriteLine("Starting ACPI");
-          mDebugger.Send("ACPI Init");
-          ACPI.Start();
+            Console.WriteLine("Finding PCI Devices");
+            mDebugger.Send("PCI Devices");
+            PCI.Setup();
 
-          // http://wiki.osdev.org/%228042%22_PS/2_Controller#Initialising_the_PS.2F2_Controller
-          // TODO: USB should be initialized before the PS/2 controller
-          // TODO: ACPI should be used to check if a PS/2 controller exists
-          mDebugger.Send("PS/2 Controller Init");
-          PS2Controller.Initialize();
+            Console.WriteLine("Starting ACPI");
+            mDebugger.Send("ACPI Init");
+            ACPI.Start();
 
-          IDE.InitDriver();
-          AHCI.InitDriver();
-          //EHCI.InitDriver();
+            // http://wiki.osdev.org/%228042%22_PS/2_Controller#Initialising_the_PS.2F2_Controller
+            // TODO: USB should be initialized before the PS/2 controller
+            // TODO: ACPI should be used to check if a PS/2 controller exists
+            mDebugger.Send("PS/2 Controller Init");
+            PS2Controller.Initialize();
+
+            IDE.InitDriver();
+            AHCI.InitDriver();
+            //EHCI.InitDriver();
+
+            mDebugger.Send("Done initializing Cosmos.HAL.Global");
 
         }
 
         public static void EnableInterrupts()
         {
-          CPU.EnableInterrupts();
+            CPU.EnableInterrupts();
         }
+
+        public static bool InterruptsEnabled => CPU.mInterruptsEnabled;
 
         public static IEnumerable<KeyboardBase> GetKeyboardDevices()
         {
