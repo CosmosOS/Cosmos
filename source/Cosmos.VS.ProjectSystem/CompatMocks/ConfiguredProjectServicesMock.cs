5cf4dbd990b0efaadf964ee645fe14d1dd6366df
José Pedro <jpedrolmail@gmail.com>
2018-12-06 16:55:11 +0000

    Project system fixes for VS 2019.

---
+++ source/Cosmos.VS.ProjectSystem/CompatMocks/ConfiguredProjectServicesMock.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Composition;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+namespace Cosmos.VS.ProjectSystem.CompatMocks
+{
+    [Export]
+    internal sealed class ConfiguredProjectServicesMock
+    {
+        [ImportingConstructor]
+        public ConfiguredProjectServicesMock(
+            IAdditionalRuleDefinitionsService additionalRuleDefinitions,
+            IPropertyPagesCatalogProvider propertyPagesCatalog)
+        {
+            AdditionalRuleDefinitions = additionalRuleDefinitions;
+            PropertyPagesCatalog = propertyPagesCatalog;
+        }
+
+        public IAdditionalRuleDefinitionsService AdditionalRuleDefinitions { get; }
+
+        public IPropertyPagesCatalogProvider PropertyPagesCatalog { get; }
+    }
+}
--- source/Cosmos.VS.ProjectSystem/ProjectProperties.cs
+++ source/Cosmos.VS.ProjectSystem/ProjectProperties.cs
@@ -1,19 +1,25 @@
 ﻿using System.Composition;
+using Microsoft.VisualStudio.Composition;
 using Microsoft.VisualStudio.ProjectSystem;
 using Microsoft.VisualStudio.ProjectSystem.Properties;
+
+using Cosmos.VS.ProjectSystem.CompatMocks;
 
 namespace Cosmos.VS.ProjectSystem
 {
     [Export]
     internal partial class ProjectProperties : StronglyTypedPropertyAccess
     {
+        public new ConfiguredProjectMock ConfiguredProject { get; }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ProjectProperties"/> class.
         /// </summary>
         [ImportingConstructor]
-        public ProjectProperties(ConfiguredProject configuredProject)
+        public ProjectProperties(ConfiguredProject configuredProject, ConfiguredProjectMock configuredProjectMock)
             : base(configuredProject)
         {
+            ConfiguredProject = configuredProjectMock;
         }
 
         /// <summary>
