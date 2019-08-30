73c29ac36091855c94929de1756e87a9ed815241
Charles Betros <charles.betros@gmail.com>
2019-08-30 07:09:23 -0500

    Update CPUReadCPUIDAsm.cs

--- source/Cosmos.Core_Asm/CPU/CPUReadCPUIDAsm.cs
+++ source/Cosmos.Core_Asm/CPU/CPUReadCPUIDAsm.cs
@@ -1,18 +1,38 @@
+using Cosmos.Debug.Kernel;
+using XSharp;
 using XSharp.Assembler;
+using static XSharp.XSRegisters;
 
 namespace Cosmos.Core_Asm
 {
     public class CPUReadCPUIDAsm : AssemblerMethod
     {
+        private const int CpuIdTypeArgOffset = 4;
+        private const int CpuIdEAXAddressArgOffset = 8;
+        private const int CpuIdEBXAddressArgOffset = 12;
+        private const int CpuIdECXAddressArgOffset = 16;
+        private const int CpuIdEDXAddressArgOffset = 20;
         public override void AssembleNew(Assembler aAssembler, object aMethodInfo)
         {
-            // TODO Get the type parameter from EBP+8 and move it to EAX
-            /*
-             * mov eax, 16h
-             * cpuid
-             */
-            // TODO The result of cpuid will be in EDX:EAX so it will need to be moved to the return value
-            // Set ESI to EBP+8?
+            XS.Comment("Load type arg");
+            XS.Set(EAX, EBP, sourceDisplacement: CpuIdTypeArgOffset, sourceIsIndirect: true);
+            XS.Cpuid();
+
+            XS.Comment("Save eax result arg");
+            XS.Set(EDI, EBP, sourceDisplacement: CpuIdEAXAddressArgOffset, sourceIsIndirect: true);
+            XS.Set(EDI, EAX, destinationIsIndirect: true);
+
+            XS.Comment("Save ebx result arg");
+            XS.Set(EDI, EBP, sourceDisplacement: CpuIdEBXAddressArgOffset, sourceIsIndirect: true);
+            XS.Set(EDI, EBX, destinationIsIndirect: true);
+
+            XS.Comment("Save ecx result arg");
+            XS.Set(EDI, EBP, sourceDisplacement: CpuIdECXAddressArgOffset, sourceIsIndirect: true);
+            XS.Set(EDI, ECX, destinationIsIndirect: true);
+
+            XS.Comment("Save edx result arg");
+            XS.Set(EDI, EBP, sourceDisplacement: CpuIdEDXAddressArgOffset, sourceIsIndirect: true);
+            XS.Set(EDI, EDX, destinationIsIndirect: true);
         }
     }
 }
