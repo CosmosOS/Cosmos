85a387ec459c4422fd3fa34d8b33da514924f58d
Arawn Davies <arawn.davies@gmail.com>
2019-08-31 22:00:58 +0100

    Update VBEDriver.cs

--- source/Cosmos.HAL2/Drivers/Video/VBEDriver.cs
+++ source/Cosmos.HAL2/Drivers/Video/VBEDriver.cs
@@ -66,7 +66,7 @@ namespace Cosmos.HAL.Drivers
         }
         public bool Available()
         {
-            if (VBERead(VBERegisterIndex.VBEDisplayID) == 0xB0C5)
+            if (VBERead(RegisterIndex.VBEDisplayID) == 0xB0C5)
             {
                 return true;
             }
