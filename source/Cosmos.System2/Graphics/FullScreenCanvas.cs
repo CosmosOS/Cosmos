5164c0a240f7d43906da1ca62529a7b55338c043
Quajak <jasper.gerigk@gmx.net>
2020-05-18 21:09:42 +0200

    Typo

--- source/Cosmos.System2/Graphics/FullScreenCanvas.cs
+++ source/Cosmos.System2/Graphics/FullScreenCanvas.cs
@@ -13,7 +13,7 @@ namespace Cosmos.System.Graphics
             if (IsInUse)
             {
                 _VideoDriver.Disable();
-                VGADriver.SetTextMode(VGADriver.TextSize.Size80x25);
+                VGAScreen.SetTextMode(VGADriver.TextSize.Size80x25);
             }
         }
 
