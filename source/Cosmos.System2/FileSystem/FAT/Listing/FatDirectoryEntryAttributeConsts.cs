fb3a86a6bf7f1090708305737b04c61934bd66e6
Elia Sulimanov <eliasulimanov@gmail.com>
2020-06-02 22:37:02 +0300

    Added FatDirectoryEntryAttributeConsts api docs

--- source/Cosmos.System2/FileSystem/FAT/Listing/FatDirectoryEntryAttributeConsts.cs
+++ source/Cosmos.System2/FileSystem/FAT/Listing/FatDirectoryEntryAttributeConsts.cs
@@ -1,10 +1,22 @@
 namespace Cosmos.System.FileSystem.FAT.Listing
 {
+    /// <summary>
+    /// FAT file system directory entry file attributes.
+    /// </summary>
     public static class FatDirectoryEntryAttributeConsts
     {
         public const int Test = 0x01;
+        /// <summary>
+        /// Hidden. Hides directories.
+        /// </summary>
         public const int Hidden = 0x02;
+        /// <summary>
+        /// System. Indicates system directory.
+        /// </summary>
         public const int System = 0x04;
+        /// <summary>
+        /// Volume Label. Indicates optional root directory label.
+        /// </summary>
         public const int VolumeID = 0x08;
         public const int Directory = 0x10;
         public const int Archive = 0x20;
