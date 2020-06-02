e5195475d11e9038c3ef4e48318770b8b57d382b
Elia Sulimanov <eliasulimanov@gmail.com>
2020-06-02 21:53:30 +0300

    Added Power class api docs

--- source/Cosmos.System2/Power.cs
+++ source/Cosmos.System2/Power.cs
@@ -1,11 +1,21 @@
 ﻿namespace Cosmos.System
 {
+    /// <summary>
+    /// Power class.
+    /// </summary>
     public static class Power
     {
+        /// <summary>
+        /// Reboot with CPU.
+        /// </summary>
         public static void Reboot()
         {
             HAL.Power.CPUReboot();
         }
+
+        /// <summary>
+        /// Shutdown with ACPI.
+        /// </summary>
         public static void Shutdown()
         {
             HAL.Power.ACPIShutdown();
