2d64da1146c81bdc49722f735b42d0ff84b63ed5
Elia Sulimanov <eliasulimanov@gmail.com>
2020-06-05 21:08:48 +0300

    Added FatDirectoryEntryAttributeConsts api docs

--- source/Cosmos.System2/FileSystem/FAT/Listing/FatDirectoryEntryAttributeConsts.cs
+++ source/Cosmos.System2/FileSystem/FAT/Listing/FatDirectoryEntryAttributeConsts.cs
@@ -5,9 +5,12 @@ namespace Cosmos.System.FileSystem.FAT.Listing
     /// </summary>
     public static class FatDirectoryEntryAttributeConsts
     {
+        /// <summary>
+        /// Read only.
+        /// </summary>
         public const int Test = 0x01;
         /// <summary>
-        /// Hidden. Hides directories.
+        /// Hidden. Hidden directory.
         /// </summary>
         public const int Hidden = 0x02;
         /// <summary>
@@ -18,8 +21,17 @@ namespace Cosmos.System.FileSystem.FAT.Listing
         /// Volume Label. Indicates optional root directory label.
         /// </summary>
         public const int VolumeID = 0x08;
+        /// <summary>
+        /// Directory. Indicates the block is sub-directory.
+        /// </summary>
         public const int Directory = 0x10;
+        /// <summary>
+        /// Archive.
+        /// </summary>
         public const int Archive = 0x20;
+        /// <summary>
+        /// Unused or deleted directory.
+        /// </summary>
         public const int UnusedOrDeletedEntry = 0xE5;
         // LongName was created after and is a combination of other attribs. Its "special".
         public const int LongName = 0x0F;
