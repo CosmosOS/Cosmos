0a5cc370a07f3579e3755222949fced5f8b63609
Elia Sulimanov <eliasulimanov@gmail.com>
2020-06-10 22:02:02 +0300

    Done FatFileSystemFactory api docs

--- source/Cosmos.System2/FileSystem/FatFileSystemFactory.cs
+++ source/Cosmos.System2/FileSystem/FatFileSystemFactory.cs
@@ -38,7 +38,27 @@ namespace Cosmos.System.FileSystem
         /// </summary>
         /// <param name="aDevice">The partition.</param>
         /// <param name="aRootPath">The root path.</param>
-        /// <exception cref="Exception">FAT signature not found.</exception>
+        /// <exception cref="ArgumentNullException">
+        /// <list type="bullet">
+        /// <item>Thrown when aDevice is null.</item>
+        /// <item>Thrown when FatFileSystem is null.</item>
+        /// <item>Thrown on fatal error (contact support).</item>
+        /// </list>
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <list type="bullet">
+        /// <item>Thrown when aRootPath is null.</item>
+        /// <item>Thrown on fatal error (contact support).</item>
+        /// </list>
+        /// </exception>
+        /// <exception cref="OverflowException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="Exception">
+        /// <list type="bullet">
+        /// <item>Thrown on fatal error (contact support).</item>
+        /// <item>>FAT signature not found.</item>
+        /// </list>
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
         public override FileSystem Create(Partition aDevice, string aRootPath, long aSize) => new FatFileSystem(aDevice, aRootPath, aSize);
     }
 }
