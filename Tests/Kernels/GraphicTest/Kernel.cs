9435168e13db7cd20cbeaa9e68dcfe008f3bd092
KM198912 <55886806+KM198912@users.noreply.github.com>
2020-10-20 16:46:05 +0200

    Update Kernel.cs
    Forgot that

--- Tests/Kernels/GraphicTest/Kernel.cs
+++ Tests/Kernels/GraphicTest/Kernel.cs
@@ -118,7 +118,7 @@ namespace GraphicTest
             aCanvas.DrawImage(bitmap, new Point(0, 0));
             aCanvas.DrawImage(bitmap2, new Point(200, 0));
             //Scale Bitmap
-            aCanvas.DrawScaledImage(bitmap,0,0,50,50);
+            aCanvas.DrawImage(bitmap,0,0,50,50);
             
             aCanvas.DrawImageAlpha(bitmap3, new Point(0, 300));
 
