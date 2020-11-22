5397e1a0ca997842837af6a9a67c56bd0de7b935
Quajak <jasper.gerigk@gmx.net>
2020-11-22 14:42:10 +0100

    Updated to .net50

--- source/Cosmos.Core_Plugs/System/Runtime/RuntimeImportsImpl.cs
+++ source/Cosmos.Core_Plugs/System/Runtime/RuntimeImportsImpl.cs
@@ -2,22 +2,23 @@
 
 namespace Cosmos.Core_Plugs.System.Runtime
 {
-    [Plug("System.Runtime.RuntimeImports, System.Private.CoreLib")]
-    public static class RuntimeImportsImpl
-    {
-        public static unsafe void RhBulkMoveWithWriteBarrier(
-            ref byte destination, ref byte source, uint byteCount)
-        {
-            fixed (byte* srcPtr = &source)
-            fixed (byte* dstPtr = &destination)
-            {
-                for (int i = 0; i < byteCount; i++)
-                {
-                    dstPtr[i] = srcPtr[i];
-                }
-            }
+    // Not needed in .net5.0
+    //[Plug("System.Runtime.RuntimeImports, System.Private.CoreLib")]
+    //public static class RuntimeImportsImpl
+    //{
+    //    public static unsafe void RhBulkMoveWithWriteBarrier(
+    //        ref byte destination, ref byte source, uint byteCount)
+    //    {
+    //        fixed (byte* srcPtr = &source)
+    //        fixed (byte* dstPtr = &destination)
+    //        {
+    //            for (int i = 0; i < byteCount; i++)
+    //            {
+    //                dstPtr[i] = srcPtr[i];
+    //            }
+    //        }
             
-            // Unsafe.CopyBlock(ref destination, ref source, byteCount);
-        }
-    }
+    //        // Unsafe.CopyBlock(ref destination, ref source, byteCount);
+    //    }
+    //}
 }
