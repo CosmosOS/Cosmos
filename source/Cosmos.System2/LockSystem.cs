64b2d3bd41b7ee2975b3f61262d53966558fa60e
F4lc0131 <nicolaflavio22@gmail.com>
2021-01-05 13:53:44 +0100

    Add files via upload

---
+++ source/Cosmos.System2/LockSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.System
+{
+    public class LockSystem
+    {
+        public static Dictionary<string, Lock> definedLocks = new Dictionary<string, Lock>();
+        public class Lock
+        {
+            internal Lock() { }
+            public Core.Processing.ProcessContext.Context LockContext = null;
+            public List<Core.Processing.ProcessContext.Context> queue = new List<Core.Processing.ProcessContext.Context>();
+        }
+        public static Lock DefineLock(string LockName)
+        {
+            definedLocks.Add(LockName, new Lock());
+            return definedLocks[LockName];
+        }
+    }
+}
