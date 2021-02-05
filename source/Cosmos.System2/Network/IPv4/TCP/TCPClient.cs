3e732dbaa3a6dc2984625d643817b776efbe73d4
valentinbreiz <valentinbreiz@gmail.com>
2021-02-05 13:46:28 +0100

    Add TCP Client + basic TCP packet

---
+++ source/Cosmos.System2/Network/IPv4/TCP/TCPClient.cs
@@ -0,0 +1,203 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          TCP Client
+* PROGRAMMERS:      Valentin Charbonnier <valentinbreiz@gmail.com>
+*                   Port of Cosmos Code.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.System.Network.IPv4.TCP
+{
+    /// <summary>
+    /// TCPClient class. Used to manage the TCP connection to a client.
+    /// </summary>
+    public class TcpClient : IDisposable
+    {
+        /// <summary>
+        /// Clients dictionary.
+        /// </summary>
+        private static Dictionary<uint, TcpClient> clients;
+
+        /// <summary>
+        /// Local port.
+        /// </summary>
+        private int localPort;
+        /// <summary>
+        /// Destination address.
+        /// </summary>
+        internal Address destination;
+        /// <summary>
+        /// Destination port.
+        /// </summary>
+        private int destinationPort;
+
+        /// <summary>
+        /// RX buffer queue.
+        /// </summary>
+        internal Queue<TCPPacket> rxBuffer;
+
+        /// <summary>
+        /// Assign clients dictionary.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        static TcpClient()
+        {
+            clients = new Dictionary<uint, TcpClient>();
+        }
+
+        /// <summary>
+        /// Get client.
+        /// </summary>
+        /// <param name="destPort">Destination port.</param>
+        /// <returns>TcpClient</returns>
+        internal static TcpClient GetClient(ushort destPort)
+        {
+            if (clients.ContainsKey((uint)destPort) == true)
+            {
+                return clients[(uint)destPort];
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Create new instance of the <see cref="TcpClient"/> class.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="ArgumentException">Thrown if TcpClient with localPort 0 exists.</exception>
+        public TcpClient()
+            : this(0)
+        { }
+
+        /// <summary>
+        /// Create new instance of the <see cref="TcpClient"/> class.
+        /// </summary>
+        /// <param name="localPort">Local port.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="ArgumentException">Thrown if localPort already exists.</exception>
+        public TcpClient(int localPort)
+        {
+            rxBuffer = new Queue<TCPPacket>(8);
+
+            this.localPort = localPort;
+            if (localPort > 0)
+            {
+                clients.Add((uint)localPort, this);
+            }
+        }
+
+        /// <summary>
+        /// Create new instance of the <see cref="TcpClient"/> class.
+        /// </summary>
+        /// <param name="dest">Destination address.</param>
+        /// <param name="destPort">Destination port.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="ArgumentException">Thrown if TcpClient with localPort 0 exists.</exception>
+        public TcpClient(Address dest, int destPort)
+            : this(0)
+        {
+            destination = dest;
+            destinationPort = destPort;
+        }
+
+        /// <summary>
+        /// Connect to client.
+        /// </summary>
+        /// <param name="dest">Destination address.</param>
+        /// <param name="destPort">Destination port.</param>
+        public void Connect(Address dest, int destPort)
+        {
+            destination = dest;
+            destinationPort = destPort;
+        }
+
+        /// <summary>
+        /// Close connection.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        public void Close()
+        {
+            if (clients.ContainsKey((uint)localPort) == true)
+            {
+                clients.Remove((uint)localPort);
+            }
+        }
+
+        /// <summary>
+        /// Send data to client.
+        /// </summary>
+        /// <param name="data">Data array to send.</param>
+        /// <exception cref="Exception">Thrown if destination is null or destinationPort is 0.</exception>
+        /// <exception cref="ArgumentException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="OverflowException">Thrown if data array length is greater than Int32.MaxValue.</exception>
+        /// <exception cref="Sys.IO.IOException">Thrown on IO error.</exception>
+        public void Send(byte[] data)
+        {
+            if ((destination == null) || (destinationPort == 0))
+            {
+                throw new InvalidOperationException("Must establish a default remote host by calling Connect() before using this Send() overload");
+            }
+
+            Send(data, destination, destinationPort);
+            NetworkStack.Update();
+        }
+
+        /// <summary>
+        /// Send data.
+        /// </summary>
+        /// <param name="data">Data array.</param>
+        /// <param name="dest">Destination address.</param>
+        /// <param name="destPort">Destination port.</param>
+        /// <exception cref="ArgumentException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="OverflowException">Thrown if data array length is greater than Int32.MaxValue.</exception>
+        /// <exception cref="Sys.IO.IOException">Thrown on IO error.</exception>
+        public void Send(byte[] data, Address dest, int destPort)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Receive data from end point.
+        /// </summary>
+        /// <param name="source">Source end point.</param>
+        /// <returns>byte array value.</returns>
+        /// <exception cref="InvalidOperationException">Thrown on fatal error (contact support).</exception>
+        public byte[] NonBlockingReceive(ref EndPoint source)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Receive data from end point.
+        /// </summary>
+        /// <param name="source">Source end point.</param>
+        /// <returns>byte array value.</returns>
+        /// <exception cref="InvalidOperationException">Thrown on fatal error (contact support).</exception>
+        public byte[] Receive(ref EndPoint source)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Receive data from packet.
+        /// </summary>
+        /// <param name="packet">Packet to receive.</param>
+        /// <exception cref="OverflowException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="Sys.IO.IOException">Thrown on IO error.</exception>
+        internal void ReceiveData(TCPPacket packet)
+        {
+            rxBuffer.Enqueue(packet);
+        }
+
+        /// <summary>
+        /// Close Client
+        /// </summary>
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+}
---
+++ source/Cosmos.System2/Network/IPv4/TCP/TCPPacket.cs
@@ -0,0 +1,241 @@
+﻿/*
+* PROJECT:          Aura Operating System Development
+* CONTENT:          TCP Packet
+* PROGRAMMERS:      Valentin Charbonnier <valentinbreiz@gmail.com>
+*                   Port of Cosmos Code.
+*/
+
+using Cosmos.HAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.System.Network.IPv4.TCP
+{
+
+    /// <summary>
+    /// DHCP Option
+    /// </summary>
+    public class TCPOption
+    {
+        public byte Kind { get; set; }
+        public byte Length { get; set; }
+        public byte[] Data { get; set; }
+    }
+
+    public class TCPPacket : IPPacket
+    {
+        protected ushort sourcePort;
+        protected ushort destinationPort;
+        protected ushort tcpLen;
+        protected ulong sequenceNumber;
+        protected ulong ackNumber;
+        protected int headerLenght;
+        protected int flags;
+        protected int wsValue;
+        protected int checksum;
+        protected int urgentPointer;
+
+        protected List<TCPOption> options = null;
+
+        public bool SYN;
+        public bool ACK;
+        public bool FIN;
+        public bool PSH;
+        public bool RST;
+
+        protected ushort optionLen;
+
+        internal static void TCPHandler(byte[] packetData)
+        {
+            var packet = new TCPPacket(packetData);
+
+            Global.mDebugger.Send("[Received] TCP packet from " + packet.SourceIP.ToString() + ":" + packet.SourcePort.ToString());
+
+            var receiver = TcpClient.GetClient(packet.DestinationPort);
+            if (receiver != null)
+            {
+                receiver.ReceiveData(packet);
+            }
+        }
+
+        /// <summary>
+        /// Work around to make VMT scanner include the initFields method
+        /// </summary>
+        public static void VMTInclude()
+        {
+            new TCPPacket();
+        }
+
+        internal TCPPacket()
+            : base()
+        { }
+
+        internal TCPPacket(byte[] rawData)
+            : base(rawData)
+        { }
+
+        public TCPPacket(Address source, Address dest, ushort srcPort, ushort destPort,
+            ulong sequencenumber, ulong acknowledgmentnb, ushort Headerlenght, ushort Flags,
+            ushort WSValue, ushort UrgentPointer, ushort len)
+            : base((ushort)(20 + len), 6, source, dest, 0x40)
+        {
+            optionLen = len;
+
+            //ports
+            RawData[DataOffset + 0] = (byte)((sourcePort >> 8) & 0xFF);
+            RawData[DataOffset + 1] = (byte)((srcPort >> 0) & 0xFF);
+
+            RawData[DataOffset + 2] = (byte)((destPort >> 8) & 0xFF);
+            RawData[DataOffset + 3] = (byte)((destPort >> 0) & 0xFF);
+
+            //sequencenumber
+            RawData[DataOffset + 4] = (byte)((sequencenumber >> 24) & 0xFF);
+            RawData[DataOffset + 5] = (byte)((sequencenumber >> 16) & 0xFF);
+            RawData[DataOffset + 6] = (byte)((sequencenumber >> 8) & 0xFF);
+            RawData[DataOffset + 7] = (byte)((sequencenumber >> 0) & 0xFF);
+
+            //Acknowledgment number
+            RawData[DataOffset + 8] = (byte)((acknowledgmentnb >> 24) & 0xFF);
+            RawData[DataOffset + 9] = (byte)((acknowledgmentnb >> 16) & 0xFF);
+            RawData[DataOffset + 10] = (byte)((acknowledgmentnb >> 8) & 0xFF);
+            RawData[DataOffset + 11] = (byte)((acknowledgmentnb >> 0) & 0xFF);
+
+            //Header lenght
+            RawData[DataOffset + 12] = (byte)((Headerlenght >> 0) & 0xFF);
+
+            //Flags
+            RawData[DataOffset + 13] = (byte)((Flags >> 0) & 0xFF);
+
+            //Window size value
+            RawData[DataOffset + 14] = (byte)((WSValue >> 8) & 0xFF);
+            RawData[DataOffset + 15] = (byte)((WSValue >> 0) & 0xFF);
+
+            //Checksum
+            RawData[DataOffset + 16] = 0x00;
+            RawData[DataOffset + 17] = 0x00;
+
+            //Urgent Pointer
+            RawData[DataOffset + 18] = (byte)((UrgentPointer >> 8) & 0xFF);
+            RawData[DataOffset + 19] = (byte)((UrgentPointer >> 0) & 0xFF);
+
+            InitFields();
+        }
+
+        protected override void InitFields()
+        {
+            base.InitFields();
+            sourcePort = (ushort)((RawData[DataOffset] << 8) | RawData[DataOffset + 1]);
+            destinationPort = (ushort)((RawData[DataOffset + 2] << 8) | RawData[DataOffset + 3]);
+            sequenceNumber = (uint)((RawData[DataOffset + 4] << 24) | (RawData[DataOffset + 5] << 16) | (RawData[DataOffset + 6] << 8) | RawData[DataOffset + 7]);
+            ackNumber = (uint)((RawData[DataOffset + 8] << 24) | (RawData[DataOffset + 9] << 16) | (RawData[DataOffset + 10] << 8) | RawData[DataOffset + 11]);
+            headerLenght = RawData[DataOffset + 12];
+            flags = RawData[DataOffset + 13];
+            wsValue = (ushort)((RawData[DataOffset + 14] << 8) | RawData[DataOffset + 15]);
+            checksum = (ushort)((RawData[DataOffset + 16] << 8) | RawData[DataOffset + 17]);
+            urgentPointer = (ushort)((RawData[DataOffset + 18] << 8) | RawData[DataOffset + 19]);
+
+            SYN = (RawData[47] & (1 << 1)) != 0;
+            ACK = (RawData[47] & (1 << 4)) != 0;
+            FIN = (RawData[47] & (1 << 0)) != 0;
+            PSH = (RawData[47] & (1 << 3)) != 0;
+            RST = (RawData[47] & (1 << 2)) != 0;
+
+            //options
+            if (RawData[DataOffset + 20] != 0)
+            {
+                options = new List<TCPOption>();
+
+                for (int i = 0; i < RawData.Length - (DataOffset + 20); i++)
+                {
+                    var option = new TCPOption();
+                    option.Kind = RawData[DataOffset + 20 + i];
+
+                    if (option.Kind != 1) //NOP
+                    {
+                        option.Length = RawData[DataOffset + 20 + i + 1];
+
+                        if (option.Length != 2)
+                        {
+                            option.Data = new byte[option.Length - 2];
+                            for (int j = 0; j < option.Length - 2; j++)
+                            {
+                                option.Data[j] = RawData[DataOffset + 20 + i + 2 + j];
+                            }
+                        }
+
+                        options.Add(option);
+
+                        i += option.Length - 1;
+                    }
+                }
+            }
+        }
+
+        internal ushort DestinationPort
+        {
+            get { return destinationPort; }
+        }
+        internal ushort SourcePort
+        {
+            get { return sourcePort; }
+        }
+        internal ulong SequenceNumber
+        {
+            get { return sequenceNumber; }
+        }
+
+        public override string ToString()
+        {
+            return "TCP Packet Src=" + SourceIP + ":" + sourcePort + ", Dest=" + DestinationIP + ":" + destinationPort;
+        }
+    }
+
+    public class TCPacketSyn : TCPPacket
+    {
+        /// <summary>
+        /// Work around to make VMT scanner include the initFields method
+        /// </summary>
+        public static void VMTInclude()
+        {
+            new TCPacketSyn();
+        }
+
+        internal TCPacketSyn()
+            : base()
+        { }
+
+        public TCPacketSyn(Address Source, Address Destination, ushort SourcePort,
+            ushort DestinationPort, ulong SequenceNumber, ulong ACKNumber,
+            ushort Flags, ushort WSValue, List<TCPOption> options, ushort optionslen)
+            : base(Source, Destination, SourcePort, DestinationPort, SequenceNumber,
+                  ACKNumber, 0x50, Flags, WSValue, 0x0000, optionslen)
+        {
+            int counter = 0;
+            foreach (var option in options)
+            {
+                RawData[DataOffset + 20 + counter] = option.Kind;
+
+                if (option.Kind != 1) //NOP
+                {
+                    RawData[DataOffset + 20 + counter + 1] = option.Length;
+
+                    if (option.Length != 2)
+                    {
+                        for (int j = 0; j < option.Length - 2; j++)
+                        {
+                            RawData[DataOffset + 20 + counter + 2 + j] = option.Data[j];
+                        }
+                    }
+                }
+
+                counter += option.Length;
+            }
+        }
+
+        protected override void InitFields()
+        {
+            base.InitFields();
+        }
+    }
+}
