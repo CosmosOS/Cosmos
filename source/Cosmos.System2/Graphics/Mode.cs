f0dae2ec35934d1fb09aee30020166ab198709bf
napalmtorch <smodgehero@gmail.com>
2021-04-19 01:28:29 +0200

    Upload files to 'source/Cosmos.System2/Graphics'

---
+++ source/Cosmos.System2/Graphics/Mode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Cosmos.System.Graphics
+{
+    public struct Mode
+    {
+        public int Width;
+        public int Height;
+        public ColorDepth Depth;
+
+        // constructor
+        public Mode(int width, int height, ColorDepth depth)
+        {
+            Width = width;
+            Height = height;
+            Depth = depth;
+        }
+    }
+}
