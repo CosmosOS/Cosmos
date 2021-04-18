ef841963d93da33ce392d58d916e1e6389a93436
napalmtorch <smodgehero@gmail.com>
2021-04-19 01:28:48 +0200

    Upload files to 'source/Cosmos.System2/Graphics'

---
+++ source/Cosmos.System2/Graphics/Image.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos.System.Graphics
+{
+    public enum ImageFormat
+    {
+        BMP,
+        PNG,
+        JPEG,
+    }
+
+    public abstract class Image
+    {
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+        public ColorDepth Depth { get; protected set; }
+        public uint[] RawData { get; protected set; }
+
+        // constructor
+        public Image(int width, int height, ColorDepth depth)
+        {
+            Width = width;
+            Height = height;
+            Depth = depth;
+        }
+    }
+}
