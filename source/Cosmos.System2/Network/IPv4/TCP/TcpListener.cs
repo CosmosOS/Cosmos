6207fbbce7c76737f273f280cf27c168f1f3671a
valentinbreiz <valentinbreiz@gmail.com>
2021-05-27 16:25:23 +0200

    Add Dispose to TcpListener class + Documentation comments

--- source/Cosmos.System2/Network/IPv4/TCP/TcpListener.cs
+++ source/Cosmos.System2/Network/IPv4/TCP/TcpListener.cs
@@ -8,7 +8,7 @@ namespace Cosmos.System.Network.IPv4.TCP
     /// <summary>
     /// TcpListener class. Used to manage the TCP connection to a client.
     /// </summary>
-    public class TcpListener
+    public class TcpListener : IDisposable
     {
         /// <summary>
         /// Tcp State machine.
@@ -44,6 +44,12 @@ namespace Cosmos.System.Network.IPv4.TCP
             return null;
         }
 
+        /// <summary>
+        /// Create new instance of the <see cref="TcpListener"/> class.
+        /// </summary>
+        /// <param name="localPort">Local port.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="ArgumentException">Thrown if localPort already exists.</exception>
         public TcpListener(int localPort)
         {
             StateMachine = new Tcp();
@@ -56,6 +62,10 @@ namespace Cosmos.System.Network.IPv4.TCP
             }
         }
 
+        /// <summary>
+        /// Receive TcpClient from remote computer. This function is blocking.
+        /// </summary>
+        /// <returns>Accepted TcpClient</returns>
         public TcpClient AcceptTcpClient()
         {
             while (StateMachine.WaitStatus(Status.ESTABLISHED, 5000) != true);
@@ -63,14 +73,32 @@ namespace Cosmos.System.Network.IPv4.TCP
             return new TcpClient(StateMachine);
         }
 
+        /// <summary>
+        /// Start listening for new TCP connections.
+        /// </summary>
+        /// <exception cref="OverflowException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="Sys.IO.IOException">Thrown on IO error.</exception>
         public void Start()
         {
             StateMachine.Status = Status.LISTEN;
         }
 
+        /// <summary>
+        /// Stop listening for new TCP connections.
+        /// </summary>
+        /// <exception cref="OverflowException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="Sys.IO.IOException">Thrown on IO error.</exception>
         public void Stop()
         {
             StateMachine.Status = Status.CLOSED;
+        }
+
+        /// <summary>
+        /// Close listener
+        /// </summary>
+        public void Dispose()
+        {
+            Stop();
         }
     }
 }
