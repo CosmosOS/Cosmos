faacf6c4b03e49d3a8ace09c1aa907185558cdc6
KM198912 <k.meerts89@googlemail.com>
2021-11-06 14:57:22 +0100

    MAke OnBoot in user kernel optional

--- source/Cosmos.System2/Kernel.cs
+++ source/Cosmos.System2/Kernel.cs
@@ -130,9 +130,17 @@ namespace Cosmos.System
         }
 
         /// <summary>
-        /// Boot Events
+        /// This Method controls the Driver initialisation process and is intended for
+        /// Advanced users developing their drivers and takes 4 additional booleans.
+        /// 1. Mousewheel, if you experience your mouse cursors being stuck in the lower left corner set this to "false", default: true
+        /// 2. PS2 Driver initialisation, true/false , default: true
+        /// 3. Network Driver initialisation, true/false, default: true
+        /// 4. IDE initialisation, true/false, default: true
+        /// If you need anything else to be initialised really early on, place it here.
         /// </summary>
-        protected virtual void OnBoot() { }
+        protected virtual void OnBoot() {
+            Global.Init(GetTextScreen());
+        }
         /// <summary>
         /// Pre-run events
         /// </summary>
