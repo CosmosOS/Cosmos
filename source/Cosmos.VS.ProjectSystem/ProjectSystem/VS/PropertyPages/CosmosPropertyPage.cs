b9244888c3c9368144741031f655620c342af713
valentinbreiz <valentinbreiz@gmail.com>
2021-11-23 01:19:19 +0000

    Add add files to ISO link in Compile category + Update packages

---
+++ source/Cosmos.VS.ProjectSystem/ProjectSystem/VS/PropertyPages/CosmosPropertyPage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.VS.PropertyPages.Designer;
+
+namespace Cosmos.VS.ProjectSystem.ProjectSystem.VS.PropertyPages
+{
+    [Export(typeof(ILinkActionHandler))]
+    [ExportMetadata("CommandName", "AddFilesISO")]
+    internal sealed class MyCommandActionHandler : ILinkActionHandler
+    {
+        public Task HandleAsync(UnconfiguredProject project, IReadOnlyDictionary<string, string> editorMetadata)
+        {
+            var projectFolder = Path.GetDirectoryName(project.FullPath);
+            var isoFolder = Path.Combine(projectFolder, "isoFiles");
+            if (!Directory.Exists(isoFolder))
+            {
+                Directory.CreateDirectory(isoFolder);
+            }
+
+            Process.Start(isoFolder);
+
+            return Task.CompletedTask;
+        }
+    }
+}
