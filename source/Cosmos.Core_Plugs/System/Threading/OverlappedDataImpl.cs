8a60c5bdf5aa8dc09c71fb2cd44977a20277f205
Misha <46088515+MishaTY@users.noreply.github.com>
2021-12-29 12:26:45 -0500

    Add plug

---
+++ source/Cosmos.Core_Plugs/System/Threading/OverlappedDataImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using IL2CPU.API.Attribs;
+
+namespace Cosmos.Core_Plugs.System.Threading
+{
+    class OverlappedDataImpl
+    {
+        [PlugMethod(Signature = "System_Threading_OverlappedData__System_Threading_OverlappedData_GetOverlappedFromNative_System_Threading_NativeOverlapped__")]
+        public unsafe static void GetOverlappedFromNative(NativeOverlapped* a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
