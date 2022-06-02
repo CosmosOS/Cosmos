00e927219cfa0661ccfb1b3ac8bba5f7f450fe64
Diamantino-Op <Diamantino.op@gmail.com>
2022-06-02 23:03:06 +0200

    s

--- Tests/Kernels/Cosmos.Compiler.Tests.MethodTests/Kernel.cs
+++ Tests/Kernels/Cosmos.Compiler.Tests.MethodTests/Kernel.cs
@@ -12,44 +12,44 @@ namespace Cosmos.Compiler.Tests.MethodTests
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
         }
-        // public static void BranchStackCorruption2()
-        // {
-        //     char c = 'l';
-        //     char c2 = (c <= 'Z' && c >= 'A') ? ((char)(c - 65 + 97)) : c;
-        //     c = 'L';
-        //     char c3 = (c <= 'Z' && c >= 'A') ? ((char)(c - 65 + 97)) : c;
-        // }
+        public static void BranchStackCorruption2()
+        {
+             char c = 'l';
+             char c2 = (c <= 'Z' && c >= 'A') ? ((char)(c - 65 + 97)) : c;
+             c = 'L';
+             char c3 = (c <= 'Z' && c >= 'A') ? ((char)(c - 65 + 97)) : c;
+        }
 
-        // public static void BranchStackCorruption()
-        // {
-        //     bool flag1 = true;
-        //     bool flag2 = false;
-        //     bool flag3 = true;
+        public static void BranchStackCorruption()
+        {
+            bool flag1 = true;
+            bool flag2 = false;
+            bool flag3 = true;
 
-        //     if (flag1 && (flag2 || flag3))
-        //     {
-        //         return;
-        //     }
-        //     else
-        //     {
-        //         throw new Exception("This should not occur");
-        //     }
-        // }
+            if (flag1 && (flag2 || flag3))
+            {
+                return;
+            }
+            else
+            {
+                throw new Exception("This should not occur");
+            }
+        }
 
         protected override void Run()
         {
-            // BranchStackCorruption();
-            // BranchStackCorruption2();
-            // try
-            // {
-            //     ReturnTests.Execute();
-            //     TestController.Completed();
-            // }
-            // catch (Exception E)
-            // {
-            //     Console.WriteLine("Exception");
-            //     Console.WriteLine(E.ToString());
-            // }
+            BranchStackCorruption();
+            BranchStackCorruption2();
+            try
+            {
+                ReturnTests.Execute();
+                TestController.Completed();
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine("Exception");
+                Console.WriteLine(E.ToString());
+            }
             DelegatesTest.Execute();
         }
     }
