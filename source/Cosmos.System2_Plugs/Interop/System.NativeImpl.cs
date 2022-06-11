dc3ee458a73268f2941f72236800d1ffaf34956b
valentinbreiz <valentinbreiz@gmail.com>
2022-06-11 23:32:22 +0200

    Update plug

---
+++ source/Cosmos.System2_Plugs/Interop/System.NativeImpl.cs
@@ -0,0 +1,16 @@
+﻿using IL2CPU.API.Attribs;
+using System;
+using System.IO;
+
+namespace Cosmos.System_Plugs.Interop
+{
+    [Plug("Interop+Sys", IsOptional = true)]
+    class SysImpl
+    {
+        [PlugMethod(Signature = "System_Void__Interop_Sys_GetNonCryptographicallySecureRandomBytes_System_Byte___System_Int32_")]
+        public static unsafe void GetNonCryptographicallySecureRandomBytes(byte* buffer, int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
