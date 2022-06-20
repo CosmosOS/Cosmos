5fbe97e8f7133496ebadaabc818462febeb39001
valentinbreiz <valentinbreiz@gmail.com>
2022-06-20 20:08:06 +0200

    Add PIT code documentation

--- source/Cosmos.HAL2/PIT.cs
+++ source/Cosmos.HAL2/PIT.cs
@@ -12,19 +12,47 @@ namespace Cosmos.HAL
     /// </summary>
     public class PIT : Device
     {
+        /// <summary>
+        /// Pit IO Ports.
+        /// </summary>
         protected Core.IOGroup.PIT IO = Core.Global.BaseIOGroups.PIT;
 
+        /// <summary>
+        /// Global PIT Frequency.
+        /// </summary>
         public const uint PITFrequency = 1193182;
+
+        /// <summary>
+        /// Channel0 Frequency. 1ms.
+        /// </summary>
         public const uint Channel0Frequency = 1000;
+
+        /// <summary>
+        /// Divisor used to set channel0 frquency.
+        /// </summary>
         public ushort Channel0Divisor = (ushort)(PITFrequency / Channel0Frequency);
+
+        /// <summary>
+        /// Is PIT Sleeping.
+        /// </summary>
         bool Sleeping = false;
+
+        /// <summary>
+        /// Countdown in Ms.
+        /// </summary>
         uint CountDown = 0;
 
+        /// <summary>
+        /// PIT ctor, Set IRQ2 Handler
+        /// </summary>
         public PIT()
         {
             INTs.SetIrqHandler(2, HandleIRQ);
         }
 
+        /// <summary>
+        /// Enable PIT Interrupts
+        /// </summary>
         public void Enable()
         {
             IO.Command.Byte = 0x34; //Mode 4, lobyte/hibyte, channel 0
@@ -32,6 +60,9 @@ namespace Cosmos.HAL
             IO.Data0.Byte = (byte)(Channel0Divisor >> 8);
         }
 
+        /// <summary>
+        /// Disable PIT Interrupts
+        /// </summary>
         public void Disable()
         {
             IO.Command.Byte = 0b11000; //Mode 0, lobyte/hibyte, channel 0
@@ -40,6 +71,10 @@ namespace Cosmos.HAL
             IO.Data0.Byte = (byte)(div >> 8);
         }
 
+        /// <summary>
+        /// PIT Wait
+        /// </summary>
+        /// <param name="TimeoutMS">Timeout Value in Milliseconds.</param>
         public void Wait(uint TimeoutMS)
         {
             Enable();
@@ -59,6 +94,9 @@ namespace Cosmos.HAL
             Disable();
         }
 
+        /// <summary>
+        /// Handle PIT IRQ
+        /// </summary>
         private void HandleIRQ(ref INTs.IRQContext aContext)
         {
             if (Sleeping)
