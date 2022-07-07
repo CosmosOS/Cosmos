5cbb4ec1eed7311ac5bd4181ebb192317fcb5b00
Evan Lawrence <evanlawrence@cyberfear.com>
2022-07-06 23:01:45 -0700

    fix building

--- source/Cosmos.Debug.Hosts/Qemu.cs
+++ source/Cosmos.Debug.Hosts/Qemu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -301,3 +302,4 @@ namespace Cosmos.Debug.Hosts
     }
   }
 }
+*/
