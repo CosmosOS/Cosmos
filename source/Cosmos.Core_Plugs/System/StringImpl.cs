75f5245b2b288b4f110e2d601f95deea5ac7c233
Quajak <jasper.gerigk@gmx.net>
2022-07-21 21:57:22 +0200

    Plug String.GetNonRandomizedHashCode to support cosmos memory model for strings allocated on the heap with no extra space

--- source/Cosmos.Core_Plugs/System/StringImpl.cs
+++ source/Cosmos.Core_Plugs/System/StringImpl.cs
@@ -974,5 +974,35 @@ namespace Cosmos.Core_Plugs.System
                     throw new ArgumentException("Not Supported StringComparison");
             }
         }
+
+        public static unsafe int GetNonRandomizedHashCode(string aString)
+        {
+            // the code is the same as the one used in .net except for the explicit == 2 and == 1 cases
+            // we need this since a new object can start directly behind the string in memory, so the standard
+            // implementation would read the allocated size of the next object and use it for the hash
+            fixed (char* ptr = &aString.AsSpan()[0])
+            {
+                uint num = 352654597u;
+                uint num2 = num;
+                uint* ptr2 = (uint*)ptr;
+                int num3 = aString.Length;
+                while (num3 > 2)
+                {
+                    num3 -= 4;
+                    num = (global::System.Numerics.BitOperations.RotateLeft(num, 5) + num) ^ *ptr2;
+                    num2 = (global::System.Numerics.BitOperations.RotateLeft(num2, 5) + num2) ^ ptr2[1];
+                    ptr2 += 2;
+                }
+                if (num3 == 2)
+                {
+                    num2 = (global::System.Numerics.BitOperations.RotateLeft(num2, 5) + num2) ^ *ptr2;
+                }
+                if (num3 == 1)
+                {
+                    num2 = (global::System.Numerics.BitOperations.RotateLeft(num2, 5) + num2) ^ *(char*)ptr2;
+                }
+                return (int)(num + num2 * 1566083941);
+            }
+        }
     }
 }
