82140a5acb772a6bbc83db3da1e8b83e694b0e1c
zarlo <5899@zarlo.dev>
2022-09-21 13:21:16 +1000

    expose a way to register your own filesystem

---
+++ source/Cosmos.System2/FileSystem/VFS/FileSystemManager.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Cosmos.System.FileSystem.VFS
+{
+    public static class FileSystemManager
+    {
+        private static List<FileSystemFactory> registeredFileSystems = 
+            new List<FileSystemFactory>() { 
+                new FAT.FatFileSystemFactory(), 
+                new ISO9660.ISO9660FileSystemFactory()
+            };
+        
+        public static List<FileSystemFactory> RegisteredFileSystems { get {
+            return registeredFileSystems;
+        }}
+
+        public static bool Register(FileSystemFactory factory)
+        {
+            foreach (var item in registeredFileSystems)
+            {
+                if(item.Name == factory.Name)
+                {
+                    return false;
+                }
+            }
+            registeredFileSystems.Add(factory);
+            return true;
+        }
+
+        public static bool Remove(FileSystemFactory factory)
+        {
+            return Remove(factory.Name);
+        }
+
+        public static bool Remove(string factoryName)
+        {
+            foreach (var item in registeredFileSystems)
+            {
+                if(item.Name == factory.Name)
+                {
+                    registeredFileSystems.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
