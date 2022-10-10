a4ee35632990fad13eb50156fdf1bf1f9fa94e3e
Evan Lawrence <evanlawrence@cyberfear.com>
2022-10-09 21:19:02 -0700

    Plug System.Runtime.InteropServices.NativeMemory

---
+++ source/Cosmos.Core_Plugs/System/Runtime/InteropServices/NativeMemoryImpl.cs
@@ -0,0 +1,29 @@
+using IL2CPU.API.Attribs;
+using Cosmos.Core.Memory;
+using Cosmos.Core;
+
+namespace System.Runtime.InteropServices
+{
+    [Plug("System.Runtime.InteropServices.NativeMemory, System.Private.CoreLib")]
+    public static class NativeMemory
+	{
+        public static void* Realloc(void* ptr, nuint byteCount)
+		{
+            return Heap.Realloc(ptr, byteCount);
+		}
+
+        public static void* Alloc(nuint elementCount, nuint elementSize)
+		{
+            return Heap.Alloc((uint)(elementCount * elementSize));
+		}
+        public static void* Alloc(nuint byteCount)
+		{
+            return Heap.Alloc((uint)byteCount);
+		}
+
+        public static void Free(void* ptr)
+		{
+            Heap.Free(ptr);
+		}
+	}
+}
