6d050189e7e62d4fe909b81146084056651fc29b
valentinbreiz <valentinbreiz@gmail.com>
2022-10-21 14:45:25 +0200

    Add plugs
    - Stat
    - ConvertErrorPlatformToPal
    - ConvertErrorPalToPlatform
    - StrErrorR

--- source/Cosmos.Core_Plugs/Interop/SysImpl.cs
+++ source/Cosmos.Core_Plugs/Interop/SysImpl.cs
@@ -12,8 +12,32 @@ namespace Cosmos.Core_Plugs.Interop
             throw new NotImplementedException();
         }
 
+        [PlugMethod(Signature = "System_Int32__Interop_Sys_Stat__System_Byte___Interop_Sys_FileStatus_")]
+        public static int Stat(string path, out object output)
+        {
+            throw new NotImplementedException();
+        }
+
         [PlugMethod(Signature = "System_Int32__Interop_Sys_LStat__System_Byte___Interop_Sys_FileStatus_")]
         public static int LStat(string path, out object output)
+        {
+            throw new NotImplementedException();
+        }
+
+        [PlugMethod(Signature = "SystemPrivateCoreLibInterop_Error__Interop_Sys_ConvertErrorPlatformToPal_System_Int32_")]
+        public static object ConvertErrorPlatformToPal(int platformErrno)
+        {
+            throw new NotImplementedException();
+        }
+
+        [PlugMethod(Signature = "System_Int32__Interop_Sys_ConvertErrorPalToPlatform_SystemPrivateCoreLibInterop_Error_")]
+        public static int ConvertErrorPalToPlatform(object error)
+        {
+            throw new NotImplementedException();
+        }
+
+        [PlugMethod(Signature = "System_Byte#__Interop_Sys_StrErrorR_System_Int32__System_Byte#__System_Int32_")]
+        public static unsafe byte* StrErrorR(int platformErrno, byte* buffer, int bufferSize)
         {
             throw new NotImplementedException();
         }
