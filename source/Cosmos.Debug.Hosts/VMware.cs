4e68f2ba809fa2ba3233a6e952ad1beafdcdf632
TheFocusMan <sdshai2004@gmail.com>
2022-12-21 17:45:28 +0200

    VMware host upgrade

--- source/Cosmos.Debug.Hosts/VMware.cs
+++ source/Cosmos.Debug.Hosts/VMware.cs
@@ -7,38 +7,48 @@ using Microsoft.Win32;
 
 //using Vestris.VMWareLib;
 using Cosmos.Build.Common;
+using System.Linq;
 
-namespace Cosmos.Debug.Hosts {
-  public class VMware : Host {
+namespace Cosmos.Debug.Hosts
+{
+  public class VMware : Host
+  {
     protected VMwareEdition mEdition;
     protected string mDir;
     protected string mVmxPath;
     protected Process mProcess;
+    protected Process[] mProcessesBefore, mProcessesAfter, mProcesses;
     protected string mWorkstationPath;
     protected string mPlayerPath;
     protected string mHarddisk;
 
-    public VMware(Dictionary<string, string> aParams, bool aUseGDB,string harddisk = "Filesystem.vmdk") : base(aParams, aUseGDB) {
+    public VMware(Dictionary<string, string> aParams, bool aUseGDB, string harddisk = "Filesystem.vmdk") : base(aParams, aUseGDB)
+    {
       mHarddisk = harddisk;
       mDir = Path.Combine(CosmosPaths.Build, @"VMWare\Workstation\");
       mVmxPath = Path.Combine(mDir, @"Debug.vmx");
 
       mWorkstationPath = GetPathname("VMware Workstation", "vmware.exe");
       mPlayerPath = GetPathname("VMware Player", "vmplayer.exe");
-      if (mWorkstationPath == null && mPlayerPath == null) {
+      if (mWorkstationPath == null && mPlayerPath == null)
+      {
         throw new Exception("VMware not found.");
       }
 
       string xFlavor = aParams[BuildPropertyNames.VMwareEditionString].ToUpper();
       mEdition = VMwareEdition.Player;
-      if (xFlavor == "WORKSTATION") {
+      if (xFlavor == "WORKSTATION")
+      {
         mEdition = VMwareEdition.Workstation;
       }
 
       // Try alternate if selected one is not installed
-      if (mEdition == VMwareEdition.Player && mPlayerPath == null && mWorkstationPath != null) {
+      if (mEdition == VMwareEdition.Player && mPlayerPath == null && mWorkstationPath != null)
+      {
         mEdition = VMwareEdition.Workstation;
-      } else if (mEdition == VMwareEdition.Workstation && mWorkstationPath == null) {
+      }
+      else if (mEdition == VMwareEdition.Workstation && mWorkstationPath == null)
+      {
         mEdition = VMwareEdition.Player;
       }
     }
@@ -51,11 +61,15 @@ namespace Cosmos.Debug.Hosts {
     //  }
     //}
 
-    protected string GetPathname(string aKey, string aEXE) {
-      using (var xRegKey = Registry.LocalMachine.OpenSubKey(@"Software\WOW6432Node\VMware, Inc.\" + aKey, false)) {
-        if (xRegKey != null) {
+    protected string GetPathname(string aKey, string aEXE)
+    {
+      using (var xRegKey = Registry.LocalMachine.OpenSubKey(@"Software\WOW6432Node\VMware, Inc.\" + aKey, false))
+      {
+        if (xRegKey != null)
+        {
           string xResult = Path.Combine(((string)xRegKey.GetValue("InstallPath")), aEXE);
-          if (File.Exists(xResult)) {
+          if (File.Exists(xResult))
+          {
             return xResult;
           }
         }
@@ -63,24 +77,31 @@ namespace Cosmos.Debug.Hosts {
       }
     }
 
-    public override void Start() {
+    public override void Start()
+    {
       Cleanup();
       CreateDebugVmx();
 
       // Target exe or file
       mProcess = new Process();
       var xPSI = mProcess.StartInfo;
-      if (mEdition == VMwareEdition.Player) {
+      if (mEdition == VMwareEdition.Player)
+      {
         xPSI.FileName = mPlayerPath;
-      } else {
+      }
+      else
+      {
         xPSI.FileName = mWorkstationPath;
       }
       var xArgSB = new StringBuilder();
 
       string xVmxPath = "\"" + mVmxPath + "\"";
-      if (mEdition == VMwareEdition.Player) {
+      if (mEdition == VMwareEdition.Player)
+      {
         xPSI.Arguments = xVmxPath;
-      } else {
+      }
+      else
+      {
         // -x: Auto power on VM. Must be small x, big X means something else.
         // -q: Close VMWare when VM is powered off.
         // Options must come beore the vmx, and cannot use shellexecute
@@ -89,7 +110,27 @@ namespace Cosmos.Debug.Hosts {
       xPSI.UseShellExecute = false;  //must be true to allow elevate the process, sometimes needed if vmware only runs with admin rights
       mProcess.EnableRaisingEvents = true;
       mProcess.Exited += ExitCallback;
+
+      mProcessesBefore = Process.GetProcessesByName("vmware-vmx");
       mProcess.Start();
+
+      Stopwatch watch = Stopwatch.StartNew();
+
+      // Wait for the process to spawn
+      mProcessesAfter = Process.GetProcessesByName("vmware-vmx");
+      while (mProcessesAfter.Length == mProcessesBefore.Length)
+      {
+        if (watch.Elapsed.Seconds == 15)
+        {
+          watch.Stop();
+          throw new TimeoutException("VMware Workstation took too long to launch!");
+        }
+
+        mProcessesAfter = Process.GetProcessesByName("vmware-vmx");
+      }
+
+      // Get the new processes
+      mProcesses = mProcessesBefore.Concat(mProcessesAfter).Distinct().ToArray();
     }
 
     private void ExitCallback(object sender, EventArgs e)
@@ -106,18 +147,19 @@ namespace Cosmos.Debug.Hosts {
       }
     }
 
-    public override void Stop() {
+    public override void Stop()
+    {
       if (null != mProcess)
       {
         try
         {
           //TODO: Close VMWare properly
 
-          //Force Kill VMWare
+          // Kill the VMware GUI
           mProcess.Kill();
 
-          //kil vmware-vmx.exe
-          foreach (var process in Process.GetProcessesByName("vmware-vmx"))
+          // Kill the actual VM instance
+          foreach (var process in mProcesses)
           {
             process.Kill();
           }
@@ -129,15 +171,19 @@ namespace Cosmos.Debug.Hosts {
       Cleanup();
     }
 
-    protected void DeleteFiles(string aPath, string aPattern) {
+    protected void DeleteFiles(string aPath, string aPattern)
+    {
       var xFiles = Directory.GetFiles(aPath, aPattern);
-      foreach (var xFile in xFiles) {
+      foreach (var xFile in xFiles)
+      {
         File.Delete(xFile);
       }
     }
 
-    protected void Cleanup() {
-      try {
+    protected void Cleanup()
+    {
+      try
+      {
         string xPath = Path.GetDirectoryName(mVmxPath);
         // Delete old Debug.vmx and other files that might be left over from previous run.
         // Especially important with newer versions of VMWare player which defaults to suspend
@@ -155,50 +201,66 @@ namespace Cosmos.Debug.Hosts {
         File.Delete(Path.Combine(xPath, "vmware-0.log"));
         File.Delete(Path.Combine(xPath, "vmware-1.log"));
         File.Delete(Path.Combine(xPath, "vmware-2.log"));
-      } catch (Exception) {
+      }
+      catch (Exception)
+      {
         // Ignore errors, users can stop VS while VMware is still running and files will be locked.
       }
     }
 
-    protected void CreateDebugVmx() {
+    protected void CreateDebugVmx()
+    {
       // VMWare doesn't like to boot a read only VMX.
       // We also need to make changes based on project / debug settings.
       // Finally we do not want to create VCS checkins based on local user changes.
       // Because of this we use Cosmos.vmx as a template and output a Debug.vmx on every run.
-      using (var xSrc = new StreamReader(File.Open(Path.Combine(mDir, "Cosmos.vmx"), FileMode.OpenOrCreate))) {
-        try {
+      using (var xSrc = new StreamReader(File.Open(Path.Combine(mDir, "Cosmos.vmx"), FileMode.OpenOrCreate)))
+      {
+        try
+        {
           // Write out Debug.vmx
-          using (var xDest = new StreamWriter(File.Open(mVmxPath, FileMode.Create))) {
+          using (var xDest = new StreamWriter(File.Open(mVmxPath, FileMode.Create)))
+          {
             string xLine;
-            while ((xLine = xSrc.ReadLine()) != null) {
+            while ((xLine = xSrc.ReadLine()) != null)
+            {
               var xParts = xLine.Split('=');
-              if (xParts.Length == 2) {
+              if (xParts.Length == 2)
+              {
                 string xName = xParts[0].Trim();
                 string xValue = xParts[1].Trim();
 
-                if ((xName == "uuid.location") || (xName == "uuid.bios")) {
+                if ((xName == "uuid.location") || (xName == "uuid.bios"))
+                {
                   // We delete uuid entries so VMWare doesnt ask the user "Did you move or copy" the file
                   xValue = null;
 
-                } else if (xName == "ide1:0.fileName")
+                }
+                else if (xName == "ide1:0.fileName")
                 {
                   // Set the ISO file for booting
                   xValue = "\"" + mParams["ISOFile"] + "\"";
-                } else if (xName == "ide0:0.fileName") {
+                }
+                else if (xName == "ide0:0.fileName")
+                {
                   xValue = "\"" + mHarddisk + "\"";
-                } else if (xName == "nvram") {
+                }
+                else if (xName == "nvram")
+                {
                   // Point it to an initially non-existent nvram.
                   // This has the effect of disabling PXE so the boot is faster.
                   xValue = "\"Debug.nvram\"";
                 }
 
-                if (xValue != null) {
+                if (xValue != null)
+                {
                   xDest.WriteLine(xName + " = " + xValue);
                 }
               }
             }
 
-            if (mUseGDB) {
+            if (mUseGDB)
+            {
               xDest.WriteLine();
               xDest.WriteLine("debugStub.listen.guest32 = \"TRUE\"");
               xDest.WriteLine("debugStub.hideBreakpoints = \"TRUE\"");
@@ -206,8 +268,11 @@ namespace Cosmos.Debug.Hosts {
               xDest.WriteLine("debugStub.listen.guest32.remote = \"TRUE\"");
             }
           }
-        } catch (IOException ex) {
-          if (ex.Message.Contains(Path.GetFileName(mDir))) {
+        }
+        catch (IOException ex)
+        {
+          if (ex.Message.Contains(Path.GetFileName(mDir)))
+          {
             throw new Exception("The VMware image " + mDir + " is still in use. Please exit current Vmware session with Cosmos and try again.", ex);
           }
           throw;
