80511f970dffd41410e077b1d1a60b9f81468456
ascpixi <44982772+ascpixi@users.noreply.github.com>
2023-03-23 02:38:12 +0100

    Cosmos.System.Network code quality refactor: DHCP

--- source/Cosmos.System2/Network/IPv4/UDP/DHCP/DHCPClient.cs
+++ source/Cosmos.System2/Network/IPv4/UDP/DHCP/DHCPClient.cs
@@ -1,56 +1,37 @@
-﻿using Cosmos.System.Network.IPv4;
-using System;
-using Cosmos.System.Network.IPv4.UDP.DNS;
+﻿using System;
 using Cosmos.System.Network.Config;
-using System.Collections.Generic;
 using Cosmos.HAL;
-
-/*
-* PROJECT:          Aura Operating System Development
-* CONTENT:          DHCP - DHCP Core
-* PROGRAMMER(S):    Alexy DA CRUZ <dacruzalexy@gmail.com>
-*/
 
 namespace Cosmos.System.Network.IPv4.UDP.DHCP
 {
     /// <summary>
-    /// DHCPClient class. Used to manage the DHCP connection to a server.
+    /// Used to manage the DHCP connection to a server.
     /// </summary>
     public class DHCPClient : UdpClient
     {
-        /// <summary>
-        /// Is DHCP ascked check variable
-        /// </summary>
         private bool asked = false;
 
         /// <summary>
-        /// Get the IP address of the DHCP server
+        /// Gets the IP address of the DHCP server.
         /// </summary>
-        /// <returns></returns>
         public static Address DHCPServerAddress(NetworkDevice networkDevice)
         {
             return NetworkConfiguration.Get(networkDevice).DefaultGateway;
         }
 
         /// <summary>
-        /// Create new instance of the <see cref="DHCPClient"/> class.
+        /// Initializes a new instance of the <see cref="DHCPClient"/> class.
         /// </summary>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error (contact support).</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown on fatal error.</exception>
         /// <exception cref="ArgumentException">Thrown if UdpClient with localPort 53 exists.</exception>
         public DHCPClient() : base(68)
         {
         }
 
-        /// <summary>
-        /// Receive data
-        /// </summary>
-        /// <param name="timeout">timeout value, default 5000ms</param>
-        /// <returns>time value (-1 = timeout)</returns>
-        /// <exception cref="InvalidOperationException">Thrown on fatal error (contact support).</exception>
         private int Receive(int timeout = 5000)
         {
             int second = 0;
-            int _deltaT = 0;
+            int deltaT = 0;
 
             while (rxBuffer.Count < 1)
             {
@@ -58,23 +39,23 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
                 {
                     return -1;
                 }
-                if (_deltaT != RTC.Second)
+                if (deltaT != RTC.Second)
                 {
                     second++;
-                    _deltaT = RTC.Second;
+                    deltaT = RTC.Second;
                 }
             }
 
             var packet = new DHCPPacket(rxBuffer.Dequeue().RawData);
 
-            if (packet.MessageType == 2) //Boot Reply
+            if (packet.MessageType == 2) // Boot Reply
             {
-                if (packet.RawData[284] == 0x02) //Offer packet received
+                if (packet.RawData[284] == 0x02) // Offer packet received
                 {
-                    Global.mDebugger.Send("Offer received.");
+                    NetworkStack.Debugger.Send("Offer received.");
                     return SendRequestPacket(packet.Client);
                 }
-                else if (packet.RawData[284] == 0x05 || packet.RawData[284] == 0x06) //ACK or NAK DHCP packet received
+                else if (packet.RawData[284] is 0x05 or 0x06) // ACK or NAK DHCP packet received
                 {
                     var ack = new DHCPAck(packet.RawData);
                     if (asked)
@@ -92,29 +73,31 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
         }
 
         /// <summary>
-        /// Send a packet to the DHCP server to make the address available again
+        /// Sends a packet to the DHCP server in order to make the address available again.
         /// </summary>
         public void SendReleasePacket()
         {
             foreach (NetworkDevice networkDevice in NetworkDevice.Devices)
             {
                 Address source = IPConfig.FindNetwork(DHCPServerAddress(networkDevice));
-                var dhcp_release = new DHCPRelease(source, DHCPServerAddress(networkDevice), networkDevice.MACAddress);
+                var dhcpRelease = new DHCPRelease(source, DHCPServerAddress(networkDevice), networkDevice.MACAddress);
 
-                OutgoingBuffer.AddPacket(dhcp_release);
+                OutgoingBuffer.AddPacket(dhcpRelease);
                 NetworkStack.Update();
 
                 NetworkStack.RemoveAllConfigIP();
 
                 IPConfig.Enable(networkDevice, new Address(0, 0, 0, 0), new Address(0, 0, 0, 0), new Address(0, 0, 0, 0));
             }
+
             Close();
         }
 
         /// <summary>
-        /// Send a packet to find the DHCP server and tell that we want a new IP address
+        /// Send a packet to find the DHCP server and inform the host that we
+        /// are requesting a new IP address.
         /// </summary>
-        /// <returns>time value (-1 = timeout)</returns>
+        /// <returns>The amount of time elapsed, or -1 if a timeout has been reached.</returns>
         public int SendDiscoverPacket()
         {
             NetworkStack.RemoveAllConfigIP();
@@ -123,8 +106,8 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
             {
                 IPConfig.Enable(networkDevice, new Address(0, 0, 0, 0), new Address(0, 0, 0, 0), new Address(0, 0, 0, 0));
 
-                var dhcp_discover = new DHCPDiscover(networkDevice.MACAddress);
-                OutgoingBuffer.AddPacket(dhcp_discover);
+                var dhcpDiscover = new DHCPDiscover(networkDevice.MACAddress);
+                OutgoingBuffer.AddPacket(dhcpDiscover);
                 NetworkStack.Update();
 
                 asked = true;
@@ -134,15 +117,15 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
         }
 
         /// <summary>
-        /// Send a request to apply the new IP configuration
+        /// Sends a request to apply the new IP configuration.
         /// </summary>
-        /// <returns>time value (-1 = timeout)</returns>
-        private int SendRequestPacket(Address RequestedAddress)
+        /// <returns>The amount of time elapsed, or -1 if a timeout has been reached.</returns>
+        private int SendRequestPacket(Address requestedAddress)
         {
             foreach (NetworkDevice networkDevice in NetworkDevice.Devices)
             {
-                var dhcp_request = new DHCPRequest(networkDevice.MACAddress, RequestedAddress);
-                OutgoingBuffer.AddPacket(dhcp_request);
+                var dhcpRequest = new DHCPRequest(networkDevice.MACAddress, requestedAddress);
+                OutgoingBuffer.AddPacket(dhcpRequest);
                 NetworkStack.Update();
             }
             return Receive();
@@ -152,9 +135,8 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
          * Method called to applied the differents options received in the DHCP packet ACK
          **/
         /// <summary>
-        /// Apply the new IP configuration received.
+        /// Applies the newly received IP configuration.
         /// </summary>
-        /// <param name="Options">DHCPOption class using the packetData from the received dhcp packet.</param>
         /// <param name="message">Enable/Disable the displaying of messages about DHCP applying and conf. Disabled by default.
         /// </param>
         private void Apply(DHCPAck packet, bool message = false)
@@ -164,6 +146,8 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
             //cf. Roadmap. (have to change this, because some network interfaces are not configured in dhcp mode) [have to be done in 0.5.x]
             foreach (NetworkDevice networkDevice in NetworkDevice.Devices)
             {
+                // NOTE: @ascpixi: Why are we checking if ToString() returns null
+                //       *four* times...? Can this be removed?
                 if (packet.Client.ToString() == null ||
                     packet.Client.ToString() == null ||
                     packet.Client.ToString() == null ||
@@ -175,11 +159,11 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
                 {
                     if (message)
                     {
-                        Global.mDebugger.Send("[DHCP ACK][" + networkDevice.Name + "] Packet received, applying IP configuration...");
-                        Global.mDebugger.Send("   IP Address  : " + packet.Client.ToString());
-                        Global.mDebugger.Send("   Subnet mask : " + packet.Subnet.ToString());
-                        Global.mDebugger.Send("   Gateway     : " + packet.Server.ToString());
-                        Global.mDebugger.Send("   DNS server  : " + packet.DNS.ToString());
+                        NetworkStack.Debugger.Send("[DHCP ACK][" + networkDevice.Name + "] Packet received, applying IP configuration...");
+                        NetworkStack.Debugger.Send("   IP Address  : " + packet.Client.ToString());
+                        NetworkStack.Debugger.Send("   Subnet mask : " + packet.Subnet.ToString());
+                        NetworkStack.Debugger.Send("   Gateway     : " + packet.Server.ToString());
+                        NetworkStack.Debugger.Send("   DNS server  : " + packet.DNS.ToString());
                     }
 
                     IPConfig.Enable(networkDevice, packet.Client, packet.Subnet, packet.Server);
@@ -187,7 +171,7 @@ namespace Cosmos.System.Network.IPv4.UDP.DHCP
 
                     if (message)
                     {
-                        Global.mDebugger.Send("[DHCP CONFIG][" + networkDevice.Name + "] IP configuration applied.");
+                        NetworkStack.Debugger.Send("[DHCP CONFIG][" + networkDevice.Name + "] IP configuration applied.");
                         asked = false;
                     }
                 }
--- source/Cosmos.System2/Network/IPv4/UDP/DHCP/DHCPRequest.cs
+++ source/Cosmos.System2/Network/IPv4/UDP/DHCP/DHCPRequest.cs
@@ -1,66 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Cosmos.HAL.Network;
-using Cosmos.System.Network.IPv4;
-using Cosmos.System.Network.IPv4.UDP.DHCP;
 
 namespace Cosmos.System.Network.IPv4.UDP.DHCP
 {
     /// <summary>
-    /// DHCPRequest class.
+    /// Represents a DHCP request packet.
     /// </summary>
     internal class DHCPRequest : DHCPPacket
     {
-
         /// <summary>
-        /// Create new instance of the <see cref="DHCPRequest"/> class.
+        /// Initializes a new instance of the <see cref="DHCPRequest"/> class.
         /// </summary>
         internal DHCPRequest() : base()
         { }
 
         /// <summary>
-        /// Create new instance of the <see cref="DHCPRequest"/> class.
+        /// Initializes a new instance of the <see cref="DHCPRequest"/> class.
         /// </summary>
         /// <param name="rawData">Raw data.</param>
         internal DHCPRequest(byte[] rawData) : base(rawData)
         { }
 
         /// <summary>
-        /// Create new instance of the <see cref="DHCPRequest"/> class.
+        /// Initializes a new instance of the <see cref="DHCPRequest"/> class.
         /// </summary>
-        /// <param name="mac_src">Source MAC Address.</param>
-        /// <param name="RequestedAddress">Requested Address.</param>
-        /// <param name="DHCPServerAddress">DHCP server IPv4 Address.</param>
+        /// <param name="sourceMAC">Source MAC Address.</param>
+        /// <param name="requestedAddress">Requested Address.</param>
         /// <exception cref="OverflowException">Thrown if data array length is greater than Int32.MaxValue.</exception>
         /// <exception cref="ArgumentException">Thrown if RawData is invalid or null.</exception>
-        internal DHCPRequest(MACAddress mac_src, Address RequestedAddress) : base(mac_src, 16)
+        internal DHCPRequest(MACAddress sourceMAC, Address requestedAddress) : base(sourceMAC, 16)
         {
-            //Request
+            // Request
             RawData[282] = 53;
             RawData[283] = 1;
             RawData[284] = 3;
 
-            //Requested Address
+            // Requested Address
             RawData[285] = 50;
             RawData[286] = 4;
 
-            RawData[287] = RequestedAddress.address[0];
-            RawData[288] = RequestedAddress.address[1];
-            RawData[289] = RequestedAddress.address[2];
-            RawData[290] = RequestedAddress.address[3];
+            RawData[287] = requestedAddress.Parts[0];
+            RawData[288] = requestedAddress.Parts[1];
+            RawData[289] = requestedAddress.Parts[2];
+            RawData[290] = requestedAddress.Parts[3];
 
-            //Parameters start here
+            // Parameters start here
             RawData[291] = 0x37;
             RawData[292] = 4;
 
-            //Parameters
+            // Parameters
             RawData[293] = 0x01;
             RawData[294] = 0x03;
             RawData[295] = 0x0f;
             RawData[296] = 0x06;
 
-            RawData[297] = 0xff; //ENDMARK
+            RawData[297] = 0xff; // ENDMARK
         }
     }
 }
