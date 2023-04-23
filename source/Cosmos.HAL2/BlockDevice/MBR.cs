fb9822b7fa336ddf8f159922c69b44484d69f6e2
Husaral <48690966+husaral2@users.noreply.github.com>
2023-04-23 11:07:57 +0300

    Add files via upload

--- source/Cosmos.HAL2/BlockDevice/MBR.cs
+++ source/Cosmos.HAL2/BlockDevice/MBR.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks.Sources;
 using Cosmos.Common.Extensions;
+using Cosmos.Core;
 
 namespace Cosmos.HAL.BlockDevice
 {
@@ -64,6 +66,56 @@ namespace Cosmos.HAL.BlockDevice
                 var xPartInfo = new PartInfo(xSystemID, xStartSector, xSectorCount);
                 Partitions.Add(xPartInfo);
             }
+        }
+
+        public void CreateMBR(BlockDevice device)
+        {
+            ManagedMemoryBlock mb = new ManagedMemoryBlock(512);
+            mb.Fill(0);
+            //Boot code
+            mb.Write32(0, 0x1000B8FA);
+            mb.Write32(4, 0x00BCD08E);
+            mb.Write32(8, 0x0000B8B0);
+            mb.Write32(12, 0xC08ED88E);
+            mb.Write32(16, 0x7C00BEFB);
+            mb.Write32(20, 0xB90600BF);
+            mb.Write32(24, 0xA4F30200);
+            mb.Write32(28, 0x000621EA);
+            mb.Write32(32, 0x07BEBE07);
+            mb.Write32(36, 0x0B750438);
+            mb.Write32(40, 0x8110C683);
+            mb.Write32(44, 0x7507FEFE);
+            mb.Write32(48, 0xB416EBF3);
+            mb.Write32(52, 0xBB01B002);
+            mb.Write32(56, 0x80B27C00);
+            mb.Write32(60, 0x8B01748A);
+            mb.Write32(64, 0x13CD024C);
+            mb.Write32(68, 0x007C00EA);
+            mb.Write32(72, 0x00FEEB00);
+            //Unique disk ID, is used to seperate different drives
+            mb.Write32(440, (uint)device.GetHashCode() * 0x5A5A);
+            //Signature
+            mb.Write16(510, 0xAA55);
+            device.WriteBlock(0, 1, ref mb.memory);
+        }
+
+
+        public void WritePartitionInformation(Partition partition, byte PartitionNo)
+        {
+            if (PartitionNo > 3)
+            {
+                return;
+            }
+
+            ManagedMemoryBlock mb = new ManagedMemoryBlock(512);
+            partition.Host.ReadBlock(0, 1, ref mb.memory);
+            //TO DO: Implement the CHS starting / ending sector adresses and partition type
+            mb.Write8((uint)(446 + (PartitionNo * 16) + 4), 0x0B);
+            mb.Write32((uint)(446 + (PartitionNo * 16) + 8), (uint) partition.StartingSector);
+            mb.Write32((uint)(446 + (PartitionNo * 16) + 12), (uint) partition.BlockCount);
+            partition.Host.WriteBlock(0, 1, ref mb.memory);
+            ParsePartition(mb.memory, 446 + (uint)(PartitionNo * 16));
+            
         }
     }
 }
