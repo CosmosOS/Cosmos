1ad2e0e45832ee5a9eaa726d1453d79baa187425
Husaral <48690966+husaral2@users.noreply.github.com>
2023-04-23 11:17:03 +0300

    Add files via upload

--- source/Cosmos.HAL2/BlockDevice/MBR.cs
+++ source/Cosmos.HAL2/BlockDevice/MBR.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks.Sources;
-using Cosmos.Common.Extensions;
 using Cosmos.Core;
 
 namespace Cosmos.HAL.BlockDevice
