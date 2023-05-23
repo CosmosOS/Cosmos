e32686502ca2f580193054cd13321c3eca2920b7
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2023-05-23 17:20:30 +0200

    Remove 300bps baud rate because that requires 16 bits

--- source/Cosmos.HAL2/BaudRate.cs
+++ source/Cosmos.HAL2/BaudRate.cs
@@ -10,56 +10,51 @@ namespace Cosmos.HAL
         /// <summary>
         /// 115200 bits per second.
         /// </summary>
-        BaudRate115200 = 0x001,
+        BaudRate115200 = 0x01,
 
         /// <summary>
         /// 57600 bits per second.
         /// </summary>
-        BaudRate57600 = 0x002,
+        BaudRate57600 = 0x02,
 
         /// <summary>
         /// 38400 bits per second.
         /// </summary>
-        BaudRate38400 = 0x003,
+        BaudRate38400 = 0x03,
 
         /// <summary>
         /// 19200 bits per second.
         /// </summary>
-        BaudRate19200 = 0x006,
+        BaudRate19200 = 0x06,
 
         /// <summary>
         /// 9600 bits per second.
         /// </summary>
-        BaudRate9600 = 0x012,
+        BaudRate9600 = 0x12,
 
         /// <summary>
         /// 7200 bits per second.
         /// </summary>
-        BaudRate7200 = 0x010,
+        BaudRate7200 = 0x10,
 
         /// <summary>
         /// 4800 bits per second.
         /// </summary>
-        BaudRate4800 = 0x018,
+        BaudRate4800 = 0x18,
 
         /// <summary>
         /// 2400 bits per second.
         /// </summary>
-        BaudRate2400 = 0x030,
+        BaudRate2400 = 0x30,
 
         /// <summary>
         /// 1200 bits per second.
         /// </summary>
-        BaudRate1200 = 0x060,
+        BaudRate1200 = 0x60,
 
         /// <summary>
         /// 600 bits per second.
         /// </summary>
-        BaudRate600 = 0x0C0,
-
-        /// <summary>
-        /// 300 bits per second.
-        /// </summary>
-        BaudRate300 = 0x180
+        BaudRate600 = 0xC0,
     }
 }
