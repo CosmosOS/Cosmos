b7dcc825d37d80bfe954060f34b3f3acc78c0550
valentinbreiz <valentinbreiz@gmail.com>
2023-09-26 18:29:46 +0200

    🐛 Fix test by using old vmdk

--- Tests/Cosmos.TestRunner.Core/Engine.Run.cs
+++ Tests/Cosmos.TestRunner.Core/Engine.Run.cs
@@ -43,7 +43,7 @@ namespace Cosmos.TestRunner.Core
             else
             {
                 xHarddiskPath = Path.Combine(workingDirectory, "Harddisk.vmdk");
-                var xOriginalHarddiskPath = Path.Combine(GetCosmosUserkitFolder(), "Build", "VMware", "Workstation", "FilesystemTest.vmdk");
+                var xOriginalHarddiskPath = Path.Combine(GetCosmosUserkitFolder(), "Build", "VMware", "Workstation", "Filesystem.vmdk");
                 File.Copy(xOriginalHarddiskPath, xHarddiskPath);
             }
 
