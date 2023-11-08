f33a693b5195f2e3f38be127a6427f0f3979067e
valentinbreiz <valentinbreiz@gmail.com>
2023-11-08 15:45:37 +0100

    🔥 Remove useless Stream plug + implement shutdown plug

---
+++ source/Cosmos.System2_Plugs/Interop/WinsockImpl.cs
@@ -0,0 +1,16 @@
+﻿using IL2CPU.API.Attribs;
+using System;
+using System.Net.Sockets;
+
+namespace Cosmos.Core_Plugs.Interop
+{
+    [Plug("Interop+Winsock, System.Net.Sockets", IsOptional = true)]
+    class WinsockImpl
+    {
+        [PlugMethod(Signature = "System_Net_Sockets_SocketError__Interop_Winsock_shutdown_System_Net_Sockets_SafeSocketHandle__System_Int32_")]
+        public static SocketError shutdown(SafeSocketHandle socketHandle, int how)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
