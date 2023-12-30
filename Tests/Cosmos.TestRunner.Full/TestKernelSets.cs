7910b947d4225841ad3f8635d316827af7a377d3
Quajak <jasper.gerigk@mail.utoronto.ca>
2023-12-30 16:15:15 +0100

    Undo changes to default files

--- Tests/Cosmos.TestRunner.Full/TestKernelSets.cs
+++ Tests/Cosmos.TestRunner.Full/TestKernelSets.cs
@@ -15,29 +15,29 @@ namespace Cosmos.TestRunner.Full
         // Stable kernel types: the ones that are stable and will run in AppVeyor
         public static IEnumerable<Type> GetStableKernelTypes()
         {
-            //yield return typeof(BoxingTests.Kernel);
+            yield return typeof(BoxingTests.Kernel);
             yield return typeof(Compiler.Tests.TypeSystem.Kernel);
-            //yield return typeof(Compiler.Tests.Bcl.Kernel);
-            //yield return typeof(Compiler.Tests.Bcl.System.Kernel);
-            ////yield return typeof(Cosmos.Compiler.Tests.Encryption.Kernel);
-            //yield return typeof(Compiler.Tests.Exceptions.Kernel);
-            //yield return typeof(Compiler.Tests.MethodTests.Kernel);
-            //yield return typeof(Compiler.Tests.SingleEchoTest.Kernel);
-            //yield return typeof(Kernel.Tests.Fat.Kernel);
-            //yield return typeof(Kernel.Tests.IO.Kernel);
-            //yield return typeof(SimpleStructsAndArraysTest.Kernel);
-            //yield return typeof(Kernel.Tests.DiskManager.Kernel);
+            yield return typeof(Compiler.Tests.Bcl.Kernel);
+            yield return typeof(Compiler.Tests.Bcl.System.Kernel);
+            //yield return typeof(Cosmos.Compiler.Tests.Encryption.Kernel);
+            yield return typeof(Compiler.Tests.Exceptions.Kernel);
+            yield return typeof(Compiler.Tests.MethodTests.Kernel);
+            yield return typeof(Compiler.Tests.SingleEchoTest.Kernel);
+            yield return typeof(Kernel.Tests.Fat.Kernel);
+            yield return typeof(Kernel.Tests.IO.Kernel);
+            yield return typeof(SimpleStructsAndArraysTest.Kernel);
+            yield return typeof(Kernel.Tests.DiskManager.Kernel);
 
-            ////yield return typeof(KernelGen3.Boot);
-            //yield return typeof(GraphicTest.Kernel);
+            //yield return typeof(KernelGen3.Boot);
+            yield return typeof(GraphicTest.Kernel);
 
-            //// Disable network tests due to our self hosted CI/CD limitations (VPS currently doesn't support VMWare with its CPU)
-            ////yield return typeof(NetworkTest.Kernel);
-            //yield return typeof(AudioTests.Kernel);
-            //// Please see the notes on the kernel itself before enabling it
-            ////yield return typeof(ConsoleTest.Kernel);
-            //yield return typeof(MemoryOperationsTest.Kernel);
-            //yield return typeof(ProcessorTests.Kernel);
+            // Disable network tests due to our self hosted CI/CD limitations (VPS currently doesn't support VMWare with its CPU)
+            //yield return typeof(NetworkTest.Kernel);
+            yield return typeof(AudioTests.Kernel);
+            // Please see the notes on the kernel itself before enabling it
+            //yield return typeof(ConsoleTest.Kernel);
+            yield return typeof(MemoryOperationsTest.Kernel);
+            yield return typeof(ProcessorTests.Kernel);
         }
     }
 }
