50af821ad280b379a71018ce6e1c2a8678505b65
Guillermo Santos <santosguillermo360@outlook.com>
2024-01-12 22:43:32 -0400

    Create KeyEventHelper.cs
    A helper class with a method to get a ConsoleKeyInfo object from a key event object (took away from ReadKey ConsoleImpl method)

---
+++ source/Cosmos.System2/Keyboard/KeyEventHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cosmos.System.Keyboard
+{
+    public static class KeyEventHelper
+    {
+        internal static ConsoleKeyInfo ToConsoleKeyInfo(this KeyEvent keyEvent)
+        {
+            //TODO: Plug HasFlag and use the next 3 lines instead of the 3 following lines
+
+            //bool xShift = key.Modifiers.HasFlag(ConsoleModifiers.Shift);
+            //bool xAlt = key.Modifiers.HasFlag(ConsoleModifiers.Alt);
+            //bool xControl = key.Modifiers.HasFlag(ConsoleModifiers.Control);
+
+            bool xShift = (keyEvent.Modifiers & ConsoleModifiers.Shift) == ConsoleModifiers.Shift;
+            bool xAlt = (keyEvent.Modifiers & ConsoleModifiers.Alt) == ConsoleModifiers.Alt;
+            bool xControl = (keyEvent.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control;
+
+            return new ConsoleKeyInfo(keyEvent.KeyChar, keyEvent.Key.ToConsoleKey(), xShift, xAlt, xControl);
+        }
+    }
+}
